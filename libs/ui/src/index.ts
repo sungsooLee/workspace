bfa33aebf0df4d24734c94d694444238cdbe24bf
9488404 <9488404@ict-companion.com>
2025-01-08 14:39:57 +0900

    feat: carousel 컴포넌트 추가

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -37,6 +37,7 @@ export * from './lib/shadcn/menubar';
 export * from './lib/shadcn/popover';
 export * from './lib/shadcn/radio-group';
 export * from './lib/shadcn/separator';
+export * from './lib/shadcn/carousel';
 // export * from './lib/shadcn/switch'
 
 export * from './lib/switch/switch';
