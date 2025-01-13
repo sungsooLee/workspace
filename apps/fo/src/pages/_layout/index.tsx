d1473da79836848a14bb481f4298984e78b0ab62
9488404 <9488404@ict-companion.com>
2025-01-13 12:47:32 +0900

    feat: ui common component figma 동기화

--- apps/fo/src/pages/_layout/index.tsx
+++ apps/fo/src/pages/_layout/index.tsx
@@ -2,12 +2,10 @@ import { useEffect } from 'react';
 import { createFileRoute } from '@tanstack/react-router';
 import { useTranslation } from 'react-i18next';
 
-import { Button } from '@learnway/ui';
-import { Carousel } from '@learnway/ui';
-import { ImageCarousel } from '@learnway/ui';
+import { Button, FieldType, ImageCarousel, Radio } from '@learnway/ui';
 
 export const Route = createFileRoute('/_layout/')({
-  component: HomeComponent,
+  component: HomeComponent,w
 });
 
 function HomeComponent() {
@@ -24,7 +22,13 @@ function HomeComponent() {
       {/** Carousel */}
       <div className="flex w-full gap-5">
         <div className="min-w-32">Carousel</div>
-        <ImageCarousel imageUrls={['a.png', 'b.png', 'c.png', 'd.png', 'e.png']} />
+        {/*<ImageCarousel imageUrls={['a.png', 'b.png', 'c.png', 'd.png', 'e.png']} />*/}
+        <Radio
+          type={FieldType.RADIO}
+          name='name'
+          label='label'
+          options={Array(5).fill(null).map((d, i) => ({value: `value${i}`, label: `label${i}`}))}
+        />
       </div>
 
       {/**primary */}
