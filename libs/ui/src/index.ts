472bce8b89c98571707b7a19f380a91a62df2855
9488404 <9488404@ict-companion.com>
2025-01-13 16:30:29 +0900

    feat: ui common component figma 동기화

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,4 +1,18 @@
 
+// avatar
+export * from './lib/avatar/avatar';
+export * from './lib/popover/popover';
+
+// backdrop
+export * from './lib/backdrop/backdrop';
+
+// card
+export * from './lib/card/card';
+
+// carousel
+export * from './lib/carousel/carousel';
+export * from './lib/carousel/image-carousel';
+
 // chekbox
 export * from './lib/checkbox/checkbox';
 
@@ -10,51 +24,9 @@ export * from './lib/date-picker/date-range-picker';
 export * from './lib/input/input';
 export * from './lib/input/number-input';
 
+// form
 export * from './lib/dynamic-form-field/dynamic-form-field-control';
 export * from './lib/dynamic-form-field/dynamic-form-field';
-
-// radio
-export * from './lib/radio/radio';
-
-// select
-export * from './lib/select/select';
-export * from './lib/select/multi-select';
-
-// card
-export * from './lib/card/card';
-
-// tree
-export * from './lib/tree-view/tree';
-export * from './lib/tree-view/tree.service';
-export * from './lib/tree-view/node-detail';
-export * from './lib/tree-view/tree.context';
-export * from './lib/tree-view/type';
-
-// switch
-export * from './lib/switch/switch';
-
-// shadcn
-export * from './lib/shadcn/toaster';
-export * from './lib/shadcn/use-toast';
-export * from './lib/shadcn/badge';
-export * from './lib/shadcn/button';
-export * from './lib/shadcn/calendar';
-// export * from './lib/shadcn/checkbox';
-export * from './lib/shadcn/command';
-export * from './lib/shadcn/dialog';
-export * from './lib/shadcn/form';
-export * from './lib/shadcn/label';
-export * from './lib/shadcn/menubar';
-export * from './lib/shadcn/radio-group';
-export * from './lib/shadcn/separator';
-// export * from './lib/select/select.shadcn';
-// export * from './lib/shadcn/switch'
-
-// backdrop
-export * from './lib/backdrop/backdrop';
-
-// spinner
-export * from './lib/spinner/spinner';
 
 // modal
 export * from './lib/modal/modal';
@@ -64,6 +36,26 @@ export * from './lib/modal/modal-container';
 export * from './lib/stores/useModalStore';
 export * from './lib/modal/modal-context';
 
+// radio
+export * from './lib/radio/radio';
+
+// select
+export * from './lib/select/select';
+export * from './lib/select/multi-select';
+
+// switch
+export * from './lib/switch/switch';
+
+// spinner
+export * from './lib/spinner/spinner';
+
+// tree
+export * from './lib/tree-view/tree';
+export * from './lib/tree-view/tree.service';
+export * from './lib/tree-view/node-detail';
+export * from './lib/tree-view/tree.context';
+export * from './lib/tree-view/type';
+
 // toast
 export * from './lib/shadcn/toast';
 
@@ -71,13 +63,22 @@ export * from './lib/shadcn/toast';
 export * from './lib/editor/hooks/editor.hook';
 export { default as Editor } from './lib/editor';
 
-// avatar
-export * from './lib/avatar/avatar';
-export * from './lib/popover/popover';
-
-// carousel
-export * from './lib/carousel/carousel';
-export * from './lib/carousel/image-carousel';
-
 // type
 export * from './lib/type';
+
+// shadcn (주석처리 예정)
+export * from './lib/shadcn/toaster';
+export * from './lib/shadcn/use-toast';
+export * from './lib/shadcn/badge';
+export * from './lib/shadcn/button';
+export * from './lib/shadcn/calendar';
+export * from './lib/shadcn/command';
+export * from './lib/shadcn/dialog';
+export * from './lib/shadcn/form';
+export * from './lib/shadcn/label';
+export * from './lib/shadcn/menubar';
+export * from './lib/shadcn/radio-group';
+export * from './lib/shadcn/separator';
+// export * from './lib/shadcn/checkbox';
+// export * from './lib/select/select.shadcn';
+// export * from './lib/shadcn/switch'
