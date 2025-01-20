4f1e1668e071c5cfe1af9b1b345bf4b77415f119
HAE\9490817 <lsstmi@naver.com>
2025-01-20 09:48:44 +0900

    [퍼블] 아코디언 아이콘 추가

--- libs/icons/src/index.ts
+++ libs/icons/src/index.ts
@@ -1,4 +1,5 @@
 export { ReactComponent as Information } from './lib/information.svg';
+export { ReactComponent as IcoArrowDown } from './lib/ic_arrow_down.svg'; // LNB Icon Arrow
 //export Default as Information from './lib/information.svg?react';
 
 //import Information from './lib/information.svg';
--- libs/ui/src/lib/accordion/accordion.tsx
+++ libs/ui/src/lib/accordion/accordion.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef } from 'react';
 import * as Primitive from '@radix-ui/react-accordion';
-import { ChevronDownIcon } from '@radix-ui/react-icons';
+import { IcoArrowDown } from '@learnway/icons';
 
 import { cn } from '@learnway/shared';
 
@@ -36,7 +36,7 @@ const AccordionComponent = forwardRef<
           <Primitive.Item value={item.key} className={cn(styles.accordion_item)} key={item.key}>
             <Primitive.Trigger className={cn(styles.accordion_btn)}>
               <span>{item.title}</span>
-              {item.children && <ChevronDownIcon />}
+              {item.children && <IcoArrowDown width={16} height={16} stroke="#131C30" />}
             </Primitive.Trigger>
             <Primitive.Content className={cn(styles.accordion_content)}>
               {item.children}
