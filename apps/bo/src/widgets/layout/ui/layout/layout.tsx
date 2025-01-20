c5154db5f4ddb0684f81e9beed2f6fffee18318b
9488404 <9488404@ict-companion.com>
2025-01-20 10:31:50 +0900

    ui component shadcn > radix 전환

--- apps/bo/src/widgets/layout/ui/layout/layout.tsx
+++ apps/bo/src/widgets/layout/ui/layout/layout.tsx
@@ -2,7 +2,7 @@ import { memo, ReactNode } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { cn } from '@learnway/shared';
-import { SidebarProvider, SidebarTrigger, Sidebar, Radio } from '@learnway/ui';
+import { SidebarProvider, SidebarTrigger, Sidebar } from '@learnway/ui';
 
 import { useActiveMenuDepthState } from '../../../../features/layout';
 import { LNB } from '../lnb/lnb';
@@ -19,17 +19,21 @@ function LayoutComponent({ children }: LayoutComponentProps) {
 
   const [activeMenuDepth] = useActiveMenuDepthState();
 
-  if (activeMenuDepth && activeMenuDepth[0].children && activeMenuDepth[0].children?.length > 0) {
+  if (
+    activeMenuDepth &&
+    activeMenuDepth?.length &&
+    activeMenuDepth[0].children &&
+    activeMenuDepth[0].children?.length > 0
+  ) {
     return (
-      <SidebarProvider className={styles._start}>
-        <Sidebar className={styles._sidebar}>
+      <div className={styles.start}>
+        <div className={styles.bo_container}>
           <LNB />
-        </Sidebar>
-        <main>
-          <SidebarTrigger />
-          <Radio options={Array(5).fill(null).map((d, i) => ({value: `value${i}`, label: `label${i}`}))} />
-        </main>
-      </SidebarProvider>
+          <main>
+            <PageContainer>{children}</PageContainer>
+          </main>
+        </div>
+      </div>
     );
   }
 
