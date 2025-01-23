3e56cc18032d9bdcbe5e2ba74fc771ba1d0e3dda
9480335 <9480335@ict-companion.com>
2025-01-23 09:25:07 +0900

    feat: [HMGSLP-207] Enhanced AccordionMenu

--- apps/bo/src/widgets/layout/ui/lnb/lnb.tsx
+++ apps/bo/src/widgets/layout/ui/lnb/lnb.tsx
@@ -1,8 +1,10 @@
-import { memo } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useCreation, useDeepCompareEffect } from 'ahooks';
 
 import { cn } from '@learnway/shared';
 import { SidebarContent, SidebarGroup, SidebarGroupLabel, SidebarGroupContent } from '@learnway/ui';
+import { IcoArrowDown } from '@learnway/icons';
 
 import { useActiveMenuDepthState } from '../../../../features/layout';
 
@@ -15,21 +17,65 @@ function LNBComponent() {
 
   const [activeMenuDepth] = useActiveMenuDepthState();
 
+  const [menus, setMenus] = useState<any>(activeMenuDepth?.[0]?.children);
+  const [openAll, setOpenAll] = useState<boolean | undefined>(undefined);
+  const [openAllButtonState, setOpenAllButtonState] = useState<boolean>(false); // LNB 최상단 타이틀 active
+
+  const buttonClass = `${openAllButtonState ? styles.active : styles.inactive}`;
+
+  useEffect(() => {
+    setOpenAll(false);
+    setOpenAllButtonState(false);
+  }, [activeMenuDepth?.[0]?.title]);
+
+  useEffect(() => {
+    if (!activeMenuDepth?.[0]?.children) {
+      return;
+    }
+    setMenus(activeMenuDepth?.[0]?.children);
+  }, [activeMenuDepth?.[0]?.children]);
+
   if (!activeMenuDepth?.[0]) {
     return <></>;
   }
+
+  const handleOpenAll = () => {
+    setOpenAllButtonState(!openAllButtonState);
+    setOpenAll(!openAllButtonState);
+  };
+
+  const handleOpenStateAll = (openAll: boolean) => {
+    // 버튼 상태가 닫혀 있는 상태에서 모든 메뉴가 열린 경우 버튼 상태를 열린 상태로 변경(모두 닫기 활성화)
+    if (openAllButtonState === false && openAll) {
+      setOpenAllButtonState(true);
+      setOpenAll(undefined);
+    }
+    // 버튼 상태가 열려 있는 상태에서 모든 메뉴가 닫힌 경우 버튼 상태를 닫힌 상태로 변경(모두 열기 활성화)
+    if (openAllButtonState === true && !openAll) {
+      setOpenAllButtonState(false);
+      setOpenAll(undefined);
+    }
+  };
 
   return (
     <div className={cn(styles.start, 'nlp--lnb')}>
       <div className={styles.lnb_wrap}>
         <h2 className={styles.lnb_title}>
-          <span>{activeMenuDepth[0].title}</span>
+          <button
+            type="button"
+            className={cn(styles.lnb_title_btn, buttonClass)}
+            onClick={() => handleOpenAll()}>
+            <span className={styles.lnb_title_text}>{activeMenuDepth[0].title}</span>
+            <IcoArrowDown width={16} height={16} stroke="#131C30" />
+          </button>
         </h2>
-        {activeMenuDepth[0]?.children && (
+        {menus && (
           <AccordionMenu
-            menus={activeMenuDepth[0]?.children}
+            menus={menus}
             depth={2}
             className={styles._depth2}
+            openAll={openAll}
+            onOpenStateAll={(e) => handleOpenStateAll(e as boolean)}
           />
         )}
       </div>
--- libs/ui/src/lib/accordion/accordion.tsx
+++ libs/ui/src/lib/accordion/accordion.tsx
@@ -1,51 +1,93 @@
 import React, { forwardRef } from 'react';
 import * as Primitive from '@radix-ui/react-accordion';
 import { IcoArrowDown } from '@learnway/icons';
+import { isArray, map } from 'lodash';
+import { useCreation } from 'ahooks';
 
 import { cn } from '@learnway/shared';
 
 import styles from './accordion.module.scss';
 
+export type accordionType = 'single' | 'multiple';
 export interface AccordionItem {
-  key: string;
+  value: string;
   title: string | React.ReactNode;
   children?: React.ReactNode;
 }
 
 export interface AccordionComponentProps {
+  type: accordionType;
   items: AccordionItem[];
   className?: string;
-  value?: string;
+  value?: string | string[];
+  defaultValue?: string | string[];
   onValueChange?: (value: string | string[]) => void;
 }
 
 const AccordionComponent = forwardRef<
   React.ElementRef<typeof Primitive.Accordion>,
   AccordionComponentProps
->(({ items, className, value, onValueChange }: AccordionComponentProps, ref) => {
-  return (
-    <Primitive.Root
-      type="single"
-      collapsible
-      className={cn(styles.start, className, 'nlp--accordion', 'w-full')}
-      value={value}
-      onValueChange={onValueChange}
-      ref={ref}>
-      {items.map((item: AccordionItem) => {
-        return (
-          <Primitive.Item value={item.key} className={cn(styles.accordion_item)} key={item.key}>
-            <Primitive.Trigger className={cn(styles.accordion_btn)}>
-              <span className="accordion_title">{item.title}</span>
-              {item.children && <IcoArrowDown width={16} height={16} stroke="#131C30" />}
-            </Primitive.Trigger>
-            <Primitive.Content className={cn(styles.accordion_content)}>
-              {item.children}
-            </Primitive.Content>
-          </Primitive.Item>
-        );
-      })}
-    </Primitive.Root>
-  );
-});
+>(
+  (
+    {
+      items,
+      className,
+      onValueChange,
+      value,
+      type = 'single',
+      defaultValue,
+    }: AccordionComponentProps,
+    ref,
+  ) => {
+    //Prop exception value
+    const editionValue: string | string[] = useCreation(() => {
+      if (!value) {
+        return type === 'single' ? '' : [];
+      }
+      return type === 'single'
+        ? ((isArray(value) && value?.length ? value[0] : value) as string)
+        : ((isArray(value) ? value : [value]) as string[]);
+    }, [type, value]);
+
+    //Prop exception default value
+    const editionDefaultValue: string | string[] = useCreation(() => {
+      if (!defaultValue) {
+        return type === 'single' ? '' : [];
+      }
+      return type === 'single'
+        ? ((isArray(defaultValue) && defaultValue?.length
+            ? defaultValue[0]
+            : defaultValue) as string)
+        : ((isArray(defaultValue) ? defaultValue : [defaultValue]) as string[]);
+    }, [type, defaultValue]);
+
+    return (
+      <Primitive.Root
+        type={type}
+        className={cn(styles.start, className, 'nlp--accordion', 'w-full')}
+        value={editionValue as any}
+        defaultValue={editionDefaultValue as any}
+        onValueChange={onValueChange}
+        ref={ref}>
+        {items.map((item: AccordionItem) => {
+          return (
+            <Primitive.Item
+              value={item.value}
+              className={cn(styles.accordion_item)}
+              key={item.value}>
+              <Primitive.Trigger className={cn(styles.accordion_btn)}>
+                <span className="accordion_title">{item.title}</span>
+                {item.children && <IcoArrowDown width={16} height={16} stroke="#131C30" />}
+              </Primitive.Trigger>
+              <Primitive.Content className={cn(styles.accordion_content)}>
+                {item.children}
+              </Primitive.Content>
+            </Primitive.Item>
+          );
+        })}
+      </Primitive.Root>
+    );
+  },
+);
 
 export const Accordion = AccordionComponent;
