10ea417c5714eafb5c49cb37ff6f7775e3f403fe
HAE\9488416 <9488416@ict-companion.com>
2025-01-24 18:26:34 +0900

    [퍼블] 전체레이아웃 진행중

--- apps/pub/fo/src/widgets/layout/ui/footer/footer.tsx
+++ apps/pub/fo/src/widgets/layout/ui/footer/footer.tsx
@@ -1,12 +1,13 @@
 import { memo } from 'react';
 import { Link } from '@tanstack/react-router';
 import styles from './footer.module.css';
+import logoImage from '../../../../assets/images/logo_footer.png';
 
 function FooterComponent() {
   return (
     <div className={`${styles.start} ${styles.footer}`}>
       <div className={styles.footer_area}>
-        <div className={styles.footer_menu}>
+        {/* <div className={styles.footer_menu}>
           <h4 className="">교육제도</h4>
           <ul className={styles.menu_list}>
             <li>
@@ -16,9 +17,46 @@ function FooterComponent() {
               <Link to={'/'}>SPA 승진제도</Link>
             </li>
           </ul>
-        </div>
+        </div> */}
 
-        <div className={styles.footer_info}></div>
+        <div className={styles.footer_info}>
+          <span className={styles.logo}>
+            <img src={logoImage} alt="Logo" />
+          </span>
+
+          <div className={styles.info_area}>
+            <div className={styles.info_menu}>
+              <ul>
+                <li>
+                  <Link to={''}>이용약관</Link>
+                </li>
+                <li>
+                  <Link to={''} className={styles.active}>
+                    개인정보 처리방침
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className={styles.info_box}>
+              <span className={styles.info}>
+                <span className={styles.space}>사업자등록번호 : 123-12-12345</span>{' '}
+                <span className={styles.space}>통신판매업신고번호 : 제1234-서울강남-1234호</span>{' '}
+                <span className={styles.space}>대표이사 : 홍길동</span>
+              </span>
+              <address className={styles.address}>
+                <span className={styles.space}>주소 : 서울시 강남구 테헤란로 510</span>{' '}
+                <span className={styles.space}>호스팅 서비스 제공 : 현대오토에버(주)</span>{' '}
+                <span className={styles.space}>
+                  고객센터 : <span className={styles.tel}>080-600-6000</span>
+                </span>
+              </address>
+              <div className={styles.copyright}>
+                Copyright © 2023 Hyundai-Autoever. All rights reserved.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
