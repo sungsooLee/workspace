fa367bc4f7e7746c44911cb2be4b1d8d5a954cfa
HAE\9488416 <9488416@ict-companion.com>
2025-01-24 10:02:05 +0900

    [퍼블 FO] 전체레이아웃

--- apps/pub/fo/src/pages/_layout/index.tsx
+++ apps/pub/fo/src/pages/_layout/index.tsx
@@ -3,6 +3,7 @@ import { createFileRoute } from '@tanstack/react-router';
 import { useTranslation } from 'react-i18next';
 
 import { Button } from '@learnway/ui';
+import styles from './index.module.css';
 
 export const Route = createFileRoute('/_layout/')({
   component: HomeComponent,
@@ -11,106 +12,5 @@ export const Route = createFileRoute('/_layout/')({
 function HomeComponent() {
   const { t, i18n } = useTranslation();
 
-  return (
-    <div className="flex flex-col gap-10 p-2">
-      <h3>FO 퍼블 가이드 어플리케이션 입니다.</h3>
-      {/**primary */}
-      <div className="flex w-10 gap-5">
-        <div className="min-w-32">Primary lg~xs</div>
-
-        <Button size="lg">작성완료</Button>
-
-        <Button>작성완료</Button>
-
-        <Button size="sm">작성완료</Button>
-
-        <Button size="xs">작성완료</Button>
-      </div>
-      <div className="flex w-10 gap-5">
-        <div className="min-w-32">Primary (disabled)</div>
-
-        <Button size="lg" disabled>
-          작성완료
-        </Button>
-
-        <Button disabled>작성완료</Button>
-
-        <Button size="sm" disabled>
-          작성완료
-        </Button>
-
-        <Button size="xs" disabled>
-          작성완료
-        </Button>
-      </div>
-      {/**outline */}
-      <div className="flex gap-5">
-        <div className="min-w-32">Outline lg~xs</div>
-        <Button size="lg" variant={'outline'}>
-          작성완료
-        </Button>
-        <Button size="md-1" variant={'outline'}>
-          작성완료
-        </Button>
-
-        <Button size="sm" variant={'outline'}>
-          작성완료
-        </Button>
-        <Button size="xs" variant={'outline'}>
-          작성완료
-        </Button>
-      </div>
-      <div className="flex gap-5">
-        <div className="min-w-32">Outline (disabled)</div>
-        <Button size="lg" variant={'outline'} disabled>
-          작성완료
-        </Button>
-        <Button size="md-1" variant={'outline'} disabled>
-          작성완료
-        </Button>
-
-        <Button size="sm" variant={'outline'} disabled>
-          작성완료
-        </Button>
-        <Button size="xs" variant={'outline'} disabled>
-          작성완료
-        </Button>
-      </div>
-      {/**gray-outline */}
-      <div className="flex gap-5">
-        <div className="min-w-32">gray lg~xs</div>
-        <Button size="lg" variant={'gray-outline'}>
-          작성완료
-        </Button>
-
-        <Button variant={'gray-outline'}>작성완료</Button>
-
-        <Button variant={'gray-outline'} size="sm">
-          작성완료
-        </Button>
-
-        <Button variant={'gray-outline'} size="xs">
-          작성완료
-        </Button>
-      </div>
-      <div className="flex gap-5">
-        <div className="min-w-32">gray (disabled)</div>
-        <Button size="lg" variant={'gray-outline'} disabled>
-          작성완료
-        </Button>
-
-        <Button variant={'gray-outline'} disabled>
-          작성완료
-        </Button>
-
-        <Button variant={'gray-outline'} size="sm" disabled>
-          작성완료
-        </Button>
-
-        <Button variant={'gray-outline'} size="xs" disabled>
-          작성완료
-        </Button>
-      </div>
-    </div>
-  );
+  return <div className={`${styles.start} ${styles.main}`}>컨텐츠 영역(첫페이지)</div>;
 }
