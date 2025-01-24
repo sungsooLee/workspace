4e06aa94910123f469adaf5b77a3b4ee73bf8990
최원석/L&D플랫폼팀 <1seok@hyundai-autoever.com>
2025-01-24 11:18:10 +0900

    merge: [HMGSLP-217] merge main branch

--- apps/fo/src/features/layout/ui/language.tsx
+++ apps/fo/src/features/layout/ui/language.tsx
@@ -1,6 +1,5 @@
 import { useTranslation } from 'react-i18next';
 import { useCodesByCodeGroup, type Code } from '../../../entities/system';
-import { useSetLanguage } from '../../system';
 import { useFetchAuthUser } from '../../../entities/user';
 import { CODE_GROUP } from '@learnway/config';
 import { Popover, Avatar } from '@learnway/ui';
@@ -8,8 +7,6 @@ import { memo } from 'react';
 
 const PopoverContent = ({ data }: { data?: Code[] }) => {
   const { t } = useTranslation();
-
-  const { set: setLanguage } = useSetLanguage();
 
   const handleLang = (lang: string) => {
     setLanguage(lang);
