7b294ae3528c4559a3bfda9f4974aa21e7e29d95
최원석/L&D플랫폼팀 <1seok@hyundai-autoever.com>
2025-02-04 14:15:23 +0900

    chore: [HMGSLP-216] pub fo tailwind config copy

--- apps/fo/tailwind.config.js
+++ apps/fo/tailwind.config.js
@@ -24,6 +24,27 @@ module.exports = {
       };
 
       addComponents({
+        '.scrollbar': {
+          '&::-webkit-scrollbar': {
+            height: '64px',
+            width: '12px',
+            backgroundColor: '#fff',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            borderRadius: '9999px',
+            borderWidth: '4px',
+            borderStyle: 'solid',
+            borderColor: 'transparent',
+            backgroundColor: 'var(--gray4)',
+            backgroundClip: 'padding-box',
+          },
+          '&::-webkit-scrollbar-track': {
+            backgroundColor: '#fff',
+          },
+          '.scroll-smooth': {
+            scrollBehavior: 'smooth',
+          },
+        },
         // title class add
         '.title_4_b': {
           ...baseTitle,
