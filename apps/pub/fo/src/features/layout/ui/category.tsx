e6d59af389b8a3826392d58423774fe59715c2ee
HAE\9488416 <9488416@ict-companion.com>
2025-02-04 10:10:15 +0900

    [퍼블]FO카테고리

--- apps/pub/fo/src/features/layout/ui/category.tsx
+++ apps/pub/fo/src/features/layout/ui/category.tsx
@@ -8,10 +8,35 @@ import bnrImage1 from '../../../assets/images/banner/banner_cate1.png';
 import bnrImage2 from '../../../assets/images/banner/banner_cate2.png';
 
 const CategoryCompoment = () => {
+  const categories = [
+    {
+      title: '경영전략',
+      link: '',
+      subCategories: ['3dpth Category'],
+    },
+    {
+      title: '마케팅 전략',
+      link: '',
+      subCategories: ['Market Analysis', 'SEO Planning'],
+    },
+  ];
+
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const toggleCategory = (index) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
   const [isActive, setIsActive] = useState(false);
 
-  const toggleCategory = () => {
+  const openCategory = () => {
     setIsActive(!isActive);
+  };
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAll = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
@@ -19,7 +44,7 @@ const CategoryCompoment = () => {
       <Button
         onlyIcon
         className={`${styles.btn_category} ${isActive ? styles.active : ''}`}
-        onClick={toggleCategory}>
+        onClick={openCategory}>
         {isActive ? (
           <IcoXclose width={24} height={24} stroke="#ffffff" />
         ) : (
@@ -174,7 +199,39 @@ const CategoryCompoment = () => {
           </div>
 
           {/* 카테고리 영역 - 뎁스영역 */}
-          <div className={styles.category_inner}></div>
+          <div className={styles.category_inner}>
+            <div className={styles.tit_head}>
+              <h2>기업경영</h2>
+              <Button className={styles.btn_cate} onClick={toggleAll}>
+                <IcoArrowForward width={16} height={16} stroke="#07287E" />
+              </Button>
+            </div>
+
+            {/* 3dapth */}
+            {categories.map((category, index) => (
+              <div key={index} className={styles.depth_wrap}>
+                <div className={styles.tit}>
+                  <h3>{category.title}</h3>
+                  <Button className={styles.btn_cate} onClick={() => toggleCategory(index)}>
+                    <IcoArrowForward width={16} height={16} stroke="#07287E" />
+                  </Button>
+                </div>
+
+                {/* openIndex 상태에 따라 열리고 닫힘 */}
+                {openIndex === index && (
+                  <div className={styles.depth_info}>
+                    <ul>
+                      {category.subCategories.map((sub, subIndex) => (
+                        <li key={subIndex}>
+                          <Link to={category.link}>{sub}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
