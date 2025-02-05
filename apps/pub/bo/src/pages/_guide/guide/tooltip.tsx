ddd073070fb7656db997108961cc6290ce627772
HAE\9490817 <lsstmi@naver.com>
2025-02-05 12:36:33 +0900

    [퍼블] bo 작업중

--- apps/pub/bo/src/pages/_guide/guide/tooltip.tsx
+++ apps/pub/bo/src/pages/_guide/guide/tooltip.tsx
@@ -6,17 +6,17 @@ export const Route = createFileRoute('/_guide/guide/tooltip')({
 });
 
 function RouteComponent() {
-  const Content = () => (
-    <div className="w-80">
-      <h3 className="bg-green-50">Content Header</h3>
-      <h4 className="bg-gray-3 h-20">Content Body</h4>
-    </div>
-  );
+  //   const Content = () => (
+  //     <div className="w-80">
+  //       <h3 className="bg-green-50">Content Header</h3>
+  //       <h4 className="bg-gray-3 h-20">Content Body</h4>
+  //     </div>
+  //   );
   return (
     <div>
-      <h2 className="guide_tit2">Confirm, Alert Component Guide</h2>
-      <Tooltip content={<Content />}>
-        <Button variant="text">Open Tooltip</Button>
+      <h2 className="guide_tit2">Tootip Component Guide</h2>
+      <Tooltip content={'tooltip content'}>
+        <Button>Button</Button>
       </Tooltip>
     </div>
   );
--- libs/ui/src/lib/tooltip/tooltip.tsx
+++ libs/ui/src/lib/tooltip/tooltip.tsx
@@ -1,27 +1,57 @@
-import React, { forwardRef, memo, ReactNode } from 'react';
+import React, { forwardRef, memo, ReactNode, useState } from 'react';
 
 import { cn } from '@learnway/shared';
 
-import * as Primitive from "@radix-ui/react-tooltip";
+import * as Primitive from '@radix-ui/react-tooltip';
 
 import styles from './tooltip.module.css';
 
 interface TooltipComponentProps extends Primitive.TooltipProps {
   children: ReactNode;
   content: ReactNode | string;
+  side?: 'top' | 'right' | 'bottom' | 'left';
+  align?: 'start' | 'center' | 'end';
+  isClickAble?: boolean; // 클릭일 경우 케이스 추가
   className?: string;
+  sideOffset?: number;
+  avoidCollisions?: boolean;
 }
 
 const TooltipComponent = forwardRef<React.ElementRef<typeof Primitive.Root>, TooltipComponentProps>(
-  ({ children, className, content, ...props }, ref) => {
+  (
+    {
+      children,
+      className,
+      content,
+      side = 'top',
+      align = 'end',
+      isClickAble = false,
+      sideOffset = 5,
+      ...props
+    },
+    ref,
+  ) => {
+    const [isOpen, setIsOpen] = useState(false); // 클릭 시 툴팁 열기/닫기 상태 관리
+
+    const toggleTooltip = () => {
+      if (isClickAble) {
+        setIsOpen((prev) => !prev); // 클릭 시 툴팁 열고 닫기
+      }
+    };
     return (
       <Primitive.Provider>
-        <Primitive.Root>
-          <Primitive.Trigger asChild>
-            {children}
+        <Primitive.Root open={isOpen} onOpenChange={setIsOpen}>
+          <Primitive.Trigger className={styles.tooltip_btn} asChild>
+            <div className={styles.tooltip_item} onClick={toggleTooltip}>
+              {children}
+            </div>
           </Primitive.Trigger>
           <Primitive.Portal>
-            <Primitive.Content className={styles.start} sideOffset={5}>
+            <Primitive.Content
+              side={side}
+              align={align}
+              className={cn(styles.start, styles.tooltip_content)}
+              sideOffset={5}>
               {content}
             </Primitive.Content>
           </Primitive.Portal>
