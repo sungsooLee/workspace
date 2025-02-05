abde139f9fc01ed2b2276d5a64565d757b6be173
HAE\9490817 <lsstmi@naver.com>
2025-02-05 09:53:19 +0900

    [퍼블] bo 라디오, 체크박스 수정

--- apps/pub/bo/src/pages/_guide/guide/radio.tsx
+++ apps/pub/bo/src/pages/_guide/guide/radio.tsx
@@ -9,13 +9,22 @@ function RouteComponent() {
   return (
     <div>
       <h2 className="guide_tit2">Checkbox Component Guide</h2>
-      <h3 className="guide_tit3">default</h3>
+      <h3 className="guide_tit3">Basic</h3>
       <Radio
         options={[
           { value: 'type1', label: 'aaaaa' },
           { value: 'type2', label: 'bbbbb' },
           { value: 'type3', label: 'ccccc' },
         ]}
+      />
+      <h3 className="guide_tit3">disabled</h3>
+      <Radio
+        options={[
+          { value: 'type4', label: 'ddd' },
+          { value: 'type5', label: 'eeee' },
+          { value: 'type6', label: 'ffff' },
+        ]}
+        disabled
       />
     </div>
   );
