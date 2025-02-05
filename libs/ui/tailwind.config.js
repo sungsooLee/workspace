347e3c13f7c8744342d90a17ef13ce5c38c44790
9480335 <9480335@ict-companion.com>
2025-02-05 14:29:04 +0900

    feat: [HMGSLP-222] Guide add tailwind plugin

--- libs/ui/tailwind.config.js
+++ libs/ui/tailwind.config.js
@@ -3,53 +3,11 @@ const { join } = require('path');
 const tailwindPreset = require('../../libs/config/src/lib/style/tailwind.preset');
 const tailwindShadcnPreset = require('../../libs/config/src/lib/style/tailwind.shadcn.preset');
 
-const plugin = require('tailwindcss/plugin');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [tailwindShadcnPreset, tailwindPreset],
   content: [
     join(__dirname, '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html,js}'),
     ...createGlobPatternsForDependencies(__dirname),
-  ],
-  plugins: [
-    function ({ addUtilities }) {
-      addUtilities({
-        '.no-spinner': {
-          '&::-webkit-outer-spin-button': {
-            '-webkit-appearance': 'none',
-            margin: '0',
-          },
-          '&::-webkit-inner-spin-button': {
-            '-webkit-appearance': 'none',
-            margin: '0',
-          },
-          '&[type="number"]': {
-            '-moz-appearance': 'textfield',
-          },
-        },
-        '.scrollbar': {
-          '&::-webkit-scrollbar': {
-            height: '64px',
-            width: '12px',
-            backgroundColor: '#fff',
-          },
-          '&::-webkit-scrollbar-thumb': {
-            borderRadius: '9999px',
-            borderWidth: '4px',
-            borderStyle: 'solid',
-            borderColor: 'transparent',
-            backgroundColor: 'var(--gray4)',
-            backgroundClip: 'padding-box',
-          },
-          '&::-webkit-scrollbar-track': {
-            backgroundColor: '#fff',
-          },
-          '.scroll-smooth': {
-            scrollBehavior: 'smooth',
-          },
-        },
-      });
-    },
   ],
 };
