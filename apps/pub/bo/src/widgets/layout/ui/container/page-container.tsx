a2f5ebdfb440211ac2be12a53b03c82b406e279f
이성수/협력사 <9490817@ict-companion.com>
2025-02-07 13:04:42 +0900

    [퍼블] bo form 작업중

--- apps/pub/bo/src/widgets/layout/ui/container/page-container.tsx
+++ apps/pub/bo/src/widgets/layout/ui/container/page-container.tsx
@@ -1,6 +1,7 @@
 import { memo, ReactNode } from 'react';
 // import { useTranslation } from 'react-i18next';
 import { Breadcrumbs } from './breadcrumbs/breadcrumbs';
+import { Button } from '@learnway/ui';
 import styles from './page-container.module.css';
 
 interface PageContainerComponentProps {
@@ -13,7 +14,34 @@ function PageContainerComponent({ children }: PageContainerComponentProps) {
   return (
     <div className={`${styles.start} ${styles.contents}`}>
       <Breadcrumbs />
-      <div className={styles.inner}>{children}</div>
+      <div className={styles.inner}>
+        {/* title_wrap */}
+        <div className={styles.title_wrap}>
+          <h3 className={styles.title}>동영상 상세</h3>
+          <div className={styles.btn_wrap}>
+            <Button variant="point" size="sm">
+              매핑과정 보기
+            </Button>
+            <Button variant="point" size="sm">
+              공유이력 보기
+            </Button>
+            <Button variant="point" size="sm">
+              삭제
+            </Button>
+            <Button variant="point" size="sm">
+              수정
+            </Button>
+            <Button variant="primary" size="sm">
+              목록
+            </Button>
+          </div>
+        </div>
+        {/* contents_wrap */}
+        <div className={styles.contents_wrap}>
+          {/* contents */}
+          <div className={styles.contents}>{children}</div>
+        </div>
+      </div>
     </div>
   );
 }
