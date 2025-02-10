46a23c8d96d580122c25a50a74bf2f81af5dd4ca
최원석/L&D플랫폼팀 <1seok@hyundai-autoever.com>
2025-02-10 11:32:17 +0900

    style: [HMGSLP-216] fo 공통 레이아웃 구역 퍼블 작업

--- apps/fo/src/widgets/layout/ui/layout/layout.tsx
+++ apps/fo/src/widgets/layout/ui/layout/layout.tsx
@@ -5,6 +5,8 @@ import { useActiveMenuDepthState } from '../../../../features/layout';
 import { PageContainer } from '../container/page-container';
 import styles from './layout.module.css';
 import { Breadcrumbs } from '../container/breadcrumbs/breadcrumbs';
+import { useShowBreadcrumbs } from '../../service/breadcurmb.hooks';
+import { useParams, useRouter } from '@tanstack/react-router';
 
 interface LayoutComponentProps {
   children: ReactNode;
@@ -15,13 +17,23 @@ function LayoutComponent({ children }: LayoutComponentProps) {
 
   const [activeMenuDepth] = useActiveMenuDepthState();
 
+  const shouldShowBreadcrumbs = useShowBreadcrumbs();
+  // const { categoryId } = useParams({ from: '/_layout/category/$categoryId' });
+  const router = useRouter();
+  const { categoryId } = useParams({ strict: false });
+
+  const currentCategoryId = categoryId ? Number(categoryId) : null;
+
   return (
     <div className={`${styles.start} ${styles.container}`}>
-      {/* <div className={styles.breadcrums}>
-        <div className={styles.inner}>
-          <Breadcrumbs />
+      {shouldShowBreadcrumbs && (
+        <div className={styles.breadcrums}>
+          <div className={styles.inner}>
+            <Breadcrumbs currentCategoryId={currentCategoryId} />
+          </div>
         </div>
-      </div> */}
+      )}
+
       <div className={styles.inner}>
         <main>
           <PageContainer>{children}</PageContainer>
