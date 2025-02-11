2fede5ff291eb9d93cbb7c6d0c334318328cac39
김태경/협력사 <9488416@ict-companion.com>
2025-02-11 09:50:27 +0900

    [퍼블]FO로그인

--- apps/pub/fo/src/widgets/layout/ui/footer/footer-auth.tsx
+++ apps/pub/fo/src/widgets/layout/ui/footer/footer-auth.tsx
@@ -2,11 +2,25 @@ import { memo } from 'react';
 import { Link } from '@tanstack/react-router';
 import { useTranslation } from 'react-i18next';
 import styles from './footer-auth.module.css';
+import { Copyright } from 'lucide-react';
 
 function FooterAuthComponent() {
   return (
     <div className={`${styles.start} ${styles.footer_auth}`}>
-      <div className={styles.footer_area}>footer</div>
+      <div className={styles.footer_area}>
+        <ul className={styles.menu_list}>
+          <li>
+            <Link to={''}>이용약관</Link>
+          </li>
+          <li>
+            <Link to={''}>개인정보처리 방침</Link>
+          </li>
+          <li>
+            <Link to={''}>고객지원</Link>
+          </li>
+        </ul>
+        <div className={styles.copy}>copyright © 2023 Hyundai-Autoever. All rights reserved. </div>
+      </div>
     </div>
   );
 }
