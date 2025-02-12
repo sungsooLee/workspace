8a518d94f8e50cfc62fbd3c2010298e9a099c2b2
김태경/협력사 <9488416@ict-companion.com>
2025-02-12 17:09:34 +0900

    [퍼블]퍼블가이드

--- apps/pub/bo/src/pages/_guide/guide/buttons.tsx
+++ apps/pub/bo/src/pages/_guide/guide/buttons.tsx
@@ -26,153 +26,149 @@ import { Button } from '@learnway/ui';
         </div>
 
         <h3 className="guide_tit3">Primary Button</h3>
-        <div className="flex_box">
-          <div className="desc">
-            <Button variant="primary" size="xs">
-              버튼
-            </Button>
-            <Button variant="primary" size="sm">
-              버튼
-            </Button>
-            <Button variant="primary" size="md">
-              버튼
-            </Button>
-            <Button variant="primary" size="lg">
-              버튼
-            </Button>
+        <div className="group">
+          <div className="flex_box">
+            <div className="desc">
+              <Button variant="primary" size="xs">
+                버튼
+              </Button>
+              <Button variant="primary" size="sm">
+                버튼
+              </Button>
+              <Button variant="primary" size="md">
+                버튼
+              </Button>
+              <Button variant="primary" size="lg">
+                버튼
+              </Button>
+            </div>
           </div>
-        </div>
 
-        <div className="code_example">
-          <pre className="code_block">
-            <code>
-              {`<Button variant="primary" size="xs">버튼</Button>
+          <div className="code_example">
+            <pre className="code_block">
+              <code>
+                {`<Button variant="primary" size="xs">버튼</Button>
 <Button variant="primary" size="sm">버튼</Button>
 <Button variant="primary" size="md">버튼</Button>
 <Button variant="primary" size="lg">버튼</Button>`}
-            </code>
-          </pre>
-        </div>
-      </div>
-      <div>
-        <h3 className="guide_tit3">line Button</h3>
-        <div className="flex_box">
-          <div className="desc">
-            <Button variant="line" size="sm" disabled>
-              버튼
-            </Button>
-            <Button variant="line" size="lg">
-              버튼
-            </Button>
+              </code>
+            </pre>
           </div>
-        </div>
 
-        <div className="code_example">
-          <pre className="code_block">
-            <code>
-              {`<Button variant="line" size="sm" disabled>버튼</Button>
-<Button variant="line" size="lg">버튼</Button>`}
-            </code>
-          </pre>
-        </div>
-      </div>
-      <div>
-        <h3 className="guide_tit3">gray Button</h3>
-        <div className="flex_box">
-          <div className="desc">
-            <Button variant="gray" size="sm" disabled>
-              버튼
-            </Button>
-            <Button variant="gray" size="md">
-              버튼
-            </Button>
-            <Button variant="gray" size="lg">
-              버튼
-            </Button>
+          <h3 className="guide_tit3">line Button</h3>
+          <div className="flex_box">
+            <div className="desc">
+              <Button variant="line" size="sm" disabled>
+                버튼
+              </Button>
+              <Button variant="line" size="lg">
+                버튼
+              </Button>
+            </div>
           </div>
-        </div>
-        <div className="code_example">
-          <pre className="code_block">
-            <code>
-              {`<Button variant="gray" size="sm" disabled>버튼</Button>
+
+          <div className="code_example">
+            <pre className="code_block">
+              <code>
+                {`<Button variant="line" size="sm" disabled>버튼</Button>
+<Button variant="line" size="lg">버튼</Button>`}
+              </code>
+            </pre>
+          </div>
+
+          <h3 className="guide_tit3">gray Button</h3>
+          <div className="flex_box">
+            <div className="desc">
+              <Button variant="gray" size="sm" disabled>
+                버튼
+              </Button>
+              <Button variant="gray" size="md">
+                버튼
+              </Button>
+              <Button variant="gray" size="lg">
+                버튼
+              </Button>
+            </div>
+          </div>
+          <div className="code_example">
+            <pre className="code_block">
+              <code>
+                {`<Button variant="gray" size="sm" disabled>버튼</Button>
 <Button variant="gray" size="md">버튼</Button>
 <Button variant="gray" size="lg">버튼</Button>`}
-            </code>
-          </pre>
-        </div>
-      </div>
-      <div>
-        <h3 className="guide_tit3">gray2 Button</h3>
-        <div className="flex_box">
-          <div className="desc">
-            <Button variant="gray2" size="sm" disabled>
-              버튼
-            </Button>
-            <Button variant="gray2" size="xs">
-              버튼
-            </Button>
-            <Button variant="gray2" size="xs" iconAlign="left" disabled>
-              <IcoDownload width={16} height={16} />
-              엑셀다운로드
-            </Button>
+              </code>
+            </pre>
           </div>
-        </div>
-        <div className="code_example">
-          <pre className="code_block">
-            <code>
-              {`<Button variant="gray2" size="sm" disabled>버튼</Button>
+
+          <h3 className="guide_tit3">gray2 Button</h3>
+          <div className="flex_box">
+            <div className="desc">
+              <Button variant="gray2" size="sm" disabled>
+                버튼
+              </Button>
+              <Button variant="gray2" size="xs">
+                버튼
+              </Button>
+              <Button variant="gray2" size="xs" iconAlign="left" disabled>
+                <IcoDownload width={16} height={16} />
+                엑셀다운로드
+              </Button>
+            </div>
+          </div>
+          <div className="code_example">
+            <pre className="code_block">
+              <code>
+                {`<Button variant="gray2" size="sm" disabled>버튼</Button>
 <Button variant="gray2" size="xs">버튼</Button>
 <Button variant="gray2" size="xs" iconAlign="left" disabled>
   <IcoDownload width={16} height={16} /> 엑셀다운로드
 </Button>`}
-            </code>
-          </pre>
-        </div>
-      </div>
-      <div>
-        <h3 className="guide_tit3">search Button</h3>
-        <div className="flex_box">
-          <div className="desc">
-            <Button variant="search" size="sm" iconAlign="left">
-              <IcoSearch width={16} height={16} stroke="#131C30" /> 검색
-            </Button>
-            <Button variant="search" size="sm">
-              버튼
-            </Button>
+              </code>
+            </pre>
           </div>
-        </div>
-        <div className="code_example">
-          <pre className="code_block">
-            <code>
-              {`<Button variant="search" size="sm" iconAlign="left">
+
+          <h3 className="guide_tit3">search Button</h3>
+          <div className="flex_box">
+            <div className="desc">
+              <Button variant="search" size="sm" iconAlign="left">
+                <IcoSearch width={16} height={16} stroke="#131C30" /> 검색
+              </Button>
+              <Button variant="search" size="sm">
+                버튼
+              </Button>
+            </div>
+          </div>
+          <div className="code_example">
+            <pre className="code_block">
+              <code>
+                {`<Button variant="search" size="sm" iconAlign="left">
     <IcoSearch width={16} height={16} stroke="#131C30" />검색
 </Button>
 
 <Button variant="search" size="sm">버튼</Button>`}
-            </code>
-          </pre>
-        </div>
-      </div>
-      <div>
-        <h3 className="guide_tit3">expand Button</h3>
-        <div className="flex_box">
-          <div className="desc">
-            <Button variant="expand" size="sm" onlyIcon disabled>
-              <IcoPlus width={16} height={16} stroke="#131C30" />
-            </Button>
-            <Button variant="expand" size="sm" onlyIcon>
-              <IcoPlus width={16} height={16} stroke="#131C30" /> 확장
-            </Button>
-            <Button variant="expand" size="xs" onlyIcon>
-              <IcoPlus width={16} height={16} stroke="#131C30" />
-            </Button>
+              </code>
+            </pre>
           </div>
-        </div>
 
-        <div className="code_example">
-          <pre className="code_block">
-            <code>
-              {`<Button variant="expand" size="sm" onlyIcon disabled>
+          <h3 className="guide_tit3">expand Button</h3>
+          <div className="flex_box">
+            <div className="desc">
+              <Button variant="expand" size="sm" onlyIcon disabled>
+                <IcoPlus width={16} height={16} stroke="#131C30" />
+              </Button>
+              <Button variant="expand" size="sm" onlyIcon>
+                <IcoPlus width={16} height={16} stroke="#131C30" /> 확장
+              </Button>
+              <Button variant="expand" size="xs" onlyIcon>
+                <IcoPlus width={16} height={16} stroke="#131C30" />
+              </Button>
+            </div>
+          </div>
+
+          <div className="code_example">
+            <pre className="code_block">
+              <code>
+                {`<Button variant="expand" size="sm" onlyIcon disabled>
     <IcoPlus width={16} height={16} stroke="#131C30" />
 </Button>
 
@@ -183,124 +179,120 @@ import { Button } from '@learnway/ui';
 <Button variant="expand" size="xs" onlyIcon>
     <IcoPlus width={16} height={16} stroke="#131C30" />
 </Button>`}
-            </code>
-          </pre>
-        </div>
-      </div>
-      <div>
-        <h3 className="guide_tit3">expand2 Button</h3>
-        <div className="flex_box">
-          <div className="desc">
-            <Button variant="expand2" size="sm" iconAlign="left">
-              <IcoPlus width={16} height={16} stroke="#131C30" /> 확장
-            </Button>
+              </code>
+            </pre>
           </div>
-        </div>
-        <div className="code_example">
-          <pre className="code_block">
-            <code>
-              {`<Button variant="expand2" size="sm" iconAlign="left">
+
+          <h3 className="guide_tit3">expand2 Button</h3>
+          <div className="flex_box">
+            <div className="desc">
+              <Button variant="expand2" size="sm" iconAlign="left">
+                <IcoPlus width={16} height={16} stroke="#131C30" /> 확장
+              </Button>
+            </div>
+          </div>
+          <div className="code_example">
+            <pre className="code_block">
+              <code>
+                {`<Button variant="expand2" size="sm" iconAlign="left">
     <IcoPlus width={16} height={16} stroke="#131C30" /> 확장
 </Button>`}
-            </code>
-          </pre>
-        </div>
-      </div>
-      <div>
-        <h3 className="guide_tit3">save Button</h3>
-        <div className="flex_box">
-          <div className="desc">
-            <Button variant="save" size="sm">
-              저장
-            </Button>
-            <Button variant="save" size="sm" disabled>
-              저장
-            </Button>
+              </code>
+            </pre>
           </div>
-        </div>
-        <div className="code_example">
-          <pre className="code_block">
-            <code>
-              {`<Button variant="save" size="sm">저장</Button>
+
+          <h3 className="guide_tit3">save Button</h3>
+          <div className="flex_box">
+            <div className="desc">
+              <Button variant="save" size="sm">
+                저장
+              </Button>
+              <Button variant="save" size="sm" disabled>
+                저장
+              </Button>
+            </div>
+          </div>
+          <div className="code_example">
+            <pre className="code_block">
+              <code>
+                {`<Button variant="save" size="sm">저장</Button>
 <Button variant="save" size="sm" disabled>저장</Button>`}
-            </code>
-          </pre>
-        </div>
-      </div>
-      <div>
-        <h3 className="guide_tit3">point Button</h3>
-        <div className="flex_box">
-          <div className="desc">
-            <Button variant="point" size="sm">
-              포인트
-            </Button>
-            <Button variant="point" size="sm" disabled>
-              포인트
-            </Button>
+              </code>
+            </pre>
           </div>
-        </div>
-        <div className="code_example">
-          <pre className="code_block">
-            <code>
-              {`<Button variant="point" size="sm">포인트</Button>
+
+          <h3 className="guide_tit3">point Button</h3>
+          <div className="flex_box">
+            <div className="desc">
+              <Button variant="point" size="sm">
+                포인트
+              </Button>
+              <Button variant="point" size="sm" disabled>
+                포인트
+              </Button>
+            </div>
+          </div>
+          <div className="code_example">
+            <pre className="code_block">
+              <code>
+                {`<Button variant="point" size="sm">포인트</Button>
 <Button variant="point" size="sm" disabled>포인트</Button>`}
-            </code>
-          </pre>
-        </div>
-      </div>
-      <div>
-        <h3 className="guide_tit3">text Button</h3>
-        <div className="flex_box">
-          <div className="desc">
-            <Button variant="text" size="sm">
-              선택
-            </Button>
-            <Button variant="text" size="sm" iconAlign="left">
-              <IcoDownload width={16} height={16} stroke="#3e4550" /> 선택
-            </Button>
-            <Button variant="text" size="sm" disabled>
-              선택
-            </Button>
+              </code>
+            </pre>
           </div>
-        </div>
-        <div className="code_example">
-          <pre className="code_block">
-            <code>
-              {`<Button variant="text" size="sm">선택</Button>
+
+          <h3 className="guide_tit3">text Button</h3>
+          <div className="flex_box">
+            <div className="desc">
+              <Button variant="text" size="sm">
+                선택
+              </Button>
+              <Button variant="text" size="sm" iconAlign="left">
+                <IcoDownload width={16} height={16} stroke="#3e4550" /> 선택
+              </Button>
+              <Button variant="text" size="sm" disabled>
+                선택
+              </Button>
+            </div>
+          </div>
+          <div className="code_example">
+            <pre className="code_block">
+              <code>
+                {`<Button variant="text" size="sm">선택</Button>
 
 <Button variant="text" size="sm" iconAlign="left">
     <IcoDownload width={16} height={16} stroke="#3e4550" /> 선택
 </Button>
 
 <Button variant="text" size="sm" disabled>선택</Button>`}
-            </code>
-          </pre>
-        </div>
-      </div>
-      <div>
-        <h3 className="guide_tit3">chips Button</h3>
-        <div className="flex_box">
-          <div className="desc">
-            <Button variant="chips" size="sm">
-              선택
-            </Button>
-            <Button variant="chips" size="sm" iconAlign="right">
-              선택
-              <IcoClose width={12} height={12} stroke="#131C30" />
-            </Button>
+              </code>
+            </pre>
           </div>
-        </div>
 
-        <div className="code_example">
-          <pre className="code_block">
-            <code>
-              {`<Button variant="chips" size="sm">선택</Button>
-              
+          <h3 className="guide_tit3">chips Button</h3>
+          <div className="flex_box">
+            <div className="desc">
+              <Button variant="chips" size="sm">
+                선택
+              </Button>
+              <Button variant="chips" size="sm" iconAlign="right">
+                선택
+                <IcoClose width={12} height={12} stroke="#131C30" />
+              </Button>
+            </div>
+          </div>
+
+          <div className="code_example">
+            <pre className="code_block">
+              <code>
+                {`<Button variant="chips" size="sm">선택</Button>
+
 <Button variant="chips" size="sm" iconAlign="right">
     선택 <IcoClose width={12} height={12} stroke="#131C30" />
 </Button>`}
-            </code>
-          </pre>
+              </code>
+            </pre>
+          </div>
         </div>
       </div>
     </div>
--- apps/pub/bo/src/pages/_guide/guide/info.tsx
+++ apps/pub/bo/src/pages/_guide/guide/info.tsx
@@ -8,16 +8,15 @@ function RouteComponent() {
   return (
     <div>
       <h2 className="guide_tit2">Guide Info</h2>
-      <h3 className="guide_tit3">링크, 버튼</h3>
-      <div className="desc">
-        <p>
-          <code>&lt;Link to=&#123;&quot;&quot;&#125;&gt;링크&lt;/Link&gt;</code>
-          페이지 전환
-        </p>
-        <p>
-          <code>&lt;Button&gt;버튼컴포넌트 활용&lt;/Button&gt;</code> 폼 제출 버튼, 팝업 열기, 토글
-          기능등, 페이지 전환 제외
-        </p>
+      <h3 className="guide_tit3">소개</h3>
+      <div className="group">
+        <div className="info">리액트,테일윈드,module.css로 진행한다.</div>
+
+        <div className="code_example">
+          <pre className="code_block">
+            <code>{``}</code>
+          </pre>
+        </div>
       </div>
     </div>
   );
