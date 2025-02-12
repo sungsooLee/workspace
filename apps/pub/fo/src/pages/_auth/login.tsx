b4a724bfdf434b4f2dc31f05ea46518ce0618df4
김태경/협력사 <9488416@ict-companion.com>
2025-02-12 12:47:32 +0900

    [퍼블]FO로그인재업

--- apps/pub/fo/src/pages/_auth/login.tsx
+++ apps/pub/fo/src/pages/_auth/login.tsx
@@ -16,7 +16,7 @@ export const Route = createFileRoute('/_auth/login')({
 function RouteComponent() {
   return (
     <div className={`${styles.start} ${styles.auth_wrap}`}>
-      <div className={authStyles.auth_box}>
+      <div className={styles.auth_box}>
         <div className={formStyles.form_row}>
           <div className={formStyles.form_item}>
             <label htmlFor="name5" className={formStyles.form_label}>
