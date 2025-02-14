778f6e88155a6c58c8e5adfabbc73176fb9a26ec
김태경/협력사 <9488416@ict-companion.com>
2025-02-14 12:51:28 +0900

    [퍼블]

--- apps/pub/bo/src/features/layout/ui/language.tsx
+++ apps/pub/bo/src/features/layout/ui/language.tsx
@@ -32,7 +32,11 @@ const PopoverContent = () => {
   );
 };
 
-const LanguageComponent = () => {
+interface LanguageComponentProp {
+  className: string;
+}
+
+const LanguageComponent = ({ className }: LanguageComponentProp) => {
   return (
     <Popover
       popoverContent={<PopoverContent />}
--- apps/pub/bo/src/widgets/layout/ui/header/header-auth.tsx
+++ apps/pub/bo/src/widgets/layout/ui/header/header-auth.tsx
@@ -14,7 +14,7 @@ function HeaderAuthComponent() {
           <Logo />
         </h1>
 
-        <Language />
+        <Language className={styles.auth} />
       </header>
     </div>
   );
