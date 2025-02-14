f9c9b5ebe1f6b4347382d9591d0d6fb9daf7c66e
배성련/협력사 <9480335@ict-companion.com>
2025-02-14 11:00:41 +0900

    feat: [HMGSLP-229] Add prop className

--- apps/bo/src/features/layout/ui/logo/logo.tsx
+++ apps/bo/src/features/layout/ui/logo/logo.tsx
@@ -1,18 +1,23 @@
 import { memo } from 'react';
 import { Link } from '@tanstack/react-router';
 
+import { cn } from '@learnway/shared';
 import { useFetchAuthUser } from '../../../../entities/user';
 import { useFetchTenant } from '../../../../entities/tenant';
 
 import logoImage from '../../../../assets/images/logo.png';
 import styles from './logo.module.css';
 
-const LogoCompoment = () => {
+interface LogoComponentProp {
+  className: string;
+}
+
+const LogoCompoment = ({ className }: LogoComponentProp) => {
   const { data } = useFetchAuthUser();
   const { data: tenant } = useFetchTenant(data?.activeTenantId);
 
   return (
-    <div className={`${styles.start} nlp--logo`}>
+    <div className={cn(styles.start, 'nlp--logo', className)}>
       <Link to={'/'}>
         {/*(tenant?.logoImageUrl && <img src={logoImage} alt="Logo" />) ?? tenant?.name*/}
         <img src={logoImage} alt="Logo" />
