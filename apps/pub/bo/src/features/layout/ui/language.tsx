e290f87e6a5550fbf44130ff2ce6ebf98d6178ef
김태경/협력사 <9488416@ict-companion.com>
2025-02-14 13:26:45 +0900

    [퍼블]로그인

--- apps/pub/bo/src/features/layout/ui/language.tsx
+++ apps/pub/bo/src/features/layout/ui/language.tsx
@@ -1,6 +1,7 @@
 import { memo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { cn } from '@learnway/shared';
 import { Button, Popover } from '@learnway/ui';
 import styles from './language.module.css';
 import { IcoArrowDown } from '@learnway/icons';
@@ -33,14 +34,14 @@ const PopoverContent = () => {
 };
 
 interface LanguageComponentProp {
-  className: string;
+  className?: string;
 }
 
 const LanguageComponent = ({ className }: LanguageComponentProp) => {
   return (
     <Popover
       popoverContent={<PopoverContent />}
-      className={styles.btn_language}
+      className={cn(styles.btn_language, className)}
       side="bottom"
       align="end"
       sideOffset={5}>
