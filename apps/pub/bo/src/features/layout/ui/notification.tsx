bdfac64a6cbf402caa5295cc8c11fdd4d8c03ed9
협력사/이성수 <9490817@ict-companion.com>
2025-02-17 15:59:59 +0900

    [퍼블] bo 작업중

--- apps/pub/bo/src/features/layout/ui/notification.tsx
+++ apps/pub/bo/src/features/layout/ui/notification.tsx
@@ -1,15 +1,112 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
 
-import { Popover } from '@learnway/ui';
-import { IcoAlarmFill } from '@learnway/icons';
+import { Button, Popover } from '@learnway/ui';
+import { IcoAlarmFill, IcoXclose } from '@learnway/icons';
+import { Link } from '@tanstack/react-router';
+import { cn } from '@learnway/shared';
+
 import styles from './notification.module.css';
 
 // import { useFetchAuthUser } from '../../../entities/user';
 
+interface NotificationInfo {
+  id: number;
+  date?: string;
+  title: string;
+  message: string;
+  time: string;
+  isRead: boolean;
+  hasLink: boolean;
+}
+
+const initialNotifications: NotificationInfo[] = [
+  {
+    id: 1,
+    date: '02.14',
+    title: '새 공지',
+    message: `${'관리자'}로 부터 ${'김현대'}님에게 요청이 왔습니다.`,
+    time: '10분전',
+    isRead: false,
+    hasLink: true,
+  },
+  {
+    id: 2,
+    date: '02.14',
+    title: '시스템 점검',
+    message: '내일 오전 2시에 점검이 있습니다.',
+    time: '10분전',
+    isRead: true,
+    hasLink: false,
+  },
+];
+
 const PopoverContent = () => {
+  const [notifications, setNotifications] = useState<NotificationInfo[]>(initialNotifications);
+
+  const handleDelete = (id: number) => {
+    setNotifications(notifications.filter((notif) => notif.id !== id));
+  };
+
+  const markAsRead = (id: number) => {
+    setNotifications(
+      notifications.map((notif) => (notif.id === id ? { ...notif, isRead: true } : notif)),
+    );
+  };
   return (
-    <div className={styles.alarm_content}>
-      <p>111</p>
+    <div className={cn(styles.start, styles.alarm_wrap)}>
+      <div className={styles.alarm_content}>
+        {/* alarm_header */}
+        <div className={styles.alarm_header}>
+          <strong className={styles.tit}>{'알림'}</strong>
+          <div className={styles.btn_wrap}>
+            <Button className={styles.btn}>전체읽음</Button>
+            <Button className={styles.btn}>전체삭제</Button>
+          </div>
+        </div>
+        <div className={styles.alarm_contents}>
+          {/* 알림 없는 경우 */}
+          {notifications.length === 0 ? (
+            <p className={styles.empty}>
+              새로운 알림이 없습니다.
+              <span className={styles.sub_text}>알림은 30일 동안 보관됩니다.</span>
+            </p>
+          ) : (
+            <ul className={styles.info_list}>
+              {notifications.map(({ id, date, title, message, time, isRead, hasLink }) => (
+                <li
+                  key={id}
+                  className={`${styles.info_item} ${!isRead ? styles.yet : ''}`}
+                  onClick={() => markAsRead(id)}>
+                  <div className={styles.title_wrap}>
+                    <strong className={styles.title}>
+                      {title}
+                      <span className={styles.date}>{date}</span>
+                    </strong>
+                    <Button
+                      className={styles.btn_close}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleDelete(id);
+                      }}>
+                      <IcoXclose width={24} height={24} stroke="#131C30" />
+                    </Button>
+                  </div>
+                  <div className={styles.message_wrap}>
+                    {hasLink ? (
+                      <Link to={''} className={styles.link}>
+                        {message}
+                      </Link>
+                    ) : (
+                      <p className={styles.message}>{message}</p>
+                    )}
+                    <p className={styles.time}>{time}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
@@ -18,12 +115,11 @@ const NotificationComponent = () => {
   // const { data } = useFetchAuthUser();
 
   return (
-    <Popover popoverContent={<PopoverContent />}>
-      {/* <Avatar imageUrl="https://*.png" fallback="Noti" /> */}
-      <button type="button" className={styles.btn_alarm}>
+    <Popover popoverContent={<PopoverContent />} side="bottom" align="end" sideOffset={5}>
+      <span className={styles.alarm_info}>
         <IcoAlarmFill width={32} height={32} stroke="#fff" />
         <em className={styles.count_view}>99</em>
-      </button>
+      </span>
     </Popover>
   );
 };
