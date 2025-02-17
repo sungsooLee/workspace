46299fbb68165b628bacdbef8d5803b008c1533a
협력사/이성수 <9490817@ict-companion.com>
2025-02-17 18:10:35 +0900

    [퍼블] bo 텍스트에어리어 수정

--- libs/ui/src/lib/textarea/textarea.tsx
+++ libs/ui/src/lib/textarea/textarea.tsx
@@ -9,23 +9,20 @@ export interface TextareaComponentProps extends React.TextareaHTMLAttributes<HTM
   className?: string;
   resize?: 'none' | 'vertical' | 'horizontal' | 'both';
   size?: 'sm' | 'md'; // textarea 높이(basic : md)
-  border?: 'none'; // border 없는 경우
 }
 
 const TextareaComponent = forwardRef<HTMLTextAreaElement, TextareaComponentProps>(
-  ({ size = 'md', resize, border, className, ...props }, ref) => {
+  ({ size = 'md', resize, className, ...props }, ref) => {
     return (
-      <div className={styles.textarea_wrap}>
+      <div className={cn(styles.start, styles.textarea_wrap)}>
         {/* textarea */}
         <textarea
           className={cn(
-            styles.start,
             styles.textarea,
             className,
             'nlp--textarea',
             resize && styles[resize],
             size && styles[size],
-            border && styles.bd_none,
           )}
           {...props}
         />
