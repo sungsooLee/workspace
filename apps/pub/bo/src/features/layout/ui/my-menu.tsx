70ee4f0f77350461ffc9ee698a99b310a65eec73
협력사/이성수 <9490817@ict-companion.com>
2025-02-18 19:21:50 +0900

    [퍼블] bo 작업중

--- apps/pub/bo/src/features/layout/ui/my-menu.tsx
+++ apps/pub/bo/src/features/layout/ui/my-menu.tsx
@@ -1,21 +1,69 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import styles from './my-menu.module.css';
-import { Popover } from '@learnway/ui';
-import { IcoStar } from '@learnway/icons';
+import { Chips, Popover } from '@learnway/ui';
+import { IcoStar, IcoAnnouncement03 } from '@learnway/icons';
+import { cn } from '@learnway/shared';
 
 // import { useFetchAuthUser } from '../../../entities/user';
 
+interface menuInfo {
+  id: number;
+  label: string;
+  value: string;
+}
+
+const items: menuInfo[] = [
+  { id: 1, label: '학습운영', value: 'A' },
+  { id: 2, label: '수강신청/현황', value: 'B' },
+  { id: 3, label: '대리 결재자 지정', value: 'C' },
+  { id: 4, label: '컨퍼런스/포럼/워크샵', value: 'E' },
+  { id: 5, label: '전시관리', value: 'F' },
+  { id: 6, label: 'COP결과 관리', value: 'g' },
+];
+
 const PopoverContent = () => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
+
+  const [menu, setMymenus] = useState<menuInfo[]>(items);
+
+  const handleDelete = (id: number) => {
+    setMymenus(menu.filter((notif) => notif.id !== id));
+    console.log('111');
+  };
 
   return (
-    <div>
-      <div>{t('RECENT MENU')}</div>
-      <div>최근 메뉴 목록</div>
-      <div>{t('FAVORITES')}</div>
-      <div>즐겨찾기 목록</div>
+    <div className={cn(styles.start, styles.mymenu_wrap)}>
+      <strong className={styles.tit}>{'최근 메뉴'}</strong>
+      <div className={styles.word_contents}>
+        {/* 최근 자주 사용한 메뉴 없는 경우 */}
+        {items.length === 0 ? (
+          <p className={styles.text}>
+            <IcoAnnouncement03
+              className={styles.icon_menu}
+              width={32}
+              height={32}
+              stroke="#131C30"
+            />
+            최근 자주 사용한 메뉴를
+            <br />
+            최대 10개 까지 볼 수 있습니다.
+          </p>
+        ) : (
+          <div className={styles.word_wrap}>
+            {items.map(({ id, label, value }) => (
+              <Chips
+                key={id}
+                className={styles.word_item}
+                size={'sm'}
+                option={{ label: label, value: value }}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+      <strong className={styles.tit}>{'즐겨찾기'}</strong>
     </div>
   );
 };
