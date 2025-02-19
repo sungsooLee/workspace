84d86371f2b3492b14f12ecea46210918feb977e
협력사/이성수 <9490817@ict-companion.com>
2025-02-19 13:57:30 +0900

    [퍼블] bo 작업중

--- libs/ui/src/lib/progress/progress.tsx
+++ libs/ui/src/lib/progress/progress.tsx
@@ -8,14 +8,18 @@ import styles from './progress.module.css';
 
 interface ProgressComponentProps extends React.ComponentProps<typeof Primitive.Root> {
   value: number;
+  max?: number;
+  label?: string;
   className?: string;
 }
 
 const ProgressComponent = forwardRef<
   React.ElementRef<typeof Primitive.Root>,
   ProgressComponentProps
->(({ className, value, ...props }, ref) => {
+>(({ className, value, label, max, ...props }, ref) => {
   const [progress, setProgress] = React.useState(value);
+
+  const progressClass = progress >= 100 ? 'completed' : progress >= 90 ? 'nearly' : '';
 
   useEffect(() => {
     const timer = setTimeout(() => setProgress(value), 500);
@@ -23,12 +27,17 @@ const ProgressComponent = forwardRef<
   }, [value]);
 
   return (
-    <Primitive.Root className={cn(styles.Root, className, 'nlp--progress')} value={progress}>
-      <Primitive.Indicator
-        className={styles.Indicator}
-        style={{ transform: `translateX(-${100 - progress}%)` }}
-      />
-    </Primitive.Root>
+    <div className={cn(styles.progress_wrap, styles[progressClass])}>
+      <p className={styles.progress_status}>
+        {value} / {max} {label}
+      </p>
+      <Primitive.Root className={cn(styles.Root, className, 'nlp--progress')} value={progress}>
+        <Primitive.Indicator
+          className={styles.indicator}
+          style={{ transform: `translateX(-${100 - progress}%)` }}
+        />
+      </Primitive.Root>
+    </div>
   );
 });
 
