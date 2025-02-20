df228e1bc1cd5a029a7ebf5e51b1cd51d6ec96cf
협력사/이성수 <9490817@ict-companion.com>
2025-02-20 15:58:30 +0900

    [퍼블] bo 작업 업로드

--- apps/bo/src/widgets/learning/ui/dialogs/lecture-type-site-url/lecture-type-site-url.tsx
+++ apps/bo/src/widgets/learning/ui/dialogs/lecture-type-site-url/lecture-type-site-url.tsx
@@ -1,6 +1,8 @@
 import { forwardRef, useEffect, useState } from 'react';
 import { Button, Input, RadioGroup } from '@learnway/ui';
 import { isEqual } from 'lodash';
+import { cn } from '@learnway/shared';
+import styles from './lecture-type-site-url.module.css';
 
 const LectureTypeSiteUrlComponent = forwardRef<HTMLDivElement, any>(
   ({ name, value, onChange }, ref) => {
@@ -19,11 +21,12 @@ const LectureTypeSiteUrlComponent = forwardRef<HTMLDivElement, any>(
     }, [data]);
 
     return (
-      <div ref={ref} className={'flex w-full flex-row'}>
+      <div ref={ref} className={cn(styles.root, styles.lecture_wrap)}>
         {/* 버튼 - 강의 타입 */}
         <Button>이러닝</Button>
         {/* 라디오 버튼 - 사내, 사외 */}
         <RadioGroup
+          className={styles.radio_box}
           value={data.officeType}
           options={[
             { label: '사내', value: '사내' },
@@ -31,26 +34,35 @@ const LectureTypeSiteUrlComponent = forwardRef<HTMLDivElement, any>(
           ]}
           onValueChange={(selectedValue) => setData({ ...data, officeType: selectedValue })}
         />
-        {/* 라디오 버튼 - 별도 앱, 사외 사이트 */}
-        <RadioGroup
-          value={data.outOfficeType}
-          options={[
-            { label: '별도 앱', value: '별도 앱' },
-            { label: '사외 사이트', value: '사외 사이트' },
-          ]}
-          disabled={data.officeType !== '사외'}
-          onValueChange={(selectedValue) => setData({ ...data, outOfficeType: selectedValue })}
-        />
-        {/* 인풋 - 사외 사이트 */}
-        <Input
-          value={data.siteUrl}
-          disabled={data.outOfficeType !== '사외 사이트'}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            setData({ ...data, siteUrl: event.target.value })
-          }
-        />
-        {/* 라벨 -사회 사이트 설명 */}
-        <span>http://붙여서 입력하세요.</span>
+        <div className={styles.option_contents}>
+          {'('}
+          {/* 라디오 버튼 - 별도 앱, 사외 사이트 */}
+          <RadioGroup
+            className={styles.radio_box}
+            value={data.outOfficeType}
+            options={[
+              { label: '별도 앱', value: '별도 앱' },
+              { label: '사외 사이트', value: '사외 사이트' },
+            ]}
+            disabled={data.officeType !== '사외'}
+            onValueChange={(selectedValue) => setData({ ...data, outOfficeType: selectedValue })}
+          />
+          {/* 인풋 - 사외 사이트 */}
+          <div className={styles.input_wrap}>
+            <Input
+              className={styles.input_area}
+              value={data.siteUrl}
+              disabled={data.outOfficeType !== '사외 사이트'}
+              placeholder="URL 입력"
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                setData({ ...data, siteUrl: event.target.value })
+              }
+            />
+            {/* 라벨 -사회 사이트 설명 */}
+            <p className={styles.text}>http://붙여서 입력하세요.</p>
+          </div>
+          {')'}
+        </div>
       </div>
     );
   },
