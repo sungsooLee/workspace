d60c648d8e336345716ed9eb95d2e5782010530a
협력사/이성수 <9490817@ict-companion.com>
2025-02-26 09:58:09 +0900

    [퍼블] bo 로그인 작업중

--- apps/pub/bo/src/pages/_auth/login.tsx
+++ apps/pub/bo/src/pages/_auth/login.tsx
@@ -1,12 +1,8 @@
 import { createFileRoute, Link } from '@tanstack/react-router';
 import { Input, Checkbox, Button } from '@learnway/ui';
-import { IcoAlertCircleGray } from '@learnway/icons';
 
 import signupStyles from './signup.module.css';
 import formStyles from '../../assets/styles/modules/form.module.css';
-import snsNaverImage from '../../assets/images/common/logo_sns_naver.png';
-import snskakaoImage from '../../assets/images/common/logo_sns_kakao.png';
-import snsGoogleImage from '../../assets/images/common/logo_sns_google.png';
 
 export const Route = createFileRoute('/_auth/login')({
   component: RouteComponent,
@@ -55,7 +51,8 @@ function RouteComponent() {
         <div className={signupStyles.login_info}>
           <Checkbox label="아이디 저장" className={signupStyles.id_save} />
           <div className={signupStyles.info}>
-            <Link to="/search-account">아이디/비밀번호찾기</Link>
+            <Link to="/search-account">아이디 찾기</Link>
+            <Link to="/search-account">비밀번호 찾기</Link>
           </div>
         </div>
 
@@ -64,36 +61,12 @@ function RouteComponent() {
             로그인
           </Button>
         </div>
-
-        <div className={signupStyles.sns_login}>
-          <h3 className={signupStyles.tit_sns}>소셜 로그인</h3>
-          <ul className={signupStyles.list}>
-            <li>
-              <Button>
-                <img src={snsNaverImage} alt="naver" />
-              </Button>
-            </li>
-            <li>
-              <Button>
-                <img src={snskakaoImage} alt="kakao" />
-              </Button>
-            </li>
-            <li>
-              <Button>
-                <img src={snsGoogleImage} alt="google" />
-              </Button>
-            </li>
-          </ul>
-          <div className={signupStyles.noti}>
-            회사 메일로 회원가입 이후 SNS 간편회원으로 로그인 할 수 있습니다.
-          </div>
-        </div>
       </div>
 
       <div className={signupStyles.login_guide}>
         <span>
           <Link to="/progress-status">회원 가입 현황</Link>
-          <Link to="/signup-step1">회원가입</Link>
+          <Link to="/signup-step1">관리자 회원가입</Link>
         </span>
       </div>
     </div>
