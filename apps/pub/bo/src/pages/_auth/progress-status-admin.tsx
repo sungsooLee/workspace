8bd863da628ae7ee747735b7a569ef84145f220e
협력사/이성수 <9490817@ict-companion.com>
2025-02-27 10:37:52 +0900

    [퍼블] bo 작업 업로드

--- apps/pub/bo/src/pages/_auth/progress-status-admin.tsx
+++ apps/pub/bo/src/pages/_auth/progress-status-admin.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { createFileRoute } from '@tanstack/react-router';
-import { Button, DatePicker, Input } from '@learnway/ui';
+import { Button, DatePicker, Input, Select } from '@learnway/ui';
 import { IcoCheck02, IcoFormRequired } from '@learnway/icons';
 import { cn } from '@learnway/shared';
 import formStyles from '../../assets/styles/modules/form.module.css';
@@ -28,10 +28,141 @@ function RouteComponent() {
           <h3 className={signupStyles.title}>관리자 권한 신청이 완료되었습니다.</h3>
           <p className={signupStyles.text}>회원가입 결과는 입력하신 메일로 발송되었습니다.</p>
         </div>
-        <div
-          className={`${formStyles.form_row} ${formStyles.no_line} ${formStyles.col} ${signupStyles.auth_form}`}>
+        <div className={signupStyles.auth_info}>
+          <p>
+            신청 :<strong className={signupStyles.date}>{'YYYY-MM-DD'}</strong>
+          </p>
+          <p>
+            승인일시 :<strong className={signupStyles.date}>{'YYYY-MM-DD'}</strong>
+          </p>
+        </div>
+        <div className={`${formStyles.form_row} ${formStyles.col} ${signupStyles.auth_form}`}>
           {/* form_item */}
           <div className={formStyles.form_item}>
+            <strong className={formStyles.form_title}>관리자 권한 신청 정보</strong>
+            <label htmlFor="name-company" className={formStyles.form_label}>
+              <span className={formStyles.form_text}>회사명</span>
+              {/* 필수 케이스 */}
+              <span className={cn(formStyles.status, formStyles.required)}>
+                <IcoFormRequired width={12} height={12} />
+              </span>
+            </label>
+            <div className={formStyles.input_box}>
+              <Input
+                id="name-company"
+                type="text"
+                value="회사명"
+                placeholder=""
+                readOnly
+                className={formStyles.lg}
+              />
+            </div>
+          </div>
+          {/* form_item */}
+          <div className={formStyles.form_item}>
+            <label htmlFor="name-myname" className={formStyles.form_label}>
+              <span className={formStyles.form_text}>이름</span>
+              {/* 필수 케이스 */}
+              <span className={cn(formStyles.status, formStyles.required)}>
+                <IcoFormRequired width={12} height={12} />
+              </span>
+            </label>
+            <div className={formStyles.input_box}>
+              <Input
+                id="name-myname"
+                type="text"
+                value="김현대"
+                placeholder=""
+                readOnly
+                className={formStyles.lg}
+              />
+            </div>
+          </div>
+          {/* form_item */}
+          <div className={formStyles.form_item}>
+            <label htmlFor="name-id" className={formStyles.form_label}>
+              <span className={formStyles.form_text}>아이디</span>
+              {/* 필수 케이스 */}
+              <span className={cn(formStyles.status, formStyles.required)}>
+                <IcoFormRequired width={12} height={12} />
+              </span>
+            </label>
+            <div className={formStyles.input_box}>
+              <Input
+                id="name-myname"
+                type="text"
+                value="asdfg@naver.com"
+                placeholder=""
+                readOnly
+                className={formStyles.lg}
+              />
+            </div>
+          </div>
+          {/* form_item */}
+          <div className={formStyles.form_item}>
+            <label htmlFor="name-companyNum" className={formStyles.form_label}>
+              <span className={formStyles.form_text}>사번</span>
+              {/* 필수 케이스 */}
+              <span className={cn(formStyles.status, formStyles.required)}>
+                <IcoFormRequired width={12} height={12} />
+              </span>
+            </label>
+            <div className={formStyles.input_box}>
+              <Input
+                id="name-companyNum"
+                type="text"
+                value="1234567"
+                placeholder=""
+                readOnly
+                className={formStyles.lg}
+              />
+            </div>
+          </div>
+          {/* form_item */}
+          <div className={formStyles.form_item}>
+            <label htmlFor="name-status" className={formStyles.form_label}>
+              <span className={formStyles.form_text}>계정 상태</span>
+              {/* 필수 케이스 */}
+              <span className={cn(formStyles.status, formStyles.required)}>
+                <IcoFormRequired width={12} height={12} />
+              </span>
+            </label>
+            <div className={formStyles.input_box}>
+              <Input
+                id="name-status"
+                type="text"
+                value="정상"
+                placeholder=""
+                readOnly
+                className={formStyles.lg}
+              />
+            </div>
+          </div>
+          {/* form_item */}
+          <div className={formStyles.form_item}>
+            <label htmlFor="name-group" className={formStyles.form_label}>
+              <span className={formStyles.form_text}>관리자 그룹</span>
+              {/* 필수 케이스 */}
+              <span className={cn(formStyles.status, formStyles.required)}>
+                <IcoFormRequired width={12} height={12} />
+              </span>
+            </label>
+            <div className={formStyles.input_box}>
+              <Select
+                className={formStyles.select_option}
+                options={[
+                  { value: 'type1', label: '관리자 그룹1' },
+                  { value: 'type2', label: '관리자 그룹2' },
+                ]}
+                disabled
+              />
+            </div>
+          </div>
+        </div>
+        <div className={`${formStyles.form_row} ${formStyles.col} ${signupStyles.auth_form}`}>
+          {/* form_item */}
+          <div className={formStyles.form_item}>
+            <strong className={formStyles.form_title}>관리자 권한 승인 정보</strong>
             <label htmlFor="name-tenant" className={formStyles.form_label}>
               <span className={formStyles.form_text}>테넌트</span>
               {/* 필수 케이스 */}
@@ -52,7 +183,7 @@ function RouteComponent() {
           </div>
           {/* form_item */}
           <div className={formStyles.form_item}>
-            <label htmlFor="name-company" className={formStyles.form_label}>
+            <label htmlFor="name-company2" className={formStyles.form_label}>
               <span className={formStyles.form_text}>회사명</span>
               {/* 필수 케이스 */}
               <span className={cn(formStyles.status, formStyles.required)}>
@@ -61,7 +192,7 @@ function RouteComponent() {
             </label>
             <div className={formStyles.input_box}>
               <Input
-                id="name-company"
+                id="name-company2"
                 type="text"
                 value="회사명"
                 placeholder=""
