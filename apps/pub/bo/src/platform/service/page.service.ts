d3a3926d1019dd2d7b5f53c4e569279a1c1b2362
협력사/이성수 <9490817@ict-companion.com>
2025-02-27 14:23:01 +0900

    [퍼블] bo 작업중

--- apps/pub/bo/src/platform/service/page.service.ts
+++ apps/pub/bo/src/platform/service/page.service.ts
@@ -5,6 +5,7 @@ export const PAGE_TITLE_BY_PATH: Record<string, string> = {
   '/pb-bo/progress-status': '진행현황',
   '/pb-bo/progress-status-certify': '진행현황',
   '/pb-bo/progress-status-admin': '관리자 권한 신청 진행현황',
+  '/pb-bo/progress-status-cp': 'CP 회원가입 신청 진행현황',
   '/pb-bo/search-account': '아이디 찾기',
   '/pb-bo/search-account-pw': '비밀번호 찾기',
   '/pb-bo/password-change': '비밀번호 변경',
