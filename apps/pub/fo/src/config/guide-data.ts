e8bcbb3ede0974e78bc8d95d11104251eaccaf6a
김태경/협력사 <9488416@ict-companion.com>
2025-03-04 09:41:34 +0900

    [퍼블]가이드업데이트

--- apps/pub/fo/src/config/guide-data.ts
+++ apps/pub/fo/src/config/guide-data.ts
@@ -355,10 +355,10 @@ export const guideData = [
   {
     layoutType: '반응형 (PC)',
     screenName: '개인정보처리방침(회원가입시정보입력)',
-    pageId: 'success',
+    pageId: '',
     screenId: 'NLP_FO_MER_1007', //스크린아이디
     pageType: 'Page',
-    completionDate: '2025-02-24', // 완료일
+    completionDate: '', // 완료일
     lastUpdateDate: '',
     remarks: '',
   },
