7466094da13579958517a5c5bf4681717d5f2c24
김태경/협력사 <9488416@ict-companion.com>
2025-03-04 18:18:13 +0900

    [퍼블]머지

--- apps/pub/fo/src/config/guide-data.ts
+++ apps/pub/fo/src/config/guide-data.ts
@@ -114,12 +114,12 @@ export const guideData = [
   {
     layoutType: '반응형 (PC)',
     screenName: 'MPASS 2차인증_FIDO 인증안내팝업',
-    pageId: '',
+    pageId: 'mpass_cert',
     screenId: 'NLP_FO_LOG_1010', //스크린아이디
     pageType: 'Pop-up',
-    completionDate: '', // 완료일
+    completionDate: '2025-03-05', // 완료일
     lastUpdateDate: '',
-    remarks: '',
+    remarks: 'NLP_FO_LOG_1008에 포함\n- 버튼 클릭',
   },
   {
     layoutType: '반응형 (PC)',
--- apps/pub/fo/src/pages/_auth/mpass_cert.tsx
+++ apps/pub/fo/src/pages/_auth/mpass_cert.tsx
@@ -19,17 +19,41 @@ function RouteComponent() {
     return (
       <div className={`${modalStyles.start} ${modalStyles.mpass_modal}`}>
         <div className={modalStyles.title_box}>
-          <h3>현재 본인 확인이 진행 중입니다.</h3>
-          <p>모바일 MPASS 앱에서 인증을 진행해 주세요</p>
+          <h3 className={modalStyles.tit}>현재 본인 확인이 진행 중입니다.</h3>
+          <p className={modalStyles.txt}>모바일 MPASS 앱에서 인증을 진행해 주세요</p>
         </div>
 
         <div className={modalStyles.guide_info}>
-          <div className={modalStyles.time}>
-            남은 시간 <strong>30초</strong>
+          <div className={modalStyles.info_box}>
+            <div className={modalStyles.time}>
+              남은 시간 <strong>30</strong>초
+            </div>
+            <p className={modalStyles.info}>
+              남은 시간 내에 모바일 MPASS 앱에서 본인 확인을 진행해 주세요. <br />
+              현재 창을 닫으면 본인 확인이 종료됩니다.
+            </p>
           </div>
-          <div className={modalStyles.guide}>
-            남은 시간 내에 모바일 MPASS 앱에서 본인 확인을 진행해 주세요. 현재 창을 닫으면 본인
-            확인이 종료됩니다.
+
+          <div className={modalStyles.guide_box}>
+            <div className={modalStyles.item}>
+              <figure className={modalStyles.img}>
+                <img src={imgGuide1} alt="" />
+              </figure>
+              <p className={modalStyles.desc}>
+                1. 모바일에서 MPASS 앱을 실행하거나
+                <br /> PUSH 메시지를 터치해 주세요.
+              </p>
+            </div>
+
+            <div className={modalStyles.item}>
+              <figure className={modalStyles.img}>
+                <img src={imgGuide2} alt="" />
+              </figure>
+              <p className={modalStyles.desc}>
+                2. MPASS 앱에서 FIDO 인증을 완료하면
+                <br /> 로그인할 수 있습니다.
+              </p>
+            </div>
           </div>
         </div>
 
