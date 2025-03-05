589bc924c6f4afe683f8e60295ac2bea6e0c2d18
김태경/협력사 <9488416@ict-companion.com>
2025-03-05 13:40:12 +0900

    [퍼블]회원가입 모달

--- apps/pub/fo/src/pages/_auth/google-cert.tsx
+++ apps/pub/fo/src/pages/_auth/google-cert.tsx
@@ -1,12 +1,10 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
 import { cn } from '@learnway/shared';
-import { IcoShieldTick01, IcoFaceId01, IcoCaution, IcoFormRequired } from '@learnway/icons';
+import { IcoShieldTick01, IcoCaution, IcoFormRequired } from '@learnway/icons';
 import formStyles from '../../assets/styles/modules/form.module.css';
 import signupStyles from './signup.module.css';
-import modalStyles from './mpass-modal.module.css';
 import { Button, RadioCard, Input, useModal, ContentsRow } from '@learnway/ui';
-import imgGuide1 from '../../assets/images/modal/img_mpass_guide1.png';
-import imgGuide2 from '../../assets/images/modal/img_mpass_guide2.png';
+import { MpassModal } from '../../features/auth';
 
 export const Route = createFileRoute('/_auth/google-cert')({
   component: RouteComponent,
@@ -14,53 +12,7 @@ export const Route = createFileRoute('/_auth/google-cert')({
 
 function RouteComponent() {
   const { open: openModal } = useModal();
-  const { close: closeModal } = useModal();
-  const MpassModalContent = () => {
-    return (
-      <div className={`${modalStyles.start} ${modalStyles.mpass_modal}`}>
-        <div className={modalStyles.title_box}>
-          <h3 className={modalStyles.tit}>현재 본인 확인이 진행 중입니다.</h3>
-          <p className={modalStyles.txt}>모바일 MPASS 앱에서 인증을 진행해 주세요</p>
-        </div>
 
-        <div className={modalStyles.guide_info}>
-          <div className={modalStyles.info_box}>
-            <div className={modalStyles.time}>
-              남은 시간 <strong>30</strong>초
-            </div>
-            <p className={modalStyles.info}>
-              남은 시간 내에 모바일 MPASS 앱에서 본인 확인을 진행해 주세요. <br />
-              현재 창을 닫으면 본인 확인이 종료됩니다.
-            </p>
-          </div>
-
-          <div className={modalStyles.guide_box}>
-            <div className={modalStyles.item}>
-              <figure className={modalStyles.img}>
-                <img src={imgGuide1} alt="" />
-              </figure>
-              <p className={modalStyles.desc}>
-                1. 모바일에서 MPASS 앱을 실행하거나
-                <br /> PUSH 메시지를 터치해 주세요.
-              </p>
-            </div>
-
-            <div className={modalStyles.item}>
-              <figure className={modalStyles.img}>
-                <img src={imgGuide2} alt="" />
-              </figure>
-              <p className={modalStyles.desc}>
-                2. MPASS 앱에서 FIDO 인증을 완료하면
-                <br /> 로그인할 수 있습니다.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className={modalStyles.inquiry}>지원 문의 계정인증 개발팀 : +82-2-6296-6409</div>
-      </div>
-    );
-  };
   return (
     <form className="form_row">
       <div className={`${signupStyles.start} ${signupStyles.auth_wrap} ${signupStyles.mpass_cert}`}>
@@ -85,25 +37,19 @@ function RouteComponent() {
             />
           </div>
 
-          <div className={signupStyles.select_txt}>
-            {/* FIDO 일때 문구 */}
-            생체인증(지문/안면인식) 인증 옵션을 선택하셨습니다. <br />
-            MPASS 인증 버튼 클릭 후 모바일 앱으로 인증을 진행해 주세요.
-          </div>
-
           {/* OTP 인증폼 */}
           <div className={cn(signupStyles.auth_form, 'no_line', 'col')}>
             <ContentsRow>
               <div className={formStyles.form_item}>
                 <label htmlFor="name" className={formStyles.form_label}>
-                  <span className={formStyles.form_text}>OTP 번호</span>
+                  <span className={formStyles.form_text}>구글 OTP 번호</span>
                   {/* 필수 케이스 */}
                   <span className={cn(formStyles.status, formStyles.required)}>
                     <IcoFormRequired width={12} height={12} />
                   </span>
                 </label>
                 <div className={formStyles.input_box}>
-                  <Input id="name" type="text" placeholder="OTP 번호 입력" value="" />
+                  <Input id="name" type="password" placeholder="구글 OTP 번호 입력" value="" />
                 </div>
               </div>
             </ContentsRow>
@@ -115,13 +61,23 @@ function RouteComponent() {
                 <IcoCaution width={16} height={16} stroke="#6F798B" />
                 유의사항
               </dt>
-              {/* OTP일때 문구 출력 */}
-              <dd>OTP를 선택한 경우 MPASS 앱에서 OTP번호를 확인하고 입력해 주세요.</dd>
-              {/* FIDO 문구 출력 */}
               <dd>
-                FIDO를 선택한 경우 MPASS 앱에서 생체인식(지문/안면) 후 로그인을 진행해 주세요.{' '}
+                구글 Authenficator 앱을 설치한 후에 QR코드로 차세대 학습 플랫폼 OTP를 생성한 후에
+                구글 OTP 인증을 할 수 있습니다.
+              </dd>
+              <dd>
+                구글 OTP 인증 가이드를 보고 이용 방법을 확인해 보세요.
+                <Link to="" className={signupStyles.link}>
+                  구글 OTP 인증 가이드
+                </Link>
               </dd>
             </dl>
+          </div>
+
+          <div className={signupStyles.noti_info_txt}>
+            <Link to="" className={signupStyles.btn_txt}>
+              QR코드로 인증키 생성
+            </Link>
           </div>
 
           <div className={signupStyles.btn_wrap}>
@@ -134,9 +90,9 @@ function RouteComponent() {
               variant="primary"
               onClick={() =>
                 openModal({
-                  title: 'FIDO 인증',
+                  title: '구글 OTP 인증',
                   width: 'sm',
-                  content: <MpassModalContent />,
+                  content: <MpassModal />,
                   footer: true,
                 })
               }>
--- apps/pub/fo/src/pages/_auth/mpass-cert.tsx
+++ apps/pub/fo/src/pages/_auth/mpass-cert.tsx
@@ -3,10 +3,10 @@ import { cn } from '@learnway/shared';
 import { IcoShieldTick01, IcoFaceId01, IcoCaution, IcoFormRequired } from '@learnway/icons';
 import formStyles from '../../assets/styles/modules/form.module.css';
 import signupStyles from './signup.module.css';
-import modalStyles from './mpass-modal.module.css';
+
 import { Button, RadioCard, Input, useModal, ContentsRow } from '@learnway/ui';
-import imgGuide1 from '../../assets/images/modal/img_mpass_guide1.png';
-import imgGuide2 from '../../assets/images/modal/img_mpass_guide2.png';
+
+import { MpassModal } from '../../features/auth';
 
 export const Route = createFileRoute('/_auth/mpass-cert')({
   component: RouteComponent,
@@ -14,53 +14,6 @@ export const Route = createFileRoute('/_auth/mpass-cert')({
 
 function RouteComponent() {
   const { open: openModal } = useModal();
-  const { close: closeModal } = useModal();
-  const MpassModalContent = () => {
-    return (
-      <div className={`${modalStyles.start} ${modalStyles.mpass_modal}`}>
-        <div className={modalStyles.title_box}>
-          <h3 className={modalStyles.tit}>현재 본인 확인이 진행 중입니다.</h3>
-          <p className={modalStyles.txt}>모바일 MPASS 앱에서 인증을 진행해 주세요</p>
-        </div>
-
-        <div className={modalStyles.guide_info}>
-          <div className={modalStyles.info_box}>
-            <div className={modalStyles.time}>
-              남은 시간 <strong>30</strong>초
-            </div>
-            <p className={modalStyles.info}>
-              남은 시간 내에 모바일 MPASS 앱에서 본인 확인을 진행해 주세요. <br />
-              현재 창을 닫으면 본인 확인이 종료됩니다.
-            </p>
-          </div>
-
-          <div className={modalStyles.guide_box}>
-            <div className={modalStyles.item}>
-              <figure className={modalStyles.img}>
-                <img src={imgGuide1} alt="" />
-              </figure>
-              <p className={modalStyles.desc}>
-                1. 모바일에서 MPASS 앱을 실행하거나
-                <br /> PUSH 메시지를 터치해 주세요.
-              </p>
-            </div>
-
-            <div className={modalStyles.item}>
-              <figure className={modalStyles.img}>
-                <img src={imgGuide2} alt="" />
-              </figure>
-              <p className={modalStyles.desc}>
-                2. MPASS 앱에서 FIDO 인증을 완료하면
-                <br /> 로그인할 수 있습니다.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className={modalStyles.inquiry}>지원 문의 계정인증 개발팀 : +82-2-6296-6409</div>
-      </div>
-    );
-  };
   return (
     <form className="form_row">
       <div className={`${signupStyles.start} ${signupStyles.auth_wrap} ${signupStyles.mpass_cert}`}>
@@ -112,7 +65,7 @@ function RouteComponent() {
                   </span>
                 </label>
                 <div className={formStyles.input_box}>
-                  <Input id="name" type="text" placeholder="OTP 번호 입력" value="" />
+                  <Input id="name" type="password" placeholder="OTP 번호 입력" value="" />
                 </div>
               </div>
             </ContentsRow>
@@ -128,7 +81,7 @@ function RouteComponent() {
               <dd>OTP를 선택한 경우 MPASS 앱에서 OTP번호를 확인하고 입력해 주세요.</dd>
               {/* FIDO 문구 출력 */}
               <dd>
-                FIDO를 선택한 경우 MPASS 앱에서 생체인식(지문/안면) 후 로그인을 진행해 주세요.{' '}
+                FIDO를 선택한 경우 MPASS 앱에서 생체인식(지문/안면) 후 로그인을 진행해 주세요.
               </dd>
             </dl>
           </div>
@@ -145,7 +98,7 @@ function RouteComponent() {
                 openModal({
                   title: 'FIDO 인증',
                   width: 'sm',
-                  content: <MpassModalContent />,
+                  content: <MpassModal />,
                   footer: true,
                 })
               }>
