459fd45a061c726e32b842e1371fc635b0e249e5
김태경/협력사 <9488416@ict-companion.com>
2025-03-10 16:26:35 +0900

    [퍼블]start수정

--- apps/pub/fo/src/features/auth/popup/google-cert-guide-popup.tsx
+++ apps/pub/fo/src/features/auth/popup/google-cert-guide-popup.tsx
@@ -1,13 +1,29 @@
 import { memo } from 'react';
 import { cn } from '@learnway/shared';
+import { Button } from '@learnway/ui';
 import styles from './google-cert-guide-popup.module.css';
 
 const GoogleCertGuidePopupCompoment = () => {
   return (
-    <div className={`${styles.start} ${styles.mpass_popup}`}>
-      <div className={styles.title_box}>
-        <h3 className={styles.tit}>현재 본인 확인이 진행 중입니다.</h3>
-        <p className={styles.txt}>모바일 MPASS 앱에서 인증을 진행해 주세요</p>
+    <div className={`${styles.start} ${styles.google_cert_guide_popup}`}>
+      <div className={styles.guide_list}>
+        <div className={styles.list_box}>
+          <h3>1.구글 OTP(Google Authenticator) 앱을 휴대폰에 설치해 주세요.</h3>
+          <ul className={styles.info_list}>
+            <li>구글 OTP 앱 소개 PC 화면(휴대폰에 앱 설치 필수)</li>
+          </ul>
+          <div className={styles.btn_box}>
+            <Button>AOS 다운로드</Button> <Button>IOS 다운로드</Button>
+          </div>
+        </div>
+
+        <div className={styles.list_box}>
+          <h3>2.러닝웨이 구글 OTP 계정을 생성해 주세요.</h3>
+          <ul className={styles.info_list}>
+            <li>PC에서 QR코드로 인증키 생성 버튼을 클릭하면 QR 코드가 생성됩니다.</li>
+          </ul>
+          <div className={styles.guide_img_box}></div>
+        </div>
       </div>
     </div>
   );
