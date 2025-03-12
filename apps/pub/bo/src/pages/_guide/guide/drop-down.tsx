9ba8d403989f347c0d58e2e5fc5a5566dafda778
협력사/이성수 <9490817@ict-companion.com>
2025-03-12 18:19:32 +0900

    [퍼블] bo 작업 업로드

--- apps/pub/bo/src/pages/_guide/guide/drop-down.tsx
+++ apps/pub/bo/src/pages/_guide/guide/drop-down.tsx
@@ -29,8 +29,9 @@ function RouteComponent() {
         placeholder="여러 항목 선택"
         label="다중 선택 (체크박스)"
         variant="text"
-        isMulti={true}
+        // isMulti={true}
         size={'sm'}
+        isSearchable={true}
       />
     </div>
   );
