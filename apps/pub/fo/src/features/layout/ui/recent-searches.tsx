a4ec953b9356429739dfaff3cd88461a9860ad4b
정민혁/협력사 <9493801@ict-companion.com>
2025-03-12 18:11:25 +0900

    [퍼블] 통합검색 작업중

--- apps/pub/fo/src/features/layout/ui/recent-searches.tsx
+++ apps/pub/fo/src/features/layout/ui/recent-searches.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
 import { Button, EmptyText } from '@learnway/ui';
 
 import styles from '@learnway/styles/fo/features/layout/ui/recent-searches.module.css';
@@ -10,6 +10,9 @@ interface RecentSearchesProps {
 }
 
 const RecentSearchesCompoment = ({ className }: RecentSearchesProps) => {
+  // 검색결과
+  const [result, setResult] = useState(true);
+
   return (
     <div className={`${styles.start} ${styles.recent} ${className}`}>
       <div className={styles.tit_box}>
@@ -17,36 +20,38 @@ const RecentSearchesCompoment = ({ className }: RecentSearchesProps) => {
         <Button>전체삭제</Button>
       </div>
       <div className={styles.recent_box}>
-        {/* 최근 검색결과 있는 경우 */}
-        <ul>
-          <li>
-            <Button>
-              <i>
-                <IcoSearch width={20} height={20} stroke="#4c515e"></IcoSearch>
-              </i>
-              기업경영
-            </Button>
-            <Button>
-              <IcoXclose width={16} height={16} stroke="#6f798b"></IcoXclose>
-            </Button>
-          </li>
-          <li>
-            <Button>
-              <i>
-                <IcoSearch width={20} height={20} stroke="#4c515e"></IcoSearch>
-              </i>
-              Ai교육
-            </Button>
-            <Button>
-              <IcoXclose width={16} height={16} stroke="#6f798b"></IcoXclose>
-            </Button>
-          </li>
-        </ul>
-
-        {/* 최근 검색결과 없는 경우 */}
-        <div className={styles.empty}>
-          <EmptyText text={'최근 검색어가 없어요.'} />
-        </div>
+        {result === true ? (
+          // 최근 검색결과 있는 경우
+          <ul>
+            <li>
+              <Button>
+                <i>
+                  <IcoSearch width={20} height={20} stroke="#4c515e"></IcoSearch>
+                </i>
+                기업경영
+              </Button>
+              <Button>
+                <IcoXclose width={16} height={16} stroke="#6f798b"></IcoXclose>
+              </Button>
+            </li>
+            <li>
+              <Button>
+                <i>
+                  <IcoSearch width={20} height={20} stroke="#4c515e"></IcoSearch>
+                </i>
+                Ai교육
+              </Button>
+              <Button>
+                <IcoXclose width={16} height={16} stroke="#6f798b"></IcoXclose>
+              </Button>
+            </li>
+          </ul>
+        ) : (
+          // 최근 검색결과 없는 경우
+          <div className={styles.empty}>
+            <EmptyText text={'최근 검색어가 없어요.'} />
+          </div>
+        )}
       </div>
     </div>
   );
