49d2c0d2cfd7fc40772ff34b976691b755bbb925
김태현/협력사 <9486585@ict-companion.com>
2025-03-12 14:58:06 +0900

    [feat][HMGNLP-2319] NLP_BO_CMS_1016-학습자원조회_나의 학습자원_등록_동영상(자체)
    퍼블 이전 중

--- apps/bo/src/shared/ui/form-row/form-guid-text.tsx
+++ apps/bo/src/shared/ui/form-row/form-guid-text.tsx
@@ -1,8 +1,13 @@
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 import { cn } from '@learnway/shared';
 import styles from '@learnway/styles/bo/assets/styles/modules/form.module.css';
 
-const FormGuideTextComponent: FC<any> = ({ children }) => {
+/**
+ * FormRow 의 가이드 텍스트 컴포넌트
+ * @param children
+ * @constructor
+ */
+const FormGuideTextComponent: FC<{ children: ReactNode }> = ({ children }) => {
   return <p className={cn(styles.guide_text, 'dynamic-form-field-guide-text')}>{children}</p>;
 };
 
