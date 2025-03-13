c4263c3657987e65b1b62717ffa85532a78bfb22
배성련/협력사 <9480335@ict-companion.com>
2025-03-13 14:47:59 +0900

    feat: [HMGNLP-2835] Add component phone number

--- apps/fo/src/features/auth/ui/verify-user-id-form-field/verify-user-id-form-field.tsx
+++ apps/fo/src/features/auth/ui/verify-user-id-form-field/verify-user-id-form-field.tsx
@@ -6,9 +6,13 @@ import { cn } from '@learnway/shared';
 import { Input, Button, InputProps } from '@learnway/ui';
 
 //import styles from './auth-tool-form-field.module.css';
-import styles from '@learnway/styles/fo/features/auth/ui/auth-tool-form-field/auth-tool-form-field.module.css';
+//import styles from '@learnway/styles/fo/features/auth/ui/auth-tool-form-field/auth-tool-form-field.module.css';
 
-interface VerifyUserIdFormFieldComponentProps extends InputProps {
+interface FormFieldComponentProps {
+  onChangeGuideText?: (text: string) => void;
+}
+
+interface VerifyUserIdFormFieldComponentProps extends InputProps, FormFieldComponentProps {
   className?: string;
   onVerify?: (isSuccess: boolean) => void;
 }
@@ -19,11 +23,12 @@ function VerifyUserIdFormFieldComponent({
 }: VerifyUserIdFormFieldComponentProps) {
   const { t } = useTranslation();
   const [checkedUserId, setVerifyedUserId] = useBoolean(false);
-
+  const { onChangeGuideText, ...restProps } = props;
   const handleVerify = () => {
     if (isFunction(onVerify)) {
       onVerify(true);
     }
+    onChangeGuideText && onChangeGuideText('아이디/이메일이 확인되었습니다.');
   };
 
   return (
