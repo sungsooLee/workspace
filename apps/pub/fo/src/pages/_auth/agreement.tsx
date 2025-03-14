6ce5eafacabf02fabaefcaddf2f2a4b3e9ee1e26
김태경/협력사 <9488416@ict-companion.com>
2025-03-14 13:48:48 +0900

    [퍼블]약관 동의

--- apps/pub/fo/src/pages/_auth/agreement.tsx
+++ apps/pub/fo/src/pages/_auth/agreement.tsx
@@ -15,7 +15,7 @@ function RouteComponent() {
   return (
     <div className={`${styles.start} ${styles.auth_wrap} ${styles.agreement}`}>
       <div className={cn(styles.auth_box, 'auth--box')}>
-        <div className={`${proccessResultStyles.start} ${styles.title_info}`}>
+        <div className={`${proccessResultStyles.start} ${styles.success_info}`}>
           <h3 className={proccessResultStyles.title}>Learningway 이용약관 및 개인정보 처리방침 </h3>
           <p className={proccessResultStyles.noti}>
             이용약관 및 개인정보 처리방침에 동의하셔야 러닝웨이 서비스를 이용할 수 있습니다.
@@ -24,40 +24,45 @@ function RouteComponent() {
 
         <div className={styles.agreement_check_box}>
           <div className={styles.check_all}>
-            <Checkbox label="전체 약관 동의(선택항목 포함)" />
+            <Checkbox label="이용약관 및 개인정보 취급방침에 모두 동의합니다." />
           </div>
-          <ul className={styles.check_list}>
-            <li>
-              <Checkbox label="러닝웨이 이용약관(필수)" />
-              <Button className={styles.btn_view}>
-                <IcoArrowForward width={14} height={14} stroke="#131C30" />{' '}
-              </Button>
-            </li>
-            <li>
-              <Checkbox label="개인정보 이용동의(필수)" />
-              <Button className={styles.btn_view}>
-                <IcoArrowForward width={14} height={14} stroke="#131C30" />{' '}
-              </Button>
-            </li>
-            <li>
-              <Checkbox label="고유식별 정보 처리 동의(필수)" />
-              <Button className={styles.btn_view}>
-                <IcoArrowForward width={14} height={14} stroke="#131C30" />{' '}
-              </Button>
-            </li>
-            <li>
-              <Checkbox label="회원가입 및 이용 개인정보 제3자 제공동의(필수)" />
-              <Button className={styles.btn_view}>
-                <IcoArrowForward width={14} height={14} stroke="#131C30" />{' '}
-              </Button>
-            </li>
-            <li>
-              <Checkbox label="민감정보 수집 및 이용(선택)" />
-              <Button className={styles.btn_view}>
-                <IcoArrowForward width={14} height={14} stroke="#131C30" />{' '}
-              </Button>
-            </li>
-          </ul>
+          <div className={styles.check_list}>
+            <div className={styles.list}>
+              <h4 className={styles.tit}>러닝웨이 이용에 관한 동의(필수)</h4>
+              <Checkbox label="개인정보 수집 및 이용에 관한 동의에 동의합니다." />
+              <div className={styles.details}>
+                1. 수집 및 이용목적
+                <br /> - 홈페이지 회원 가입 및 관리
+                <br /> - 재화 또는 서비스 제공 1. 수집 및 이용목적
+                <br /> - 홈페이지 회원 가입 및 관리
+                <br /> - 재화 또는 서비스 제공
+              </div>
+            </div>
+
+            <div className={styles.list}>
+              <h4 className={styles.tit}>개인정보 이용동의(필수)</h4>
+              <Checkbox label="개인정보 이용에 동의합니다." />
+              <div className={styles.details}></div>
+            </div>
+
+            <div className={styles.list}>
+              <h4 className={styles.tit}>개인정보 제3자 제공동의(필수)</h4>
+              <Checkbox label="개인정보 이용에 동의합니다." />
+              <div className={styles.details}></div>
+            </div>
+
+            <div className={styles.list}>
+              <h4 className={styles.tit}>고유식별 정보 처리 동의(필수)</h4>
+              <Checkbox label="개인정보 제3자 동의에 동의합니다." />
+              <div className={styles.details}></div>
+            </div>
+
+            <div className={styles.list}>
+              <h4 className={styles.tit}>민감정보 수집 및 이용(선택)</h4>
+              <Checkbox label="민감정보 수집 및 이용에 동의합니다." />
+              <div className={styles.details}></div>
+            </div>
+          </div>
         </div>
 
         <div className={cn(styles.btn_wrap, 'auth--btn_wrap')}>
