9bcbf6679f74f7ae4d026549b735f4d9d1a23684
협력사/유홍식 <9488404@ict-companion.com>
2025-03-14 09:41:07 +0900

    feat: [HMGNLP-2140] NPL-BO-EDO-1104-이러닝 등록_기본정보

--- libs/ui/src/lib/modal/type.ts
+++ libs/ui/src/lib/modal/type.ts
@@ -1,15 +1,8 @@
 import { ReactNode } from 'react';
 import { AlertComponentProps } from '../alert/alert';
-//import { ConfirmComponentProps } from '../confirm/confirm';
 
 export type ModalSize = 'auto' | 'sm' | 'md' | 'lg' | 'xl' | 'full' | 'm_full'; // sm : 600px , md : 800px, lg : 1024px, xl : 1400px, m_full : 모바일 full
 export type ModalHeight = 'auto' | 'sm' | 'md' | 'lg' | 'full';
-
-export interface BaseModalProps {
-  setModalData: (data: any) => void;
-  onClose?: (data?: any) => void;
-  children: React.ReactNode;
-}
 
 export interface ModalConfig<T = any> {
   content: ReactNode;
@@ -28,13 +21,7 @@ export interface ModalClose<T = any> {
   data?: T;
 }
 
-export interface ModalContainerProps {
-  index: number;
-  config: ModalConfig;
-}
-
-// TODO: rename : ModalControl > useModalReturnValue
-export interface ModalControl {
+export interface useModalReturnValue {
   modals: ModalConfig[];
   open: (props: ModalConfig) => Promise<any>;
   close: (data?: any) => void;
