2b25458029504f427fe549d3eff79a762bfde8cf
협력사/이성수 <9490817@ict-companion.com>
2025-03-24 10:39:47 +0900

    [퍼블] bo 작업 업로드

--- apps/pub/bo/src/pages/_guide/guide/date-picker.tsx
+++ apps/pub/bo/src/pages/_guide/guide/date-picker.tsx
@@ -9,28 +9,28 @@ function RouteComponent() {
   return (
     <div>
       <h2>day</h2>
-      <DatePicker displayType={'day'} />
+      <DatePicker displayType={'day'} size={'lg'} />
       <br />
       <h2>year</h2>
-      <DatePicker displayType={'year'} />
+      <DatePicker displayType={'year'} size={'lg'} />
       <br />
       <h2>month</h2>
-      <DatePicker displayType={'month'} />
+      <DatePicker displayType={'month'} size={'lg'} />
       <br />
       <h2>from-to</h2>
-      <DatePicker displayType={'from-to'} numberOfMonths={2} />
+      <DatePicker displayType={'from-to'} numberOfMonths={2} size={'lg'} />
       <br />
       <h2>time</h2>
-      <DatePicker displayType={'time'} />
+      <DatePicker displayType={'time'} size={'lg'} />
       <br />
       <h2>time-hm</h2>
-      <DatePicker displayType={'time-hm'} />
+      <DatePicker displayType={'time-hm'} size={'lg'} />
       <br />
       <h2>day-time</h2>
-      <DatePicker displayType={'day-time'} />
+      <DatePicker displayType={'day-time'} size={'lg'} />
       <br />
       <h2>day-time-hm</h2>
-      <DatePicker displayType={'day-time-hm'} />
+      <DatePicker displayType={'day-time-hm'} size={'lg'} />
     </div>
   );
 }
