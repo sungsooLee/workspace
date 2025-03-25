cb964f465f2f8d46201622d4b699fb3d49ac9b09
김태경/협력사 <9488416@ict-companion.com>
2025-03-25 14:22:19 +0900

    [퍼블]로그인관련 반응형구조

--- apps/pub/fo/src/widgets/layout/ui/auth/auth-container/auth-container.tsx
+++ apps/pub/fo/src/widgets/layout/ui/auth/auth-container/auth-container.tsx
@@ -8,6 +8,9 @@ import {
 } from '../../../../../features/platform';
 import { AuthFooter } from './auth-footer/auth-footer';
 import { Footer } from '../../../../../widgets/layout/ui/main/footer/footer';
+import { MobileView, BrowserView } from 'react-device-detect';
+import { MobileContainerFooter } from '../../../../../shared/m.ui/container-footer/container-footer';
+import { MobileAuthContainerHeader } from '../../../m.ui/auth/auth-container/auth-container-header';
 
 //import styles from './auth-container.module.css';
 import styles from '@learnway/styles/fo/widgets/layout/ui/auth/auth-container/auth-container.module.css';
@@ -23,23 +26,49 @@ function AuthContainerComponent({ children }: AuthContainerComponentProps) {
   return (
     <div
       className={`${styles.start} ${styles.auth_container} ${isSigninPage(location.pathname) ? styles.login : ''}`}>
-      {/* 퍼블수정 20250317 : className 분기 */}
-      <div
-        className={`${styles.auth_area} ${isSigninPageNone(location.pathname) ? styles.none : ''}`}>
-        {/* 퍼블수정 20250317 : 타이틀 분기 */}
-        {!isSigninPageNone(location.pathname) ? (
-          <h2 className={isSigninPage(location.pathname) ? styles.title_login : ''}>{pageTitle}</h2>
-        ) : (
-          ''
-        )}
+      <div className={`${styles.auth_area}`}>
+        <BrowserView>
+          <h2 className={isSigninPage(location.pathname) ? styles.title_login : ''}>
+            {/*t(meta?.title ?? '')*/}
+          </h2>
+        </BrowserView>
+        <MobileView>
+          {isSigninPage(location.pathname) ? (
+            <h2 className={styles.title_login}>{/*t(meta?.title ?? '')*/}서브타이틀</h2>
+          ) : (
+            <MobileAuthContainerHeader />
+          )}
+        </MobileView>
+
         <div
           className={`${styles.auth_inner} ${isSigninPage(location.pathname) ? styles.login : ''}`}>
           {children}
         </div>
       </div>
-      {/* 퍼블수정 20250317 : 푸터위치수정 및 케이스추가 */}
-      {isSigninPageNone(location.pathname) ? <Footer /> : <AuthFooter />}
+      <BrowserView>
+        <AuthFooter />
+      </BrowserView>
+      <MobileView>{isSigninPage(location.pathname) && <AuthFooter />}</MobileView>
     </div>
+    // <div
+    //   className={`${styles.start} ${styles.auth_container} ${isSigninPage(location.pathname) ? styles.login : ''}`}>
+    //   {/* 퍼블수정 20250317 : className 분기 */}
+    //   <div
+    //     className={`${styles.auth_area} ${isSigninPageNone(location.pathname) ? styles.none : ''}`}>
+    //     {/* 퍼블수정 20250317 : 타이틀 분기 */}
+    //     {!isSigninPageNone(location.pathname) ? (
+    //       <h2 className={isSigninPage(location.pathname) ? styles.title_login : ''}>{pageTitle}</h2>
+    //     ) : (
+    //       ''
+    //     )}
+    //     <div
+    //       className={`${styles.auth_inner} ${isSigninPage(location.pathname) ? styles.login : ''}`}>
+    //       {children}
+    //     </div>
+    //   </div>
+    //   {/* 퍼블수정 20250317 : 푸터위치수정 및 케이스추가 */}
+    //   {isSigninPageNone(location.pathname) ? <Footer /> : <AuthFooter />}
+    // </div>
   );
 }
 
--- apps/pub/fo/src/widgets/layout/ui/auth/auth-layout.tsx
+++ apps/pub/fo/src/widgets/layout/ui/auth/auth-layout.tsx
@@ -2,7 +2,7 @@ import { memo, ReactNode } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { cn } from '@learnway/shared';
-
+//import { useCurrentRoute } from '../../../../features/platform';
 import { AuthHeader } from './auth-header/auth-header';
 import { AuthContainer } from './auth-container/auth-container';
 import styles from './auth-layout.module.css';
@@ -13,10 +13,13 @@ interface AuthLayoutComponentProps {
 
 function AuthLayoutComponent({ children }: AuthLayoutComponentProps) {
   const { t } = useTranslation();
+  //const { meta } = useCurrentRoute();
 
   return (
     <>
-      <AuthHeader />
+      {/* {meta.mobile.showHeader && <AuthHeader />} */}
+      {/* <AuthHeader /> */}
+
       <div className={`${styles.start} ${styles.container}`}>
         <div className={styles.inner}>
           <main>
