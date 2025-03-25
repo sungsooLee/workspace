2fd146b247be59ad029c742c5ced129a35f5adbc
협력사/이성수 <9490817@ict-companion.com>
2025-03-25 14:32:08 +0900

    [퍼블] bo 작업 업로드

---
+++ apps/pub/bo/src/features/auth/popup/tenant-search-popup.tsx
@@ -0,0 +1,150 @@
+import { memo, useState } from 'react';
+import { cn } from '@learnway/shared';
+import styles from './tenant-search-popup.module.css';
+import {
+  Button,
+  ModalBody,
+  ModalContainer,
+  ModalFooter,
+  ModalTitle,
+  useModal,
+  Grid,
+  RadioGroup,
+  ContentsRow,
+  DropdownList,
+  DropdownOption,
+  Input,
+} from '@learnway/ui';
+import { IcoSearch } from '@learnway/icons';
+import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
+
+const TenantSearchPopupCompoment = () => {
+  const { close: closeModal } = useModal();
+
+  // drop down
+  const [selectedOptions, setSelectedOptions] = useState<DropdownOption[]>([]);
+  const options = [
+    { value: 'option1', label: '테넌트명' },
+    { value: 'option2', label: '테넌트명 2' },
+    { value: 'option3', label: '테넌트명 3' },
+  ];
+  // grid
+  const [pageIndex, setPageIndex] = useState(0);
+  const [pageSize, setPageSize] = useState(1);
+  const data: any[] = [
+    {
+      select: <RadioGroup options={[{ value: 'type1', label: '' }]} />,
+      tenantName: '테넌트명1',
+      companyName: '회사명1',
+      registerNumber: '123-12-12345',
+      tenantAdmin: '김현대',
+    },
+    {
+      select: <RadioGroup options={[{ value: 'type2', label: '' }]} />,
+      tenantName: '테넌트명2',
+      companyName: '회사명2',
+      registerNumber: '123-12-12345',
+      tenantAdmin: '김현대',
+    },
+  ];
+  const columnHelper = createColumnHelper<any>();
+  const columns = [
+    columnHelper.accessor('select', {
+      cell: (info) => info.getValue(),
+      header: '',
+      size: 64,
+      enableGrouping: false,
+      meta: {
+        headerAlign: 'center',
+        cellAlign: 'center',
+      },
+    }),
+    columnHelper.accessor('tenantName', {
+      cell: (info) => info.getValue(),
+      header: '테넌트명',
+      size: 220,
+      enableGrouping: false,
+      meta: {
+        headerAlign: 'left',
+        cellAlign: 'left',
+      },
+    }),
+    columnHelper.accessor('companyName', {
+      cell: (info) => info.getValue(),
+      header: '회사명',
+      size: 220,
+      enableGrouping: false,
+      meta: {
+        headerAlign: 'left',
+        cellAlign: 'left',
+      },
+    }),
+    columnHelper.accessor('registerNumber', {
+      cell: (info) => info.getValue(),
+      header: '사업자 등록 번호',
+      size: 220,
+      enableGrouping: false,
+      meta: {
+        headerAlign: 'left',
+        cellAlign: 'left',
+      },
+    }),
+    columnHelper.accessor('tenantAdmin', {
+      cell: (info) => info.getValue(),
+      header: '테넌트 관리자',
+      size: 220,
+      enableGrouping: false,
+      meta: {
+        headerAlign: 'left',
+        cellAlign: 'left',
+      },
+    }),
+  ] as ColumnDef<any, unknown>[];
+  return (
+    <ModalContainer>
+      <ModalTitle>{'업무 담당 회사 정보'}</ModalTitle>
+      <ModalBody>
+        <div className={cn(styles.start, styles.wrap)}>
+          <ContentsRow className={styles.search_box}>
+            <DropdownList
+              options={options}
+              value={selectedOptions}
+              onChange={(selected) => setSelectedOptions(selected as DropdownOption[])}
+              placeholder="테넌트명"
+              variant="default"
+              isMulti={false}
+              size={'sm'}
+              className={styles.select_area}
+            />
+            <Input type="text" placeholder="입력" className={styles.input_area} />
+            <Button className={styles.btn_area} variant={'search'} size={'sm'}>
+              <IcoSearch width={16} height={16} stroke={'#131C30'} />
+              {'조회'}
+            </Button>
+          </ContentsRow>
+          <Grid
+            data={data}
+            columns={columns}
+            title="업무 담당 회사 정보"
+            hideColumnSettings={true}
+            enableRowSelectionToggle={false}
+            pagination={{
+              pageSize,
+              pageIndex,
+              totalRows: 10,
+              onPageChange: setPageIndex,
+              onPageSizeChange: setPageSize,
+            }}
+          />
+        </div>
+      </ModalBody>
+
+      <ModalFooter>
+        <Button label={'취소'} variant={'gray'} size={'lg'} onClick={() => closeModal()} />
+        <Button label={'확인'} variant={'primary'} size={'lg'} />
+      </ModalFooter>
+    </ModalContainer>
+  );
+};
+
+export const TenantSearchPopup = memo(TenantSearchPopupCompoment);
