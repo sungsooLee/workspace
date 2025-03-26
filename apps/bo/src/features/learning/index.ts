fcd95e722be6fa283d5752a09d3da723896dd94d
김태현/협력사 <9486585@ict-companion.com>
2025-03-26 12:25:37 +0900

    [fix] DynamicConfig 업데이트로 boolean 타입 format 설정 추가

--- apps/bo/src/features/learning/index.ts
+++ apps/bo/src/features/learning/index.ts
@@ -2,3 +2,4 @@ export * from './ui/resource/learning-type-choice-modal';
 export * from './ui/resource/thumbnail-uploader-form-field';
 export * from './ui/resource/movie-info';
 export * from './ui/resource/scorm-info';
+export * from './ui/resource/video-upload-modal';
