232b208320b59035404820c724460e9d04285085
협력사/이성수 <9490817@ict-companion.com>
2025-03-26 10:30:31 +0900

    [퍼블] bo 작업 업로드

--- apps/pub/bo/src/pages/_auth/admin-auth-step4.tsx
+++ apps/pub/bo/src/pages/_auth/admin-auth-step4.tsx
@@ -1,20 +1,24 @@
 import { useState } from 'react';
 import { createFileRoute } from '@tanstack/react-router';
 import { cn } from '@learnway/shared';
-import { Button, Stepper, ContentsRow, Input, DropdownList, DropdownOption } from '@learnway/ui';
+import {
+  Button,
+  Stepper,
+  ContentsRow,
+  Input,
+  DropdownList,
+  DropdownOption,
+  DatePicker,
+  Textarea,
+} from '@learnway/ui';
 import { IcoFormRequired } from '@learnway/icons';
 import styles from './admin-auth-step3.module.css';
 import formStyles from '@learnway/styles/bo/assets/styles/modules/form.module.css';
-import SingleValue from 'react-select/dist/declarations/src/components/SingleValue';
+import dynamicFormStyles from '@learnway/styles/bo/assets/styles/modules/dynamic.form.module.css';
 
 export const Route = createFileRoute('/_auth/admin-auth-step4')({
   component: RouteComponent,
 });
-
-interface Option {
-  value: string;
-  label: string;
-}
 
 function RouteComponent() {
   const items = [
@@ -31,6 +35,20 @@ function RouteComponent() {
     { value: 'option2', label: '채널 소유자2' },
     { value: 'option3', label: '채널 소유자3' },
   ];
+
+  // Date picker
+  const [date, setDate] = useState(new Date());
+  const [date2, setDate2] = useState(new Date());
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleDate = (value: any) => {
+    setDate(value);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleDate2 = (value: any) => {
+    setDate2(value);
+  };
   return (
     <div className={`${styles.start} ${styles.auth_wrap} ${styles.signup_step}`}>
       <div className={cn(styles.auth_box, 'auth--box')}>
@@ -39,8 +57,8 @@ function RouteComponent() {
             <Stepper items={items} variant="check" selectedStep="step4" />
           </div>
         </div>
+        <h4 className={cn(styles.title, 'auth--title')}>{'관리자 권한 신청 정보'}</h4>
         <div className={cn(styles.auth_form, 'no_line', 'col')}>
-          <h4 className={cn(styles.title, 'auth--title')}>{'관리자 권한 신청 정보'}</h4>
           <ContentsRow>
             <div className={formStyles.form_item}>
               <label htmlFor="name" className={formStyles.form_label}>
@@ -150,7 +168,60 @@ function RouteComponent() {
               </div>
             </div>
           </ContentsRow>
+          <ContentsRow>
+            <div className={formStyles.form_item}>
+              <label htmlFor="name-term" className={formStyles.form_label}>
+                <span className={formStyles.form_text}>권한 신청 기간</span>
+                {/* 필수 케이스 */}
+                <span className={cn(formStyles.status, formStyles.required)}>
+                  <IcoFormRequired width={12} height={12} />
+                </span>
+              </label>
+              <div className={formStyles.input_box}>
+                <div className={dynamicFormStyles.datepicker_wrap}>
+                  <DatePicker
+                    onChange={handleDate}
+                    value={date}
+                    className={dynamicFormStyles.datepicker_item}
+                    size={'lg'}
+                  />
+                  <span className={dynamicFormStyles.dash}></span>
+                  <DatePicker
+                    onChange={handleDate2}
+                    value={date2}
+                    className={dynamicFormStyles.datepicker_item}
+                    size={'lg'}
+                  />
+                </div>
+              </div>
+            </div>
+          </ContentsRow>
+          <ContentsRow>
+            <div className={formStyles.form_item}>
+              <label htmlFor="name-reason" className={formStyles.form_label}>
+                <span className={formStyles.form_text}>신청 사유</span>
+                {/* 필수 케이스 */}
+                <span className={cn(formStyles.status, formStyles.required)}>
+                  <IcoFormRequired width={12} height={12} />
+                </span>
+              </label>
+              <div className={formStyles.input_box}>
+                <Textarea
+                  id="name-reason"
+                  rows={5}
+                  cols={33}
+                  resize="none"
+                  value=""
+                  placeholder="입력"
+                  maxLength={100}
+                />
+              </div>
+            </div>
+          </ContentsRow>
         </div>
+        <hr className={`${formStyles.divider} ${styles.divider}`} />
+        <h4 className={cn(styles.title, 'auth--title')}>{'관리자 권한 승인 정보'}</h4>
+        <div className={cn(styles.auth_form, 'no_line', 'col')}></div>
         <div className={cn(styles.btn_wrap, 'auth--btn_wrap')}>
           <Button variant="gray" size="xl">
             취소
