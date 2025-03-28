6dbcc04852aaf6f75586234676dc81b45a69e4b8
협력사/이성수 <9490817@ict-companion.com>
2025-03-28 15:20:54 +0900

    [퍼블] bo 작업 업로드

--- libs/ui/src/lib/dropdown-list/dropdown-list.tsx
+++ libs/ui/src/lib/dropdown-list/dropdown-list.tsx
@@ -143,11 +143,7 @@ const DropdownComponent = forwardRef<any, DropdownComponentProps>(
           onBlur={handleBlur}
           onMenuOpen={handleMenuOpen}
           onMenuClose={handleMenuClose}
-          className={cn(
-            'select',
-            isFocused || isMenuOpen ? 'focused' : '',
-            isReadonly ? 'readonly' : '',
-          )}
+          className={cn(isFocused || isMenuOpen ? 'focused' : '', isReadonly ? 'readonly' : '')}
           classNamePrefix="nlp-select"
           components={{
             Option,
