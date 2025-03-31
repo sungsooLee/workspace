d44ac396b81a12e0b3d6a2f44747237b5decc164
협력사/이성수 <9490817@ict-companion.com>
2025-03-31 16:54:12 +0900

    [퍼블] bo 작업 업로드

---
+++ apps/pub/bo/src/pages/_layout/pms/popup-widget-management.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useRef } from 'react';
+import { createFileRoute } from '@tanstack/react-router';
+import { cn } from '@learnway/shared';
+import { Button, ModalBody, ModalContainer, ModalFooter, useModal, ModalTitle } from '@learnway/ui';
+import styles from '@learnway/styles/bo/assets/styles/modules/widget-management.module.css'; // 화면 css
+
+export const Route = createFileRoute('/_layout/pms/popup-widget-management')({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const { open: openModal, close: closeModal } = useModal();
+  const TabContents = () => {
+    return (
+      <ModalContainer>
+        <ModalTitle>{'위젯 미리보기'}</ModalTitle>
+        <ModalBody>
+          <div className={styles.wrap}></div>
+        </ModalBody>
+        <ModalFooter>
+          <Button label={'확인'} variant={'primary'} size={'lg'} onClick={() => closeModal()} />
+        </ModalFooter>
+      </ModalContainer>
+    );
+  };
+  const hasRun = useRef(false);
+  useEffect(() => {
+    if (!hasRun.current) {
+      openModal({
+        width: 'xl', // sm(600px), md(800px), lg(1024px), xl(1400px)
+        content: <TabContents />,
+      });
+      hasRun.current = true;
+    }
+  }, [openModal]);
+  return <div>위젯 미리보기</div>;
+}
