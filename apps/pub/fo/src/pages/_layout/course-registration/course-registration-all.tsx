4ac7cdf7c56794b26e140a73d6cf1f0cc0ed87ee
정민혁/협력사 <9493801@ict-companion.com>
2025-04-01 09:47:23 +0900

    [퍼블] 바텀 fix 작업

--- apps/pub/fo/src/pages/_layout/course-registration/course-registration-all.tsx
+++ apps/pub/fo/src/pages/_layout/course-registration/course-registration-all.tsx
@@ -13,6 +13,7 @@ import {
 import { cn } from '@learnway/shared';
 import { IcoCaution } from '@learnway/icons';
 import { MobileView, BrowserView } from 'react-device-detect';
+import { MobileContainerFooter } from '../../../shared/m.ui/container-footer/container-footer';
 import { isMobile } from 'react-device-detect';
 import { AddressPopup } from '../../../features/layout';
 
@@ -411,14 +412,29 @@ function RouteComponent() {
       </div>
 
       {/* button */}
-      <div className={cn(authFormStyles.btn_wrap, 'auth--btn_wrap')}>
-        <Button variant="gray" size="xl" className="min">
-          취소
-        </Button>
-        <Button variant="primary" size="xl">
-          신청
-        </Button>
-      </div>
+      <BrowserView>
+        <div className={cn(authFormStyles.btn_wrap, styles.btn_wrap, 'auth--btn_wrap')}>
+          <Button variant="gray" size="xl" className="min">
+            취소
+          </Button>
+          <Button variant="primary" size="xl">
+            신청
+          </Button>
+        </div>
+      </BrowserView>
+
+      <MobileView>
+        <MobileContainerFooter>
+          <div className={cn(authFormStyles.btn_wrap, styles.btn_wrap, 'auth--btn_wrap')}>
+            <Button variant="gray" size="xl" className="min">
+              취소
+            </Button>
+            <Button variant="primary" size="xl">
+              신청
+            </Button>
+          </div>
+        </MobileContainerFooter>
+      </MobileView>
     </div>
   );
 }
