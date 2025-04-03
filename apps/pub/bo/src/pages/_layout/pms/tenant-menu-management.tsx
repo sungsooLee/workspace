86fa6844ebdae2f8232b555cd54430280d072422
협력사/이성수 <9490817@ict-companion.com>
2025-04-03 16:52:32 +0900

    [퍼블] bo 작업 업로드

--- apps/pub/bo/src/pages/_layout/pms/tenant-menu-management.tsx
+++ apps/pub/bo/src/pages/_layout/pms/tenant-menu-management.tsx
@@ -4,31 +4,67 @@ import styles from '@learnway/styles/bo/assets/styles/modules/page-contents.modu
 import { Tabs } from '@learnway/ui';
 
 /* tab contents */
-import { TenantLearningMenu } from './-tabcontents/tenant-learning-menu'; // 학습자 메뉴
-import { TenantHrdMenu } from './-tabcontents/tenant-hrd-menu'; // HRD 메뉴
 
 export const Route = createFileRoute('/_layout/pms/tenant-menu-management')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const items = [
+  const menuItems = [
     {
-      title: '학습자',
-      key: 'tab01',
-      content: <TenantLearningMenu />,
+      title: '1. 테넌트 속성 관리',
+      key: 'menu01',
+      content: '',
     },
     {
-      title: 'HRD센터',
+      title: '2. 테넌트 디자인/테마관리',
+      key: 'menu02',
+      content: '',
+    },
+    {
+      title: '3. 테넌트 메뉴관리',
+      key: 'menu03',
+      content: '',
+    },
+    {
+      title: '4. 테넌트 카테고리 관리',
+      key: 'menu04',
+      content: '',
+    },
+    {
+      title: '5. 테넌트 역할 관리',
+      key: 'menu05',
+      content: '',
+    },
+    {
+      title: '6. 메인 위젯관리',
+      key: 'menu06',
+      content: '',
+    },
+    {
+      title: '7. 배너관리',
+      key: 'menu07',
+      content: '',
+    },
+  ];
+  const tabItems = [
+    {
+      title: '학습자 메뉴',
+      key: 'tab01',
+      content: '',
+    },
+    {
+      title: 'HRD센터 메뉴',
       key: 'tab02',
-      content: <TenantHrdMenu />,
+      content: '',
     },
   ];
   return (
     <PageContainer scrollHidden={true}>
       {/* main_contents */}
       <div className={styles.main_contents}>
-        <Tabs items={items} type="line" size={'sm'} className={styles.tab_wrap} />
+        <Tabs items={menuItems} type="progress" size="sm" />
+        <Tabs items={tabItems} type="line" size={'sm'} className={styles.tab_wrap} />
       </div>
     </PageContainer>
   );
