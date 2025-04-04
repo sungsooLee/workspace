c5e51d205a007e62dc36820b9c3d04c7432daf89
정민혁/협력사 <9493801@ict-companion.com>
2025-04-04 18:01:30 +0900

    [퍼블] 과정상세 작업중

--- apps/pub/fo/src/pages/_layout/course-introduction/detail.tsx
+++ apps/pub/fo/src/pages/_layout/course-introduction/detail.tsx
@@ -10,8 +10,9 @@ import {
   IcoThumbsUp,
   IcoMessageCircle,
   IcoMoreVertical,
+  IcoDownArrow,
 } from '@learnway/icons';
-import { CourseDashboard } from '../../../features/layout';
+import { CourseDashboard, ReviewOptionPopover } from '../../../features/layout';
 
 import pageContentsStyles from '../../_page-contents.module.css';
 import operatorStyles from './operator.module.css';
@@ -46,12 +47,7 @@ function RouteComponent() {
     { label: '스마트팩토리', value: 'M' },
   ];
 
-  const sectionB = useRef<HTMLDivElement>(null);
-  const handleActiveTab = (activeKey: string) => {
-    if (activeKey === 'c') {
-      sectionB.current?.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
+  const [thumbs, setThumbs] = useState(true);
 
   const [selectedTabKey, selectedTabKey2] = useState<string>('');
   const items = [
@@ -279,7 +275,7 @@ function RouteComponent() {
             </div>
           </div>
 
-          <div className={styles.review_wrap} ref={sectionB}>
+          <div className={styles.review_wrap}>
             <h2>
               후기<span>999,999+</span>
             </h2>
@@ -358,14 +354,53 @@ function RouteComponent() {
                           </i>
                         </div>
                         <Popover
-                          popoverContent={<SettingPopover />}
+                          popoverContent={<ReviewOptionPopover />}
                           side="bottom"
                           align="end"
                           sideOffset={5}
                           className={styles.setting}
                         >
-                          <IcoMoreVertical width={24} height={24} />
+                          <IcoMoreVertical width={24} height={24} fill="#6f798b" stroke="#6f798b" />
                         </Popover>
+                      </div>
+                    </div>
+                    <p className={styles.txt}>
+                      안녕하세요. 저는 영어 발음이 잘 이해가 안갑니다.
+                      <br />
+                      그래도 선생님 설명이 아주 좋아요! 저는 만족합니다.
+                      <br />
+                      발음때문에 -1점 했어요
+                    </p>
+                    <div className={styles.count_box}>
+                      <Button className={thumbs === true ? styles.active : ''}>
+                        <IcoThumbsUp width={20} height={20} fill="#000" stroke="red" />
+                        34
+                      </Button>
+                      <Button>
+                        <IcoMessageCircle width={20} height={20} />0
+                      </Button>
+                    </div>
+                  </div>
+
+                  {/* comment */}
+                  <div className={styles.comment}>
+                    <i className={styles.ico_comment}>
+                      <IcoDownArrow width={32} height={32} stroke="#a9afb8" />
+                    </i>
+                    <div className={styles.profile}>
+                      <Avatar
+                        imageUrl="https://github.com/shadcn.png"
+                        className={styles.info_avata}
+                      />
+                      <div className={styles.information}>
+                        <div className={styles.box}>
+                          <strong>박이나</strong>
+                        </div>
+                        <div className={styles.box}>
+                          <span>현대오토에버</span>
+                          <span>L&D플랫폼팀</span>
+                          <span>2026.07.12</span>
+                        </div>
                       </div>
                     </div>
                     <p className={styles.txt}>
@@ -379,9 +414,6 @@ function RouteComponent() {
                       <Button>
                         <IcoThumbsUp width={20} height={20} stroke="6f798b" />
                         34
-                      </Button>
-                      <Button>
-                        <IcoMessageCircle width={20} height={20} />0
                       </Button>
                     </div>
                   </div>
@@ -417,12 +449,7 @@ function RouteComponent() {
           </div>
 
           <div className={styles.tab_wrap}>
-            <Tabs
-              selectedTabKey={selectedTabKey}
-              items={items}
-              type="line"
-              onActiveTab={handleActiveTab}
-            />
+            <Tabs selectedTabKey={selectedTabKey} items={items} type="line" />
           </div>
         </div>
 
@@ -497,12 +524,3 @@ function RouteComponent() {
     </div>
   );
 }
-
-const SettingPopover = () => {
-  return (
-    <div className={`${styles.start} ${styles.setting}`}>
-      <Button>수정</Button>
-      <Button>삭제</Button>
-    </div>
-  );
-};
