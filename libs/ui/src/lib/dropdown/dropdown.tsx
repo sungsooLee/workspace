7d335d8edab4f3475a33152b89c4d305c353ca8d
김태경/협력사 <9488416@ict-companion.com>
2025-04-07 14:00:39 +0900

    [퍼블]panel 콤포넌트, 드랍다운 css

--- libs/ui/src/lib/dropdown/dropdown.tsx
+++ libs/ui/src/lib/dropdown/dropdown.tsx
@@ -167,7 +167,6 @@ const PrimitiveComponent = forwardRef<any, ReactSelectComponentProps>(
     return (
       <div className={cn(dropdownClass.trim(), styles.select_wrap, 'dropdown', className)}>
         <Select
-          menuIsOpen
           id={uuid}
           ref={ref}
           options={options}
