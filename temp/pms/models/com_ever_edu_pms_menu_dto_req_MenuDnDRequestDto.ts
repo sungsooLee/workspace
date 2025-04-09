85f876e259fa3fefdc19be6498564b9e7060de74
협력사/유홍식 <9488404@ict-companion.com>
2025-04-09 13:06:17 +0900

    feat: [HMGNLP-2023] NLP_BO_TMS_1300 회사관리

---
+++ temp/pms/models/com_ever_edu_pms_menu_dto_req_MenuDnDRequestDto.ts
@@ -0,0 +1,9 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type com_ever_edu_pms_menu_dto_req_MenuDnDRequestDto = {
+    destinationParentId: number;
+    sortSeq: number;
+};
+
