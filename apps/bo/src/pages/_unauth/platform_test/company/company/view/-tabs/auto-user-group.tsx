6ce0c1f463a5fbdca9dc4f8a9549ccb8864a357b
협력사/유홍식 <9488404@ict-companion.com>
2025-04-09 16:57:19 +0900

    feat: [HMGNLP-2023] NLP_BO_TMS_1300 회사관리

--- apps/bo/src/pages/_unauth/platform_test/company/company/view/-tabs/auto-user-group.tsx
+++ apps/bo/src/pages/_unauth/platform_test/company/company/view/-tabs/auto-user-group.tsx
@@ -1,5 +1,53 @@
+import { NoticeBox } from '../../../../../../../shared/ui';
+import { useTranslation } from 'react-i18next';
+import { Tabs } from '@learnway/ui';
+import React from 'react';
+import { Organization } from './organization';
+import { Position } from './position';
+import { Group } from './group';
+import { Designation } from './designation';
+import { Role } from './role';
+
 const AutoUserGroupComponent = () => {
-  return <h1>AutoUserGroup</h1>;
+  const { t } = useTranslation();
+  const tabItems = [
+    {
+      title: '조직',
+      key: '조직',
+      content: <Organization />,
+    },
+    {
+      title: '보직',
+      key: '보직',
+      content: <Position />,
+    },
+    {
+      title: '직군',
+      key: '직군',
+      content: <Group />,
+    },
+    {
+      title: '호칭',
+      key: '호칭',
+      content: <Designation />,
+    },
+    {
+      title: '직무',
+      key: '직무',
+      content: <Role />,
+    },
+  ];
+  return (
+    <div className={'flex flex-col'}>
+      <NoticeBox
+        descriptions={[
+          t('자동 유저그룹은 인사 DB를 기준으로 특정 시간에 배치로 자동 매핑됩니다.'),
+          t('회사별로 자동 매핑되며, 유저그룹 설정 시 사용할 수 있습니다.'),
+        ]}
+      />
+      <Tabs type={'line'} size={'sm'} items={tabItems} />
+    </div>
+  );
 };
 
 export const AutoUserGroup = AutoUserGroupComponent;
---
+++ apps/bo/src/pages/_unauth/platform_test/company/company/view/-tabs/role.tsx
@@ -0,0 +1,9 @@
+import { useTranslation } from 'react-i18next';
+import React from 'react';
+
+const RoleComponent = () => {
+  const { t } = useTranslation();
+  return <div className={'flex flex-col'}>role.tsx</div>;
+};
+
+export const Role = RoleComponent;
