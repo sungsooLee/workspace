3c008671482ad3903265019f368cf63d12096bbe
협력사/이성수 <9490817@ict-companion.com>
2025-04-11 17:58:23 +0900

    [퍼블] bo 작업 업로드

---
+++ apps/pub/bo/src/pages/_layout/common/-tabcontents/outside-user-menu.tsx
@@ -0,0 +1,114 @@
+/* eslint-disable @nx/enforce-module-boundaries */
+import { FC, useState } from 'react';
+import { cn } from '@learnway/shared';
+import { Input, Dropdown, Button } from '@learnway/ui';
+import { IcoRefresh02, IcoSearch } from '@learnway/icons';
+
+/* style */
+import styles from './user-search.module.css';
+import searchStyles from '@learnway/styles/bo/assets/styles/modules/search-box.module.css'; // search-box.module.css
+
+const OutsideUserMenuComponent: FC<{}> = ({}) => {
+  const [selectedValues, setSelectedValues] = useState<string[]>([]);
+  const options = [
+    { value: 'option1', label: '전체' },
+    { value: 'option2', label: '옵션 2' },
+    { value: 'option3', label: '옵션 3' },
+  ];
+  return (
+    <div className={cn(searchStyles.start, searchStyles.wrap)}>
+      <div className={searchStyles.contents}>
+        <div className={searchStyles.item_row}>
+          <div className={searchStyles.item_wrap}>
+            <div className={searchStyles.inner}>
+              <div className={searchStyles.item}>
+                <label htmlFor="name-select1" className={searchStyles.label}>
+                  <span className={searchStyles.text}>사외이용자 유형</span>
+                </label>
+                <div className={searchStyles.box}>
+                  <Dropdown
+                    options={options}
+                    value={selectedValues}
+                    onChange={(selected) => setSelectedValues(selected)}
+                    variant="default"
+                    size={'sm'}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className={searchStyles.inner}>
+              <div className={searchStyles.item}>
+                <label htmlFor="name-select2" className={searchStyles.label}>
+                  <span className={searchStyles.text}>회사</span>
+                </label>
+                <div className={searchStyles.box}>
+                  <Dropdown
+                    options={options}
+                    value={selectedValues}
+                    onChange={(selected) => setSelectedValues(selected)}
+                    variant="default"
+                    size={'sm'}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className={searchStyles.inner}>
+              <div className={searchStyles.item}>
+                <label htmlFor="name-select3" className={searchStyles.label}>
+                  <span className={searchStyles.text}>소속</span>
+                </label>
+                <div className={searchStyles.box}>
+                  <Dropdown
+                    options={options}
+                    value={selectedValues}
+                    onChange={(selected) => setSelectedValues(selected)}
+                    variant="default"
+                    size={'sm'}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className={searchStyles.inner}>
+              <div className={searchStyles.item}>
+                <label htmlFor="name-input1" className={searchStyles.label}>
+                  <span className={searchStyles.text}>사번</span>
+                </label>
+                <div className={searchStyles.box}>
+                  <Input id="name-input1" type="text" placeholder="입력" />
+                </div>
+              </div>
+            </div>
+            <div className={searchStyles.inner}>
+              <div className={searchStyles.item}>
+                <label htmlFor="name-input2" className={searchStyles.label}>
+                  <span className={searchStyles.text}>이름</span>
+                </label>
+                <div className={searchStyles.box}>
+                  <Input id="name-input2" type="text" placeholder="입력" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={searchStyles.btn_box}>
+          <Button
+            type="button"
+            className={searchStyles.btn_refresh}
+            variant="search"
+            size="sm"
+            onlyIcon
+          >
+            <IcoRefresh02 className={searchStyles.icon_refresh} />
+          </Button>
+          <Button type="button" variant="search" size="sm" className={searchStyles.btn_search}>
+            <IcoSearch className={searchStyles.icon_sm_search} />
+            조회
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+OutsideUserMenuComponent.displayName = 'OutsideUserMenu';
+export const OutsideUserMenu = OutsideUserMenuComponent;
---
+++ apps/pub/bo/src/pages/_layout/common/pop-user-search.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useRef } from 'react';
+import { createFileRoute } from '@tanstack/react-router';
+import { cn } from '@learnway/shared';
+import {
+  ModalBody,
+  ModalContainer,
+  ModalFooter,
+  ModalTitle,
+  useModal,
+  Tabs,
+  Button,
+} from '@learnway/ui';
+
+/* tab contents */
+import { UserMenu } from './-tabcontents/user-menu';
+import { OutsideUserMenu } from './-tabcontents/outside-user-menu';
+
+export const Route = createFileRoute('/_layout/common/pop-user-search')({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const { open: openModal, close: closeModal } = useModal();
+  const UserSearchContent = () => {
+    const tabItems = [
+      {
+        title: '유저',
+        key: 'tab01',
+        content: <UserMenu />,
+      },
+      {
+        title: '사외 이용자',
+        key: 'tab02',
+        content: <OutsideUserMenu />,
+      },
+    ];
+    return (
+      <ModalContainer>
+        <ModalTitle>유저 조회</ModalTitle>
+        <ModalBody>
+          <Tabs items={tabItems} type="line" size={'sm'} selectedTabKey={'tab01'} />
+        </ModalBody>
+        <ModalFooter>
+          <Button label={'취소'} variant={'gray'} size={'lg'} onClick={() => closeModal()} />
+          <Button label={'확인'} variant={'primary'} size={'lg'} onClick={() => closeModal()} />
+        </ModalFooter>
+      </ModalContainer>
+    );
+  };
+  // 한번만 실행
+  const hasRun = useRef(false);
+  useEffect(() => {
+    if (!hasRun.current) {
+      openModal({
+        width: 'xl', // sm(600px), md(800px), lg(1024px), xl(1400px)
+        content: <UserSearchContent />,
+      });
+      hasRun.current = true;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [openModal]);
+  return <div>유저조회</div>;
+}
