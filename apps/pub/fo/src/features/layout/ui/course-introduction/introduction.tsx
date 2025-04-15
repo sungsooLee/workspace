544d7b68830d7630b6ab8fc2cc9ed007d479b8e2
정민혁/협력사 <9493801@ict-companion.com>
2025-04-15 15:40:47 +0900

    [퍼블] dropdown popover 형식 추가

--- apps/pub/fo/src/features/layout/ui/course-introduction/introduction.tsx
+++ apps/pub/fo/src/features/layout/ui/course-introduction/introduction.tsx
@@ -1,9 +1,10 @@
 import { memo, useState } from 'react';
 import { Link } from '@tanstack/react-router';
-import { ChipList, SelectOption, Button, Avatar, Dropdown } from '@learnway/ui';
+import { ChipList, SelectOption, Button, Avatar, Popover } from '@learnway/ui';
 import { IcoArrowDown, IcoDownload } from '@learnway/icons';
 import { Review, ReviewRating, Curriculum, Arrays, Education } from '../../../../features/layout';
 
+import dropdownPopoverStyles from '../../../../shared/ui/dropdown-popover/dropdown-popover.module.css';
 import operatorStyles from '../../../../pages/_layout/course-introduction/operator.module.css';
 import definitionListStyles from '../../../../pages/_layout/course-introduction/definition-list.module.css';
 import bulletStyles from '../../../../pages/_layout/course-introduction/bullet.module.css';
@@ -37,11 +38,18 @@ const CourseIntroductionCompoment = () => {
     items: ['수강신청만', '전체보기'],
     initialSelectedItem: 0, // 초기 선택값
   };
-  const [dropdownValues, setDropdownValues] = useState<string[]>([]);
-  const dropdownOptions = [
-    { value: 'option1', label: '옵션 1' },
-    { value: 'option2', label: '옵션 2' },
-  ];
+
+  // 교육일정 년도별 보기 popover
+  const DropdownPopoverCompoment = () => {
+    return (
+      <div className={`${dropdownPopoverStyles.start} ${dropdownPopoverStyles.dropdown_wrap}`}>
+        <Button>년도별 보기1</Button>
+        <Button>년도별 보기2</Button>
+        <Button>년도별 보기3</Button>
+        <Button>년도별 보기444444444</Button>
+      </div>
+    );
+  };
 
   // 교육일정 더보기 버튼
   const [btnEducation, setBtnEducation] = useState<boolean>(false);
@@ -241,7 +249,7 @@ const CourseIntroductionCompoment = () => {
               받아주세요.
             </li>
             <li>
-              보안프로그램 : 본 과정은 보안프로그램을 설치해야 하는 과정입니다. 학습 전,{' '}
+              보안프로그램 : 본 과정은 보안프로그램을 설치해야 하는 과정입니다. 학습 전,
               <Link to="">보안프로그램</Link>을 먼저 설치해주세요.
             </li>
             <li>승인필요 : 본 과정은 수강신청 후 팀장 및 교육담당자 승인이 필요합니다.</li>
@@ -316,14 +324,17 @@ const CourseIntroductionCompoment = () => {
           <span className={styles.date}>2026년</span>
           <div className={styles.filter}>
             <Arrays arraysData={arrays} className={styles.array}></Arrays>
-            <Dropdown
-              options={dropdownOptions}
-              value={dropdownValues}
-              onChange={(selected) => setDropdownValues(selected)}
-              variant="text"
-              isMulti={false}
-              size={'sm'}
-            />
+            {/* dropdownpopover module */}
+            <Popover
+              className={`${dropdownPopoverStyles.btn} ${styles.drop_btn}`}
+              popoverContent={<DropdownPopoverCompoment />}
+              side="bottom"
+              align="end"
+              sideOffset={5}
+            >
+              <span>{'년도별 보기'}</span>
+              <IcoArrowDown width={16} height={16} stroke="#131C30" />
+            </Popover>
           </div>
         </div>
         <div className={styles.education_box}>
--- apps/pub/fo/src/pages/_guide/guide/popover.tsx
+++ apps/pub/fo/src/pages/_guide/guide/popover.tsx
@@ -1,6 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { useState } from 'react';
-import { Popover } from '@learnway/ui';
+import { IcoArrowDown } from '@learnway/icons';
+import { Popover, Button } from '@learnway/ui';
+
+import dropdownPopoverStyles from '../../../shared/ui/dropdown-popover/dropdown-popover.module.css';
 
 export const Route = createFileRoute('/_guide/guide/popover')({
   component: RouteComponent,
@@ -8,6 +11,17 @@ export const Route = createFileRoute('/_guide/guide/popover')({
 
 const PopoverContent = () => {
   return <div>팝오버 내용</div>;
+};
+
+const DropdownPopoverCompoment = () => {
+  return (
+    <div className={`${dropdownPopoverStyles.start} ${dropdownPopoverStyles.dropdown_wrap}`}>
+      <Button>년도별 보기1</Button>
+      <Button>년도별 보기2</Button>
+      <Button>년도별 보기3</Button>
+      <Button>년도별 보기444444444</Button>
+    </div>
+  );
 };
 
 function RouteComponent() {
@@ -70,6 +84,76 @@ export const PopOverCompoment = memo(PopOverCompoment);`}</code>
           </div>
         </div>
       </div>
+
+      <div className="code_example">
+        <pre className="code_block">
+          <code>{`// import 
+import { memo } from 'react';
+import { Popover, Button } from '@learnway/ui';
+import dropdownPopoverStyles from '../../../shared/ui/dropdown-popover/dropdown-popover.module.css';
+
+const DropdownPopoverCompoment = () => {
+  return (
+    <div className={\`\${dropdownPopoverStyles.start} \${dropdownPopoverStyles.dropdown_wrap}\`}>
+        <Button>년도별 보기1</Button>
+        <Button>년도별 보기2</Button>
+        <Button>년도별 보기3</Button>
+        <Button>년도별 보기444444444</Button>
+      </div>
+  );
+};
+
+const PopOverCompoment = () => {
+  
+  return (
+    <Popover
+      popoverContent={<DropdownPopoverCompoment />}
+      className={dropdownPopoverStyles.btn}
+      side="bottom"
+      align="end"
+      sideOffset={10}>
+        <span>{'popover dropdown 형식'}</span>
+        <IcoArrowDown width={16} height={16} stroke="#131C30" />
+    </Popover>
+  );
+};
+
+export const PopOverCompoment = memo(PopOverCompoment);`}</code>
+        </pre>
+      </div>
+
+      <div className="group">
+        <h3 className="guide_tit3">popover</h3>
+
+        <div className="flex_box">
+          <div className="desc w-full">
+            <Popover
+              popoverContent={<DropdownPopoverCompoment />}
+              className={dropdownPopoverStyles.btn}
+              side="bottom"
+              align="end"
+              sideOffset={10}
+            >
+              <span>popover dropdown 형식</span>
+              <IcoArrowDown width={16} height={16} stroke="#131C30" />
+            </Popover>
+          </div>
+        </div>
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
