ad4f2afc7f7a2981a8b621572da111f0b1352eb1
김석태 <9496491@ict-companion.com>
2025-04-22 14:32:20 +0900

    chore: features 폴더 구조 변경

---
+++ apps/bo/src/features/tenant/ui/tenant-detail-menu.tsx
@@ -0,0 +1,43 @@
+import { FC, useState } from 'react';
+
+import styles from '@learnway/styles/bo/assets/styles/modules/page-contents.module.css';
+import { Tabs } from '@learnway/ui';
+import { TenantDetailMenuTree } from './tenant-detail-menu-tree';
+
+const TenantDetailMenuComponent: FC<any> = () => {
+  const [selectedTabKey, setSelectedTabKey] = useState<string>('FO');
+
+  const renderTabContent = () => {
+    return <TenantDetailMenuTree menuScope={selectedTabKey} />;
+  };
+  const tabItems = [
+    {
+      title: '학습자 메뉴',
+      key: 'FO',
+      content: renderTabContent(),
+    },
+    {
+      title: 'HRD센터 메뉴',
+      key: 'BO',
+      content: renderTabContent(),
+    },
+  ];
+  const handleTabChange = (tabKey: string) => {
+    if (tabKey !== selectedTabKey) {
+      setSelectedTabKey(tabKey);
+    }
+  };
+
+  return (
+    <Tabs
+      items={tabItems}
+      type="line"
+      size={'sm'}
+      className={styles.tab_wrap}
+      selectedTabKey={selectedTabKey}
+      onTabChange={handleTabChange}
+    />
+  );
+};
+
+export const TenantDetailMenu = TenantDetailMenuComponent;
