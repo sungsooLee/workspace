6381ae3f9aae089ddf52307d7636fef17d65b534
김정민/협력사 <9496487@ict-companion.com>
2025-04-22 13:28:21 +0900

    feat: 테넌트 상세 카테고리 관리 메뉴 추가

---
+++ apps/bo/src/features/platform/ui/platform/tenant/tenant-detail-category.tsx
@@ -0,0 +1,18 @@
+import { FC, useState } from 'react';
+import { t } from 'i18next';
+import {
+  Button,
+  ContentsRow,
+  Textarea,
+  Switch,
+  Tooltip,
+  Input,
+  TreeView,
+  TreeNode,
+} from '@learnway/ui';
+
+const TenantDetailCategoryComponent: FC<any> = ({ menuScope }) => {
+  return 'tenant category mapping';
+};
+
+export const TenantDetailCategory = TenantDetailCategoryComponent;
