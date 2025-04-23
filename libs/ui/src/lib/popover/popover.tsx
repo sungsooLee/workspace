4160af9ace55c0a312d09d284e71d29d7f26535e
배성련/협력사 <9480335@ict-companion.com>
2025-04-23 18:40:16 +0900

    feat: [PMS_1030] useSessionTimout

--- libs/ui/src/lib/popover/popover.tsx
+++ libs/ui/src/lib/popover/popover.tsx
@@ -4,6 +4,8 @@ import { cn } from '@learnway/shared';
 import * as Primitive from '@radix-ui/react-popover';
 import styles from './popover.module.css';
 
+import { useModalStore } from '../stores/useModalStore';
+
 interface PopoverComponentProps extends Primitive.PopoverContentProps {
   children: ReactNode;
   popoverContent?: ReactNode;
@@ -11,27 +13,64 @@ interface PopoverComponentProps extends Primitive.PopoverContentProps {
   open?: boolean;
   side?: 'top' | 'right' | 'bottom' | 'left';
   onOpenChange?: (open: boolean) => void;
+  // modal 에서 popover 사용 시 FocusOutside 이벤트 예외 처리
+  forceCloseFocusOutside?: boolean;
 }
 
 const PopoverComponent = forwardRef<
   React.ElementRef<typeof Primitive.Popover>,
   PopoverComponentProps
->(({ children, className, popoverContent, open, onOpenChange, ...props }, ref) => {
-  return (
-    <Primitive.Root open={open} onOpenChange={onOpenChange}>
-      <Primitive.PopoverTrigger className={cn('nlp--popover-trigger', className)}>
-        {children}
-      </Primitive.PopoverTrigger>
-      <Primitive.Portal>
-        <Primitive.Content
-          className={cn(styles.popover_content, 'nlp--popover-content', className)}
-          {...props}>
-          {popoverContent}
-        </Primitive.Content>
-      </Primitive.Portal>
-    </Primitive.Root>
-  );
-});
+>(
+  (
+    {
+      children,
+      className,
+      popoverContent,
+      open,
+      onOpenChange,
+      forceCloseFocusOutside = false,
+      ...props
+    },
+    ref,
+  ) => {
+    const { modals } = useModalStore();
+
+    return (
+      <Primitive.Root
+        open={open}
+        onOpenChange={(op) => {
+          onOpenChange?.(op);
+        }}
+      >
+        <Primitive.PopoverTrigger className={cn('nlp--popover-trigger', className)}>
+          {children}
+        </Primitive.PopoverTrigger>
+        <Primitive.Portal>
+          <Primitive.Content
+            className={cn(styles.popover_content, 'nlp--popover-content', className)}
+            {...props}
+            onInteractOutside={(e) => {
+              // popover open 상태에서 modal open 시 modal content event 버블링 문제
+              if (!forceCloseFocusOutside && modals?.length > 0) {
+                e.preventDefault();
+                e.stopPropagation();
+              }
+            }}
+            onFocusOutside={(e) => {
+              // popover open 상태에서 modal open 시 modal content event 버블링 문제
+              if (!forceCloseFocusOutside && modals?.length > 0) {
+                e.preventDefault();
+                e.stopPropagation();
+              }
+            }}
+          >
+            {popoverContent}
+          </Primitive.Content>
+        </Primitive.Portal>
+      </Primitive.Root>
+    );
+  },
+);
 
 const PopoverRoot = PopoverComponent;
 export const Popover = Object.assign(PopoverRoot, {
