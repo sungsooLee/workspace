1df61cc1f3c6ddb26563d1eb44b36a95136a9ac2
김태경/협력사 <9488416@ict-companion.com>
2025-04-25 18:06:43 +0900

    [퍼블]학습창 레이아웃 추가

--- apps/pub/fo/src/widgets/layout/index.ts
+++ apps/pub/fo/src/widgets/layout/index.ts
@@ -2,3 +2,4 @@ export * from './ui/auth/auth-layout';
 export * from './ui/main/layout';
 export * from './m.ui/main/layout';
 export * from './service/menu.service';
+export * from './ui/learning/learning-layout';
---
+++ apps/pub/fo/src/widgets/layout/ui/learning/learning-layout.tsx
@@ -0,0 +1,34 @@
+import { memo, ReactNode } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useLocation } from '@tanstack/react-router';
+import { cn } from '@learnway/shared';
+
+import { LearningHeader } from './learning-header/learning-header';
+import { LearningContainer } from './learning-container/learning-container';
+import styles from './learning-layout.module.css';
+
+import { MobileView, BrowserView } from 'react-device-detect';
+
+interface LearningLayoutComponentProps {
+  children: ReactNode;
+}
+
+function LearningLayoutComponent({ children }: LearningLayoutComponentProps) {
+  return (
+    <>
+      <BrowserView>
+        <LearningHeader />
+      </BrowserView>
+
+      <div className={`${styles.start} ${styles.container}`}>
+        <div className={styles.inner}>
+          <main>
+            <LearningContainer>{children}</LearningContainer>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export const LearningLayout = memo(LearningLayoutComponent);
