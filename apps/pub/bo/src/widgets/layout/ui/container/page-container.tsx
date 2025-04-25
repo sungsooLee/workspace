7048f044f72b44bbe548def810d6b7ce1b425d61
협력사/이성수 <9490817@ict-companion.com>
2025-04-25 15:29:52 +0900

    [퍼블] bo 작업 업로드

--- apps/pub/bo/src/widgets/layout/ui/container/page-container.tsx
+++ apps/pub/bo/src/widgets/layout/ui/container/page-container.tsx
@@ -95,7 +95,7 @@ function PageContainerComponent({
   };
 
   return (
-    <div className={`${styles.start} ${styles.contents}`}>
+    <div className={cn(styles.start, styles.contents)}>
       <Breadcrumbs />
       <div ref={scrollContainerRef} className={cn(styles.inner, 'scroll_inner')}>
         {/* title_wrap */}
