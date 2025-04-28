2c90404b40b59b78556cbe62ad5da8ffe47f16b2
이창기/협력사 <9496504@ict-companion.com>
2025-04-28 11:09:51 +0900

    feat: [BO_TMS_1001_19] 회사선택 셔틀 팝업

--- apps/bo/src/features/shared/index.ts
+++ apps/bo/src/features/shared/index.ts
@@ -1,2 +1,3 @@
 export * from './ui/modal/channel-choice-modal';
 export * from './ui/modal/manager-choice-modal';
+export * from './ui/modal/company-choice-modal';
---
+++ apps/bo/src/features/shared/ui/modal/company-choice-modal.tsx
@@ -0,0 +1,144 @@
+import { t } from 'i18next';
+import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
+
+import { SearchBoxConfig, useSearchBox } from '@learnway/hooks';
+import { Button, ModalBody, ModalContainer, ModalFooter, ModalTitle, useModal } from '@learnway/ui';
+
+import { SearchBox } from '@shared/ui/search-box';
+
+import { TransferGrid, TransferGridImperative } from '@learnway/ui';
+import { IcoRefresh02 } from '@learnway/icons';
+
+import { useRef, useState } from 'react';
+
+const dummyData = Array(10)
+  .fill(null)
+  .map((_, i) => ({ id: `id${i}`, companyName: `name${i}` }));
+
+const CompanyChoice = () => {
+  const ref = useRef<TransferGridImperative>(null);
+
+  const { close } = useModal();
+
+  const { provider: sProvider } = useSearchBox(searchConfig);
+  const [option, setOption] = useState<any>();
+
+  const columnHelper = createColumnHelper();
+  const columns = [
+    columnHelper.accessor('id', {
+      header: t('회사구분'),
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor('companyName', {
+      header: t('회사명'),
+      cell: (info) => info.getValue(),
+      meta: {
+        headerAlign: 'left', // 헤더만 가운데 정렬
+        cellAlign: 'left', // 셀은 오른쪽 정렬
+      },
+    }),
+  ] as ColumnDef<any, unknown>[];
+
+  /**
+   * @param data
+   */
+  const handleOnSearch = (data: any) => {
+    console.log('data', data);
+    //TODO fetch
+  };
+
+  const handleOnClose = () => {
+    close();
+  };
+  const handleOnConfirm = () => {
+    if (!option) return;
+    close(option);
+  };
+
+  return (
+    <ModalContainer className="h-[740]">
+      <ModalTitle>{t('회사 선택')}</ModalTitle>
+      <ModalBody>
+        <SearchBox provider={sProvider} onSearch={handleOnSearch} />
+        <TransferGrid
+          ref={ref}
+          onChange={(data: any) => {
+            console.log('data :::: ', data);
+            setOption(data);
+          }}
+          showNumberingColumn={true}
+          gridData={dummyData}
+          columns={columns}
+          rowKey={'id'}
+          leftTitle={t('회사 목록')}
+          rightTitle={t('회사 선택')}
+        />
+      </ModalBody>
+      <ModalFooter>
+        <Button
+          icon={<IcoRefresh02 width={16} height={16} className="icon_refresh" />}
+          variant={'gray'}
+          size={'lg'}
+          onClick={() => {
+            ref.current?.resetSelection();
+          }}
+        >
+          {t('초기화')}
+        </Button>
+        <Button label={t('취소')} variant={'gray'} size={'lg'} onClick={handleOnClose} />
+        <Button
+          type={'button'}
+          label={t('확인')}
+          variant={'primary'}
+          size={'lg'}
+          onClick={handleOnConfirm}
+        />
+      </ModalFooter>
+    </ModalContainer>
+  );
+};
+
+export const CompanyChoiceModal = CompanyChoice;
+
+const searchConfig: SearchBoxConfig = {
+  builders: [
+    [
+      {
+        name: 'companyGroup',
+        type: 'dropdown',
+        label: t('회사구분'),
+        value: undefined,
+        options: [
+          { value: 'company-g-cd-A', label: t('회사구분A') },
+          { value: 'company-g-cd-B', label: t('회사구분B') },
+          { value: 'company-g-cd-C', label: t('회사구분C') },
+          { value: 'company-g-cd-D', label: t('회사구분D') },
+          { value: 'company-g-cd-E', label: t('회사구분E') },
+          { value: 'company-g-cd-F', label: t('회사구분F') },
+        ],
+        dropdownConfig: {
+          isSearchable: true,
+          placeholder: t('입력 선택'),
+        },
+      },
+      {
+        name: 'company',
+        type: 'dropdown',
+        label: t('회사'),
+        value: undefined,
+        options: [
+          { value: 'company-cd-A', label: t('회사A') },
+          { value: 'company-cd-B', label: t('회사B') },
+          { value: 'company-cd-C', label: t('회사C') },
+          { value: 'company-cd-D', label: t('회사D') },
+          { value: 'company-cd-E', label: t('회사E') },
+          { value: 'company-cd-F', label: t('회사F') },
+        ],
+        dropdownConfig: {
+          isSearchable: true,
+          placeholder: t('입력 선택'),
+        },
+      },
+    ],
+  ],
+};
