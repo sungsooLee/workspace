ba076c1b24b19d6ee716c737532238c4141784bb
협력사/이성수 <9490817@ict-companion.com>
2025-04-29 15:26:28 +0900

    [퍼블] bo 작업 업로드

--- apps/pub/bo/src/pages/_layout/pms/-tabcontents/user-group-setting.tsx
+++ apps/pub/bo/src/pages/_layout/pms/-tabcontents/user-group-setting.tsx
@@ -1,8 +1,17 @@
 import { FC } from 'react';
 import { cn } from '@learnway/shared';
+import { ChipList, SelectOption } from '@learnway/ui';
 // eslint-disable-next-line no-empty-pattern
 const UserGroupSettingComponent: FC<{}> = ({}) => {
-  return <div></div>;
+  const options: SelectOption[] = [
+    { label: '채널 - 채널1 > 유저그룹명1', value: 'A' },
+    { label: '조직 - 테넌트A > 현대자동차 >  경영지원본부 > 경영지원1팀', value: 'B' },
+  ];
+  return (
+    <div>
+      <ChipList options={options} size="xs" />
+    </div>
+  );
 };
 
 UserGroupSettingComponent.displayName = 'UserGroupSetting';
