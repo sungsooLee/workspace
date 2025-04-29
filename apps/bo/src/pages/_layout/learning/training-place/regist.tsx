b55ca710730b7129fc53fdac461613c53697e635
김정민/협력사 <9496487@ict-companion.com>
2025-04-29 12:28:26 +0900

    feat: [NLP_BO_EDO_1603] 교육 장소 등록 UI 추가

--- apps/bo/src/pages/_layout/learning/training-place/regist.tsx
+++ apps/bo/src/pages/_layout/learning/training-place/regist.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
+import { t } from 'i18next';
 import { createFileRoute } from '@tanstack/react-router';
 import { cn } from '@learnway/shared';
 import { IcoRefresh02, IcoSearch, IcoFormRequired, IcoTrash03, IcoPpt } from '@learnway/icons';
 import { PageContainer } from '@widgets/layout/ui/container/page-container';
-import { FormSubTitle } from '@shared/ui';
+import { ContentsHistoryInfoFormField, FormSubTitle } from '@shared/ui';
 
 import styles from '@learnway/styles/bo/assets/styles/modules/page-contents.module.css';
 import formStyles from '@learnway/styles/bo/assets/styles/modules/form.module.css';
@@ -11,6 +12,7 @@ import searchStyles from '@learnway/styles/bo/assets/styles/modules/search-box.m
 import popupStyles from '@learnway/styles/bo/assets/styles/modules/popup-contents.module.css';
 import dynamicFormStyles from '@learnway/styles/bo/assets/styles/modules/dynamic.form.module.css';
 import fileUploadStyles from '@learnway/styles/bo/assets/styles/modules/file-upload.module.css'; // 파일 업로드
+import { ChipListModalSelectorFormField } from '@learnway/ui';
 
 import {
   Button,
@@ -23,11 +25,17 @@ import {
   ModalFooter,
   ModalTitle,
   useModal,
-  GridBox,
+  DynamicFormField,
   Textarea,
   RadioGroupFormField,
 } from '@learnway/ui';
 import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
+import { MainContents } from '@widgets/layout/ui/container/slot/main-contents';
+import { useDynamicForm } from '@/libs/hooks/src';
+import { FormRow } from '@shared/ui';
+import { DynamicFormConfig } from '@/libs/hooks/src';
+import { ContentsButtons } from '@widgets/layout/ui/container/slot/contents-buttons';
+import { LinkBox } from '@widgets/layout/ui/container/slot/link-box';
 
 export const Route = createFileRoute('/_layout/learning/training-place/regist')({
   component: RouteComponent,
@@ -41,85 +49,60 @@ function RouteComponent() {
     { value: 'option3', label: '옵션 3' },
   ];
 
+  const { provider, fetchData, onSubmit, onFormChange, getValues, clearFormError, setFormError } =
+    useDynamicForm(formConfig);
+
   return (
     <form className="form_row">
       <PageContainer>
-        {/* main_contents */}
-        <div className={styles.main_contents}>
+        <ContentsButtons>
+          <LinkBox>
+            <Button variant="point" size="sm">
+              {t('목록')}
+            </Button>
+          </LinkBox>
+
+          <Button variant="point" size="sm">
+            {t('초기화')}
+          </Button>
+          <Button variant="primary" size="sm">
+            {t('저장')}
+          </Button>
+        </ContentsButtons>
+        <MainContents>
           <FormSubTitle label={'교육장소 정보 '} />
           <ContentsRow>
-            {/* form_item */}
-            <div className={formStyles.form_item}>
-              <label htmlFor="name-sort" className={formStyles.form_label}>
-                <span className={formStyles.form_text}>구분</span>
-                {/* 필수 케이스 */}
-                <span className={cn(formStyles.status, formStyles.required)}>
-                  <IcoFormRequired width={12} height={12} />
-                </span>
-              </label>
-              <div className={formStyles.input_box}>
-                {/* 50% 만 적용인 경우 */}
-                <div className={dynamicFormStyles.w_half}>
-                  <Dropdown
-                    options={options}
-                    value={selectedValues}
-                    onChange={(selected) => setSelectedValues(selected)}
-                    variant="default"
-                    size={'sm'}
-                    placeholder="선택"
-                  />
-                </div>
-              </div>
-            </div>
+            <FormRow provider={provider} className={dynamicFormStyles.w_half}>
+              <DynamicFormField name={'placeDivision'} />
+            </FormRow>
           </ContentsRow>
           <ContentsRow>
-            {/* form_item */}
-            <div className={formStyles.form_item}>
-              <label htmlFor="name-locationCode" className={formStyles.form_label}>
-                <span className={formStyles.form_text}>장소 코드</span>
-                {/* 필수 케이스 */}
-                <span className={cn(formStyles.status, formStyles.required)}>
-                  <IcoFormRequired width={12} height={12} />
-                </span>
-              </label>
-              <div className={formStyles.input_box}>
-                <Input id="name-locationCode" type="text" placeholder="입력" />
-              </div>
-            </div>
-            {/* form_item */}
-            <div className={formStyles.form_item}>
-              <label htmlFor="name-locationName" className={formStyles.form_label}>
-                <span className={formStyles.form_text}>장소 명</span>
-                {/* 필수 케이스 */}
-                <span className={cn(formStyles.status, formStyles.required)}>
-                  <IcoFormRequired width={12} height={12} />
-                </span>
-              </label>
-              <div className={formStyles.input_box}>
-                <Input id="name-locationName" type="text" placeholder="입력" />
-              </div>
-            </div>
+            <FormRow provider={provider} className={dynamicFormStyles.w_half}>
+              <DynamicFormField name={'placeCode'} />
+            </FormRow>
+            <FormRow provider={provider} className={dynamicFormStyles.w_half}>
+              <DynamicFormField name={'placeName'} />
+            </FormRow>
           </ContentsRow>
           <ContentsRow>
-            {/* form_item */}
-            <div className={formStyles.form_item}>
-              <label htmlFor="name-tenantName" className={formStyles.form_label}>
-                <span className={formStyles.form_text}>테넌트 명</span>
-                {/* 필수 케이스 */}
-                <span className={cn(formStyles.status, formStyles.required)}>
-                  <IcoFormRequired width={12} height={12} />
-                </span>
-              </label>
-              <div className={formStyles.input_box}>
-                <InputModalSelectorFormField
+            <FormRow provider={provider}>
+              <DynamicFormField name={'tenantName'}>
+                <ChipListModalSelectorFormField
+                  chipList={{
+                    labelField: 'name',
+                    valueField: 'value',
+                    hideBorder: true,
+                  }}
                   modalConfig={{
+                    title: '',
                     width: 'xl',
-                    content: <ModalHistoryInfoContent />,
+                    content: '',
                   }}
                 />
-              </div>
-            </div>
+              </DynamicFormField>
+            </FormRow>
           </ContentsRow>
+
           <ContentsRow>
             {/* form_item */}
             <div className={formStyles.form_item}>
@@ -170,75 +153,112 @@ function RouteComponent() {
               </p>
             </div>
             {/* form_item */}
-            <div className={formStyles.form_item}>
-              <label htmlFor="name-address" className={formStyles.form_label}>
-                <span className={formStyles.form_text}>링크 주소</span>
-              </label>
-              <div className={formStyles.input_box}>
-                <Input
-                  id="name-address"
-                  type="text"
-                  placeholder="http:// 또는 https:// 전체 URL을 입력하세요."
-                />
-              </div>
-            </div>
+            <FormRow provider={provider} className={dynamicFormStyles.w_half}>
+              <DynamicFormField name={'linkAddress'} />
+            </FormRow>
           </ContentsRow>
           <ContentsRow>
-            {/* Textarea type */}
-            <div className={formStyles.form_item}>
-              <label htmlFor="name-note" className={formStyles.form_label}>
-                <span className={formStyles.form_text}>비고</span>
-              </label>
-              <div className={formStyles.input_box}>
-                <Textarea
-                  id="name-note"
-                  rows={5}
-                  cols={33}
-                  resize="none"
-                  placeholder="비고 내용을 입력하세요."
-                  size="sm"
-                  maxLength={2000}
-                  className={formStyles.textarea}
-                />
-              </div>
-              <p className={cn(formStyles.guide_text)}>{'※ 사용자에게 노출되지 않습니다.'}</p>
-            </div>
+            <FormRow provider={provider}>
+              <DynamicFormField name={'description'} size="sm" resize="none" />
+            </FormRow>
           </ContentsRow>
+
           <ContentsRow>
-            <div className={formStyles.form_item}>
-              <label htmlFor="name-reservation" className={formStyles.form_label}>
-                <span className={formStyles.form_text}>예약 가능</span>
-              </label>
-              <div className={formStyles.input_box}>
-                <div className={dynamicFormStyles.radio_wrap}>
-                  <RadioGroupFormField
-                    options={[
-                      { value: 'y', label: '예약 가능' },
-                      { value: 'n', label: '예약 불가' },
-                    ]}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className={formStyles.form_item}>
-              <label htmlFor="name-use" className={formStyles.form_label}>
-                <span className={formStyles.form_text}>사용 가능</span>
-              </label>
-              <div className={formStyles.input_box}>
-                <div className={dynamicFormStyles.radio_wrap}>
-                  <RadioGroupFormField
-                    options={[
-                      { value: 'y', label: '사용 가능' },
-                      { value: 'n', label: '사용 불가' },
-                    ]}
-                  />
-                </div>
-              </div>
-            </div>
+            <FormRow provider={provider} className={dynamicFormStyles.w_half}>
+              <DynamicFormField name={'isReservationUsed'} />
+            </FormRow>
+            <FormRow provider={provider} className={dynamicFormStyles.w_half}>
+              <DynamicFormField name={'isUsed'} />
+            </FormRow>
           </ContentsRow>
-          <ContentsHistoryInfoFormField />
-        </div>
+
+          <ContentsRow className={cn(formStyles.no_line, formStyles.space2)}>
+            <ContentsHistoryInfoFormField />
+          </ContentsRow>
+        </MainContents>
       </PageContainer>
     </form>
   );
 }
+
+const formConfig: DynamicFormConfig = {
+  builders: [
+    {
+      name: 'placeDivision',
+      type: 'dropdown',
+      label: t('구분'),
+      value: '',
+      options: [
+        { label: '선택', value: '' },
+        { value: 'campus', label: t('캠퍼스') },
+        { value: 'option1', label: t('서비스기술교육') },
+        { value: 'option2', label: t('생기클러스터') },
+        { value: 'external', label: t('외부') },
+        { value: 'overseas', label: t('해외') },
+      ],
+    },
+    {
+      name: 'placeCode',
+      type: 'text',
+      label: t('장소 코드'),
+      value: '',
+      placeholder: '입력',
+    },
+    {
+      name: 'placeName',
+      type: 'text',
+      label: t('장소 명'),
+      value: '',
+      placeholder: '입력',
+    },
+    {
+      name: 'tenantName',
+      type: 'custom',
+      label: t('테넌트 명'),
+      value: '',
+    },
+    {
+      name: 'linkAddress',
+      type: 'text',
+      label: t('링크 주소'),
+      value: '',
+      placeholder: 'http:// 또는 https:// 전체 URL을 입력하세요.',
+    },
+    {
+      name: 'description',
+      type: 'textarea',
+      label: t('비고'),
+      value: '',
+      maxLength: 2000,
+      placeholder: '비고 내용을 입력하세요.',
+      guideText: '※ 사용자에게 노출되지 않습니다.',
+    },
+    {
+      name: 'isReservationUsed',
+      type: 'radio-group',
+      label: t('예약 가능'),
+      value: 'Y',
+      options: [
+        { value: 'Y', label: '예약 가능' },
+        { value: 'N', label: '예약 불가' },
+      ],
+    },
+    {
+      name: 'isUsed',
+      type: 'radio-group',
+      label: t('사용 가능'),
+      value: 'Y',
+      options: [
+        { value: 'Y', label: '사용 가능' },
+        { value: 'N', label: '사용 불가' },
+      ],
+    },
+  ],
+  validator: {
+    placeDivision: { required: true },
+    placeCode: { required: true },
+    placeName: { required: true },
+    tenantName: { required: true },
+    isUsed: { required: true },
+  },
+};
