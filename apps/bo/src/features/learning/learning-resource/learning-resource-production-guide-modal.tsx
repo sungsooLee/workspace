ea5abe45fa4bf4638a98eb6d15de23595fe7fd28
협력사/김석태 <9496491@ict-companion.com>
2025-04-30 14:45:55 +0900

    feat: [NLP_BO_CMS_1056] 제작 프로그램/가이드 다운로드 팝업 (고정 page 로 url 목록 차후 등록 필요

---
+++ apps/bo/src/features/learning/learning-resource/learning-resource-production-guide-modal.tsx
@@ -0,0 +1,79 @@
+import { FC } from 'react';
+import { t } from 'i18next';
+import {
+  ModalBody,
+  ModalContainer,
+  ModalFooter,
+  ModalTitle,
+  useModal,
+  Button,
+  GridBox,
+} from '@learnway/ui';
+import popupStyles from '@learnway/styles/bo/assets/styles/modules/popup-contents.module.css';
+
+const LearningResourceProductionGuideModalComponent: FC<any> = () => {
+  const { open: openModal, close: closeModal, confirm: openConfirm } = useModal();
+  return (
+    <ModalContainer>
+      <ModalTitle>{t('프로그램/가이드 다운로드')}</ModalTitle>
+      <ModalBody>
+        <div className={popupStyles.wrap}>
+          <div className={popupStyles.container}>
+            <GridBox
+              config={gridConfig}
+              height={310}
+              showColumnSettings={false}
+              showTotalCount={true}
+              hideRowSelectionCheckBox={false}
+              title={t('공유현황')}
+            />
+          </div>
+        </div>
+      </ModalBody>
+      <ModalFooter>
+        <Button label={t('적용')} variant={'primary'} size={'lg'} onClick={() => closeModal()} />
+      </ModalFooter>
+    </ModalContainer>
+  );
+};
+
+export const LearningResourceProductionGuideModal = LearningResourceProductionGuideModalComponent;
+
+const handleDownload = (fileName: string, fileUrl: string) => {
+  const link = document.createElement('a');
+
+  link.href = fileUrl;
+  link.download = fileName;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
+
+const gridConfig = {
+  data: [
+    {
+      fileName: t('테스트 문서 1'),
+      downloadButton: (
+        <Button
+          className="link"
+          onClick={() =>
+            handleDownload(
+              'test.epub',
+              'https://mond-al.github.io/assets/images/forTest/ratio/all_ratio.zip',
+            )
+          }
+        >
+          {t('다운로드')}
+        </Button>
+      ),
+    },
+    {
+      fileName: t('자료 없음'),
+      downloadButton: <Button className="link">{t('다운로드')}</Button>,
+    },
+  ],
+  columns: [
+    { name: 'fileName', sortBy: true, label: t('파일명') },
+    { name: 'downloadButton', sortBy: true, label: t('다운로드') },
+  ],
+};
