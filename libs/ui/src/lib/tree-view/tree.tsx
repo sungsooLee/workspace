abe1be1af40dedda3f5be70bf09894c98a1344a2
협력사/이성수 <9490817@ict-companion.com>
2025-05-02 16:55:06 +0900

    [퍼블] bo 작업 업로드

--- libs/ui/src/lib/tree-view/tree.tsx
+++ libs/ui/src/lib/tree-view/tree.tsx
@@ -534,7 +534,7 @@ const TreeNodeComponent = ({
           <div className={styles.drag_wrap}>
             {nodeButtons && (
               <div
-                className={`mr-2 flex space-x-1 transition-opacity duration-150 ${isHovered || level === 0 ? 'opacity-100' : 'invisible opacity-0'}`}
+                className={`flex space-x-1 transition-opacity duration-150 ${isHovered || level === 0 ? 'opacity-100' : 'invisible opacity-0'}`}
                 onClick={(e) => e.stopPropagation()}
               >
                 {nodeButtons(enhanceNode, level)}
@@ -542,7 +542,7 @@ const TreeNodeComponent = ({
             )}
             {level >= 1 && (
               <span
-                className={`ml-2 flex items-center justify-center text-5xl transition-opacity ${isDragAndDropMode && isActuallyDraggable ? 'cursor-grab' : 'cursor-pointer'}`}
+                className={`flex items-center justify-center text-5xl transition-opacity ${isDragAndDropMode && isActuallyDraggable ? 'cursor-grab' : 'cursor-pointer'}`}
                 draggable={true}
                 onDragStart={handleDragStart}
               >
