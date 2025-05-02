045610456d882d3fbf7bfb92e49e63c67b8599d0
협력사/이성수 <9490817@ict-companion.com>
2025-05-02 14:10:59 +0900

    [퍼블] bo 작업 업로드

--- apps/bo/src/features/learning/learning-resource/learning-resource-file-upload-modal.tsx
+++ apps/bo/src/features/learning/learning-resource/learning-resource-file-upload-modal.tsx
@@ -54,17 +54,19 @@ const LearningResourceFileUploadModalComponent: FC<Props> = ({ channel }) => {
             <p className={popupStyles.text}>{'파일은 최대 1개, 4G 이하로 업로드 가능합니다.'}</p>
           </div>
           <div className={popupStyles.pop_contents}>
-            <DndFileProgress
-              files={files}
-              maxFileCount={maxFileCount}
-              maxFileSize={maxFileSize}
-              addFiles={addFiles}
-              acceptFiles={acceptFiles}
-              onRemove={onRemove}
-              onPause={onPause}
-              onResume={onResume}
-              onRetry={onRetry}
-            />
+            <div className="dnd_file_wrap">
+              <DndFileProgress
+                files={files}
+                maxFileCount={maxFileCount}
+                maxFileSize={maxFileSize}
+                addFiles={addFiles}
+                acceptFiles={acceptFiles}
+                onRemove={onRemove}
+                onPause={onPause}
+                onResume={onResume}
+                onRetry={onRetry}
+              />
+            </div>
             <p className={cn(popupStyles.sub_text, popupStyles.dot)}>
               {'업로드된 동영상은 학습자원목록에서 조회가능합니다.'}
             </p>
