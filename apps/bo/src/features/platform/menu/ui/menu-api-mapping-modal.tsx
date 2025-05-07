7d45bb3e4f76a008e59b28ac1a6ed721438c6eb7
9497019 <9497019@ict-companion.com>
2025-05-07 16:50:39 +0900

    fix: tree-view shuttle-tree-to-chips 네이밍 변경 및 props 설정

--- apps/bo/src/features/platform/menu/ui/menu-api-mapping-modal.tsx
+++ apps/bo/src/features/platform/menu/ui/menu-api-mapping-modal.tsx
@@ -5,7 +5,7 @@ import {
   ModalContainer,
   ModalFooter,
   ModalTitle,
-  ShuttleTreeWithChips,
+  ShuttleTreeToChips,
   TreeNode,
   useModal,
 } from '@learnway/ui';
@@ -61,7 +61,8 @@ const MenuApiMappingModalComponent = ({ menuScopeCode, selectedApiKeys }: any) =
       <ModalBody>
         <div className={styles.wrap}>
           <div className={styles.pop_contents}>
-            <ShuttleTreeWithChips
+            <ShuttleTreeToChips
+              title="API 목록"
               sourceData={treeData as TreeNode[]}
               selectedItems={selectedItems}
               onItemsChange={handleSelectedItemsChange}
