124408aec1040a543d06ade64a369a83be34249c
이성수/협력사 <9490817@ict-companion.com>
2025-05-08 13:28:48 +0900

    [퍼블] bo 작업 업로드

--- apps/pub/bo/src/config/guide-data.ts
+++ apps/pub/bo/src/config/guide-data.ts
@@ -9,15 +9,15 @@
 // lastUpdateDate: 수정일
 // remarks: 비고
 export const guideData = [
-  {
-    screenName: '컨텐츠 레이아웃(상단 Tab)',
-    pageId: 'contents/layout',
-    screenId: '',
-    pageType: 'Page',
-    completionDate: '',
-    lastUpdateDate: '',
-    remarks: '작업중', // 비고
-  },
+  // {
+  //   screenName: '컨텐츠 레이아웃(상단 Tab)',
+  //   pageId: 'contents/layout',
+  //   screenId: '',
+  //   pageType: 'Page',
+  //   completionDate: '',
+  //   lastUpdateDate: '',
+  //   remarks: '작업중', // 비고
+  // },
   {
     screenName: '공통/공통기능_유저검색 외 공통팝업(텝)',
     pageId: 'common/pop-user-search',
