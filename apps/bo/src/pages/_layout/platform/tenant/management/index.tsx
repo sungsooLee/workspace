9798b31186f627122e145b7fda41bf58e02b476a
협력사/김석태 <9496491@ict-companion.com>
2025-05-09 19:14:49 +0900

    feat:[NLP_BO_TMS_1002] 플렛폼 / 테넌트 관리 페이지 추가

---
+++ apps/bo/src/pages/_layout/platform/tenant/management/index.tsx
@@ -0,0 +1,242 @@
+import { useState, useCallback } from 'react';
+import { createFileRoute, Link } from '@tanstack/react-router';
+import { t } from 'i18next';
+import { IcoRefresh02, IcoSearch } from '@learnway/icons';
+import { CellContext, ColumnDef, createColumnHelper } from '@tanstack/react-table';
+import { PageContainer } from '@widgets/layout/ui/container/page-container';
+import searchStyles from '@learnway/styles/bo/assets/styles/modules/search-box.module.css'; // search-box.module.css
+import boxStyles from '@learnway/styles/bo/assets/styles/modules/wrap-box.module.css'; // 하단 layout style - line
+import { SearchBox } from '@shared/ui/search-box';
+import { useRouter } from '@tanstack/react-router';
+
+import { cn } from '@learnway/shared';
+
+import { Button, GridBox, useGridBox, Input, DynamicFormField, Dropdown } from '@learnway/ui';
+import { useSearchBox, SearchBoxConfig } from '@learnway/hooks';
+import { MainContents } from '@widgets/layout/ui/container/slot/main-contents';
+import { ContentsButtons } from '@widgets/layout/ui/container/slot/contents-buttons';
+
+import { tenantQueryOptions } from '@entities/tenant/service/tenant.queries';
+
+export const Route = createFileRoute('/_layout/platform/tenant/management/')({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const router = useRouter();
+
+  const gridConfig = {
+    query: tenantQueryOptions.all,
+    columns: [
+      {
+        name: 'no1',
+        label: 'NO.',
+        type: 'numbering',
+      },
+      {
+        name: 'tenantName',
+        label: t('테넌트명'),
+        render: (info: any) => (
+          <Button
+            className="link"
+            onClick={() => {
+              router.navigate({
+                to: '/tenant/management/detail',
+                state: { tenantId: info.row.original.tenantId },
+              });
+            }}
+          >
+            {info.row.original.tenantName}
+          </Button>
+        ),
+      },
+      {
+        name: 'tenantSite',
+        label: t('테넌트 사이트'),
+        render: (info: any) => (
+          <Link to={info.getValue()} className="link">
+            {info.getValue()}
+          </Link>
+        ),
+      },
+      { name: 'company', label: t('회사') },
+      { name: 'hrdOwner', label: '테넌트 담당자' },
+      { name: 'companyNumber', label: '사용여부' },
+      { name: 'name', label: '등록자' },
+      { name: 'roleTerm', label: '등록일시' },
+      { name: 'tenure', label: '수정자' },
+      { name: 'roleStatus', label: '수정일시' },
+    ],
+    data: [
+      {
+        id: '1',
+        tenantId: 1,
+        tenantName: '테넌트A',
+        tenantSite: '/abcdefg',
+        channelName: '내 관리 채널명',
+        company: '현대자동차',
+        tenantOwner: '경영지원팀',
+        hrdOwner: '테넌트 담당자',
+        companyNumber: '1234567',
+        name: '김현대',
+        roleTerm: '2025-01-03 ~ 2025-01-03',
+        tenure: '재직',
+        roleStatus: '정상',
+      },
+      {
+        id: '1',
+        tenantId: 2,
+        tenantName: '테넌트B 2',
+        tenantSite: '/abcdefg',
+        channelName: '내 관리 채널명',
+        company: '현대자동차',
+        tenantOwner: '경영지원팀',
+        hrdOwner: '테넌트 담당자',
+        companyNumber: '1234567',
+        name: '김현대',
+        roleTerm: '2025-01-03 ~ 2025-01-03',
+        tenure: '재직',
+        roleStatus: '정상',
+      },
+    ],
+
+    pagination: {
+      pageSize: 10,
+      pageIndex: 0,
+      totalRows: 0,
+    },
+  };
+
+  const { provider: searchProvider, getValues } = useSearchBox(searchConfig);
+  const { config: gConfig, gridFetch, data } = useGridBox(gridConfig, getValues);
+
+  // useDynamicForm(formConfig);
+
+  const handleOnSearch = useCallback((data: any) => {
+    gridFetch(data);
+  }, []);
+  const handleNewTenant = useCallback(async () => {
+    if (!data) return;
+  }, [data]);
+
+  return (
+    <PageContainer>
+      <ContentsButtons>
+        <Button
+          variant="primary"
+          size="sm"
+          onClick={() => router.navigate({ to: '/platfomr/tenant/management/regist' })}
+        >
+          등록
+        </Button>
+      </ContentsButtons>
+      <MainContents>
+        <SearchBox provider={searchProvider} onSearch={handleOnSearch} />
+        <GridBox config={gConfig} />
+      </MainContents>
+    </PageContainer>
+  );
+}
+
+const searchConfig: SearchBoxConfig = {
+  builders: [
+    [
+      {
+        name: 'tenant',
+        type: 'dropdown',
+        label: t('테넌트명'),
+        value: '',
+        options: [
+          { value: 'tenantA', label: t('테넌트A') },
+          { value: 'tenantB', label: t('테넌트B') },
+          { value: 'tenantC', label: t('테넌트C') },
+          { value: 'tenantD', label: t('테넌트D') },
+          { value: 'tenantE', label: t('테넌트E') },
+          { value: 'tenantF', label: t('테넌트F') },
+        ],
+        dropdownConfig: {
+          // onChange: () => {
+          //   return '';
+          // },
+          isSearchable: true,
+          placeholder: '입력 선택',
+        },
+      },
+      {
+        name: 'company',
+        type: 'dropdown',
+        label: t('회사명'),
+        value: '',
+        options: [
+          { value: 'companyA', label: t('회사A') },
+          { value: 'companyB', label: t('회사B') },
+          { value: 'companyC', label: t('회사C') },
+          { value: 'companyD', label: t('회사D') },
+          { value: 'companyE', label: t('회사E') },
+          { value: 'companyF', label: t('회사F') },
+        ],
+        dropdownConfig: {
+          // onChange: () => {
+          //   return '';
+          // },
+          isSearchable: true,
+          placeholder: '입력 선택',
+        },
+      },
+      {
+        name: 'tenantOwner',
+        type: 'dropdown',
+        label: t('테넌트담당자'),
+        value: '',
+        options: [
+          { value: 'tenantOwner', label: t('회사A') },
+          { value: 'companyB', label: t('회사B') },
+          { value: 'companyC', label: t('회사C') },
+          { value: 'companyD', label: t('회사D') },
+          { value: 'companyE', label: t('회사E') },
+          { value: 'companyF', label: t('회사F') },
+        ],
+        dropdownConfig: {
+          // onChange: () => {
+          //   return '';
+          // },
+          isSearchable: true,
+          placeholder: '입력 선택',
+        },
+      },
+    ],
+    [
+      {
+        name: 'companyOwner',
+        type: 'dropdown',
+        label: t('회사 담당자'),
+        value: '',
+        options: [
+          { value: 'companyOwner1', label: t('회사담당1') },
+          { value: 'companyOwner2', label: t('회사담당2') },
+          { value: 'companyOwner3', label: t('회사담당3') },
+          { value: 'companyOwner4', label: t('회사담당4') },
+          { value: 'companyOwner5', label: t('회사담당5') },
+        ],
+        dropdownConfig: {
+          // onChange: () => {
+          //   return '';
+          // },
+          isSearchable: true,
+          placeholder: '입력 선택',
+        },
+      },
+      {
+        name: 'useable',
+        type: 'dropdown',
+        label: t('사용여부'),
+        value: 'ALL',
+        options: [
+          { value: 'ALL', label: t('전체') },
+          { value: 'Y', label: t('사용') },
+          { value: 'N', label: t('미사용') },
+        ],
+      },
+    ],
+  ],
+};
---
+++ apps/bo/src/pages/_layout/platform/tenant/management/regist.tsx
@@ -0,0 +1,390 @@
+import { useState, useRef } from 'react';
+import { t } from 'i18next';
+import { createFileRoute, useRouter } from '@tanstack/react-router';
+
+import { PageContainer } from '@widgets/layout/ui/container/page-container';
+import {
+  ContentsRow,
+  Button,
+  ChipListModalSelectorFormField,
+  DynamicFormField,
+  useModal,
+} from '@learnway/ui';
+import { FormRow } from '@shared/ui';
+import { ThumbnailUploaderFormField } from '@features/learning';
+import { DynamicFormConfig, useDynamicForm } from '@/libs/hooks/src';
+import { CompanyModal } from '@features/tenant/management/ui/company-modal';
+import { MainContents } from '@widgets/layout/ui/container/slot/main-contents';
+import { ContentsButtons } from '@widgets/layout/ui/container/slot/contents-buttons';
+import { LinkBox } from '@widgets/layout/ui/container/slot/link-box';
+
+import { useCreateTenant } from '@entities/tenant/service/tenant.hook';
+import { HrdUserInquiryModal } from '@features/shared/ui/modal/hrd-user-inquiry-modal';
+import {
+  DuplicateState,
+  DuplicateCheckInputFormField,
+} from '@features/tenant/management/ui/duplicate-check-input-form-field';
+import TenantService from '@entities/tenant/api/tenant';
+import { z as baseZ } from 'zod';
+
+export const Route = createFileRoute('/_layout/platform/tenant/management/regist')({
+  component: RouteComponent,
+});
+
+const duplicateCheck = async (value: string) => {
+  const result: any = await TenantService.fetchAllTenant({ tenantName: value });
+
+  return DuplicateState.ok;
+  // if (result.content || result.content.length > 0) return DuplicateState.duplicated;
+  // else return DuplicateState.ok;
+};
+
+function RouteComponent() {
+  const router = useRouter();
+  const { open: openModal, confirm: openConfirm } = useModal();
+  const { provider, onSubmit, onFormChange } = useDynamicForm(formConfig);
+  const { create } = useCreateTenant({});
+
+  const formRef = useRef<HTMLFormElement>(null);
+  const handleListButtonClick = () => {
+    router.navigate({ to: '/tenant/management' });
+  };
+
+  const handleSaveButtonClick = async () => {
+    const form = formRef.current;
+    if (form) {
+      form.dispatchEvent(new Event('submit', { cancelable: true, bubbles: true }));
+    }
+  };
+  const handleResetButtonClick = () => {
+    onFormChange();
+  };
+
+  const handleOnSubmit = async (data: any) => {
+    console.log('data {} => ', data);
+    const payload = {
+      ...data,
+      tenantName: data.tenantName.fieldValue,
+    };
+    console.log('data {} => ', payload);
+    if (await openConfirm('저장 하시겠습니까?')) {
+      create(payload);
+    }
+  };
+
+  return (
+    <PageContainer>
+      <ContentsButtons>
+        <LinkBox>
+          <Button onClick={handleListButtonClick} variant="point" size="sm">
+            {t('목록')}
+          </Button>
+        </LinkBox>
+
+        <Button onClick={handleResetButtonClick} variant="point" size="sm">
+          {t('초기화')}
+        </Button>
+        <Button variant="primary" size="sm" onClick={handleSaveButtonClick}>
+          {t('저장')}
+        </Button>
+      </ContentsButtons>
+      <MainContents>
+        <form ref={formRef} onSubmit={onSubmit(handleOnSubmit)}>
+          <div className="title_wrap">
+            <strong className="title">{t('기본 정보')}</strong>
+          </div>
+          <ContentsRow>
+            <FormRow provider={provider}>
+              <DynamicFormField name={'tenantName'}>
+                <DuplicateCheckInputFormField onDuplicationCheck={duplicateCheck} />
+              </DynamicFormField>
+            </FormRow>
+          </ContentsRow>
+          <ContentsRow>
+            <FormRow provider={provider}>
+              <DynamicFormField name="tenantLogo">
+                <ThumbnailUploaderFormField />
+              </DynamicFormField>
+            </FormRow>
+          </ContentsRow>
+          <ContentsRow>
+            <FormRow provider={provider}>
+              <DynamicFormField name={'managerName'}>
+                <ChipListModalSelectorFormField
+                  chipList={{
+                    labelField: 'name',
+                    valueField: 'id',
+                    wordwrap: true,
+                  }}
+                  modalConfig={{
+                    title: '',
+                    width: 'xl',
+                    content: <HrdUserInquiryModal />,
+                  }}
+                />
+              </DynamicFormField>
+            </FormRow>
+          </ContentsRow>
+          <ContentsRow>
+            <FormRow provider={provider}>
+              <DynamicFormField name={'tenantJungsanTag'} />
+            </FormRow>
+          </ContentsRow>
+          <ContentsRow>
+            <FormRow provider={provider}>
+              <DynamicFormField name={'company'}>
+                <ChipListModalSelectorFormField
+                  chipList={{
+                    labelField: 'name',
+                    valueField: 'value',
+                    hideBorder: true,
+                  }}
+                  modalConfig={{
+                    title: '',
+                    width: 'xl',
+                    content: <CompanyModal />,
+                  }}
+                />
+              </DynamicFormField>
+            </FormRow>
+          </ContentsRow>
+
+          <ContentsRow type={'horizontal'}>
+            <FormRow provider={provider}>
+              <DynamicFormField name={'isUsed'} />
+            </FormRow>
+          </ContentsRow>
+          <ContentsRow>
+            <FormRow provider={provider}>
+              <DynamicFormField name={'description'} resize="none" />
+            </FormRow>
+          </ContentsRow>
+          <div className="title_wrap no_line">
+            <strong className="title">{t('시스템 설정')}</strong>
+          </div>
+          <ContentsRow>
+            <FormRow provider={provider}>
+              <DynamicFormField name={'device'} disabled={true} />
+            </FormRow>
+          </ContentsRow>
+          <ContentsRow>
+            <FormRow provider={provider}>
+              <DynamicFormField name={'useCategory'} />
+            </FormRow>
+          </ContentsRow>
+          <ContentsRow>
+            <FormRow provider={provider}>
+              <DynamicFormField name={'language'} />
+            </FormRow>
+          </ContentsRow>
+        </form>
+      </MainContents>
+    </PageContainer>
+  );
+}
+
+const formConfig: DynamicFormConfig = {
+  builders: [
+    {
+      name: 'tenantName',
+      type: 'custom',
+      label: t('테넌트명'),
+      value: { fieldValue: '', checkState: DuplicateState.needInput },
+      format: 'object',
+      placeholder: '',
+      maxLength: 150,
+    },
+    {
+      label: t('테넌트 로고 (Size : 000x000)'),
+      name: 'tenantLogo',
+      type: 'custom',
+      format: 'array',
+      value: [],
+      tooltip: t('테넌트에 사용할 로고로 파일 1개만 등록할 수 있습니다.'),
+    },
+    {
+      name: 'managerName',
+      label: t('테넌트 담당자'),
+      type: 'custom',
+      value: '',
+      placeholder: t('담당자를 선택해주세요.'),
+    },
+    {
+      name: 'tenantJungsanTag',
+      type: 'text',
+      label: t('테넌트 정산 태그'),
+      value: '',
+      placeholder: '',
+      maxLength: 150,
+    },
+    {
+      name: 'company',
+      label: t('회사 선택'),
+      type: 'custom',
+      value: '',
+      tooltip: t(
+        '테넌트 소속 회사를 여러개 선택할 수 있습니다. 회사가 여러 개인 경우 회사별로 개별 설정이 필요합니다.',
+      ),
+    },
+    {
+      name: 'isUsed',
+      type: 'switch',
+      label: t('사용 여부'),
+      value: true,
+      format: 'boolean',
+      tooltip: t(
+        '테넌트 사용이 ON이면 학습자 사이트에 로그인 할 수 있으며, OFF이면 로그인 할 수 없습니다.',
+      ),
+      switchConfig: {
+        label: (value: boolean) => (value ? '사용' : '미사용'),
+      },
+      guideText: t('테넌트 사용 여부를 설정할 수 있습니다.'),
+    },
+    {
+      name: 'description',
+      type: 'textarea',
+      label: t('설명'),
+      value: '',
+      maxLength: 2000,
+      placeholder: '설명을 입력해 주세요.',
+    },
+    {
+      name: 'device',
+      type: 'checkbox-group',
+      label: t('디바이스'),
+      format: 'array',
+      tooltip: t(
+        'PC, 모바일, APP 모두 사용가능하며 과정 등록 시 PC, 모바일 학습 여부를 설정할 수 있습니다.',
+      ),
+      value: ['isWebExposed', 'isMobileExposed', 'isAppExposed'],
+      options: [
+        {
+          value: 'isWebExposed',
+          label: 'PC',
+        },
+        {
+          value: 'isMobileExposed',
+          label: 'Mobile',
+        },
+        {
+          value: 'isAppExposed',
+          label: 'APP',
+        },
+      ],
+      showSelectAll: true,
+    },
+    {
+      name: 'language',
+      type: 'checkbox-group',
+      label: t('언어'),
+      format: 'array',
+      tooltip: t(
+        '테넌트에서 사용할 언어를 선택하고, 선택한 언어에서 다국어 설정을 할 수 있습니다.',
+      ),
+      value: ['ko', 'en'],
+      options: [
+        { value: 'ko', label: '한국어', disabled: true },
+        { value: 'en', label: '영어', disabled: true },
+        { value: 'ne', label: '네팔어' },
+        { value: 'ms', label: '말레이어' },
+        { value: 'vi', label: '베트남어' },
+        { value: 'a1', label: '스페인어1' },
+        { value: 'a2', label: '스페인어2' },
+        { value: 'a3', label: '스페인어3' },
+        { value: 'a4', label: '스페인어4' },
+        { value: 'a5', label: '스페인어5' },
+        { value: 'a6', label: '스페인어6' },
+        { value: 'a7', label: '스페인어7' },
+        { value: 'a8', label: '스페인어8' },
+        { value: 'a9', label: '스페인어9' },
+      ],
+      showSelectAll: true,
+      cols: 6,
+    },
+    {
+      name: 'useCategory',
+      type: 'checkbox-group',
+      label: t('카테고리 사용 여부'),
+      format: 'array',
+      tooltip: '테넌트 - 카테고리 관리에서 사용할 카테고리를 선택할 수 있습니다',
+      value: ['common', 'tenant'],
+      options: [
+        {
+          value: 'common',
+          label: '공통 카테고리',
+        },
+        {
+          value: 'tenant',
+          label: '테넌트 카테고리',
+        },
+      ],
+      showSelectAll: true,
+    },
+  ],
+  validator: {
+    tenantName: {
+      format: 'object',
+      required: true,
+      conditions: [
+        {
+          fn: (values) => {
+            const fieldValue = values.tenantName.fieldValue;
+            if (fieldValue === '') return true;
+            return false;
+          },
+          message: t('LABEL.form.validation.needInput', { code: t('테넌트명') }),
+        },
+        {
+          fn: (values: Record<string, any>) =>
+            values.tenantName.checkState === DuplicateState.check ||
+            values.tenantName.checkState === DuplicateState.needInput,
+          message: t('LABEL.form.validation.check', { code: t('테넌트명') }),
+        },
+        {
+          fn: (values: Record<string, any>) =>
+            values.tenantName.checkState === DuplicateState.duplicated,
+          message: t('LABEL.form.validation.duplicated', { code: t('테넌트명') }),
+        },
+      ],
+    },
+
+    // tenantLogo: {
+    //   required: {
+    //     fn: (values) => {
+    //       return false;
+    //     },
+    //     message: 'ddddd',
+    //   },
+    // },
+    // managerName: { required: true },
+    // tenantJungsanTag: { required: true },
+    // company: { required: true },
+    isUsed: { required: true },
+    device: {
+      required: {
+        fn: (values) => {
+          return (
+            !values.isMobileExposed && !values.isWebExposed && !values.isAppExposed && !values.all
+          );
+        },
+        message: t('1개 이상 선택하세요.'),
+      },
+    },
+    useCategory: {
+      required: {
+        fn: (values) => {
+          return !values.common && !values.tenant && !values.all;
+        },
+        message: t('1개 이상 선택하세요.'),
+      },
+    },
+    language: {
+      required: {
+        fn: (values) => {
+          return !values.common && !values.tenant && !values.all;
+        },
+        message: t('1개 이상 선택하세요.'),
+      },
+    },
+  },
+};
