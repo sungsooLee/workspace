25fef4e4a974342baa9506b99733e96cdccffd31
이성수/협력사 <9490817@ict-companion.com>
2025-05-09 16:43:50 +0900

    [퍼블] bo 작업 업로드

--- apps/pub/bo/src/pages/_layout/learning/pop-program-guide.tsx
+++ apps/pub/bo/src/pages/_layout/learning/pop-program-guide.tsx
@@ -1,7 +1,15 @@
-import { useEffect, useRef } from 'react';
-import { createFileRoute } from '@tanstack/react-router';
-import { cn } from '@learnway/shared';
-import { Button, useModal, ModalContainer, ModalBody, ModalFooter, ModalTitle } from '@learnway/ui';
+import { useEffect, useRef, useState } from 'react';
+import { createFileRoute, Link } from '@tanstack/react-router';
+import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
+import {
+  Button,
+  useModal,
+  ModalContainer,
+  ModalBody,
+  ModalFooter,
+  ModalTitle,
+  GridBox,
+} from '@learnway/ui';
 
 export const Route = createFileRoute('/_layout/learning/pop-program-guide')({
   component: RouteComponent,
@@ -10,11 +18,57 @@ export const Route = createFileRoute('/_layout/learning/pop-program-guide')({
 function RouteComponent() {
   const { open: openModal, close: closeModal } = useModal();
   const GuideContent = () => {
+    // grid
+    const [pageIndex, setPageIndex] = useState(0);
+    const [pageSize, setPageSize] = useState(10);
+    const data: any[] = [
+      {
+        fileName: 'TOAST 프로그램 설치파일',
+        download: (
+          // <Link to={'/'} className="link">
+          //   다운로드
+          // </Link>
+          <Button className="link" label={'다운로드'} />
+        ),
+      },
+    ];
+
+    const columnHelper = createColumnHelper<any>();
+
+    const columns = [
+      columnHelper.accessor('fileName', {
+        cell: (info) => info.getValue(),
+        header: '파일명',
+        size: 516,
+        enableGrouping: false,
+      }),
+      columnHelper.accessor('download', {
+        cell: (info) => info.getValue(),
+        header: '다운로드',
+        size: 220,
+        enableGrouping: false,
+      }),
+    ] as ColumnDef<any, unknown>[];
     return (
       <ModalContainer>
         <ModalTitle>프로그램/가이드 다운로드</ModalTitle>
         <ModalBody>
-          <div></div>
+          <div className="grid_wrap">
+            <GridBox
+              data={data}
+              columns={columns}
+              height={370}
+              showColumnSettings={false}
+              pagination={{
+                pageSize,
+                pageIndex,
+                totalRows: 100,
+                onPageChange: setPageIndex,
+                onPageSizeChange: setPageSize,
+              }}
+              title="공유현황"
+            />
+          </div>
         </ModalBody>
         <ModalFooter>
           <Button label={'닫기'} variant={'primary'} size={'lg'} onClick={() => closeModal()} />
