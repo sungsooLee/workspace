1c5c33ed7333c4fb4e44d8d9f44a0b26144d858e
협력사/유홍식 <9488404@ict-companion.com>
2025-05-12 16:31:00 +0900

    feat: [NLP_BO_EDO_1107] 과정개설-이러닝

--- libs/ui/src/lib/thumbnail/type.ts
+++ libs/ui/src/lib/thumbnail/type.ts
@@ -4,17 +4,17 @@ export interface ImageOption {
   checked?: boolean; // 체크 여부
   readonly?: boolean; // 삭제, 체크 불가 여부
 
-  // 파일 업로드 return type 모두 다 붙여넣음 사용 여부에 따라 수정 예정..
-  file?: File
-  uploadType?: string
-  extension?: string
-  s3FileName?: string
-  fileName?: string
-  size?: number
-  displaySize?: string
-  progress?: number
-  status?: string
-  key?: string
-  parts?: any[]
-  contentType?: string
+  // s3 업로드 통해 받은 내용 모두 다 붙여넣음 사용 여부에 따라 수정 예정..
+  file?: File;
+  uploadType?: string;
+  extension?: string;
+  s3FileName?: string;
+  fileName?: string;
+  size?: number;
+  displaySize?: string;
+  progress?: number;
+  status?: string;
+  key?: string;
+  parts?: any[];
+  contentType?: string;
 }
