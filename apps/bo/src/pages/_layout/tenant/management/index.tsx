73929066c95e25277f8969082dbabb3cfe95bf8d
협력사/김석태 <9496491@ict-companion.com>
2025-05-12 21:02:37 +0900

    chore:[NLP_BO_TMS_1000] 목록 출력 안되는 버그 수정

--- apps/bo/src/pages/_layout/tenant/management/index.tsx
+++ apps/bo/src/pages/_layout/tenant/management/index.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from '@tanstack/react-router';
 import { PageContainer } from '@widgets/layout/ui/container/page-container';
 
 import { TenantManagmentList } from '@features/tenant/management/ui/tenant-managment-list';
+import { MainContents } from '@widgets/layout/ui/container/slot/main-contents';
 export const Route = createFileRoute('/_layout/tenant/management/')({
   component: RouteComponent,
 });
@@ -9,7 +10,9 @@ export const Route = createFileRoute('/_layout/tenant/management/')({
 function RouteComponent() {
   return (
     <PageContainer>
-      <TenantManagmentList />
+      <MainContents>
+        <TenantManagmentList />
+      </MainContents>
     </PageContainer>
   );
 }
