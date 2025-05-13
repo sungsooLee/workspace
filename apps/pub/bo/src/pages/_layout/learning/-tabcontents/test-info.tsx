e8e9112ba66d6ad6a62ebcf922c863138b28e30e
이성수/협력사 <9490817@ict-companion.com>
2025-05-13 16:50:51 +0900

    [퍼블] bo 작업 업로드

--- apps/pub/bo/src/pages/_layout/learning/-tabcontents/test-info.tsx
+++ apps/pub/bo/src/pages/_layout/learning/-tabcontents/test-info.tsx
@@ -1,14 +1,25 @@
 /* eslint-disable @nx/enforce-module-boundaries */
 import { FC } from 'react';
-import { FormSubTitle } from '../../../../../../../bo/src/shared/ui';
+import { FormSubTitle, NoticeBox } from '../../../../../../../bo/src/shared/ui';
 
 import styles from './test-detail.module.css';
 
 const TestInfoComponent: FC<{}> = ({}) => {
   return (
     <div className={styles.wrap}>
-      <div>
-        <FormSubTitle label={'역할 정보'} />
+      <NoticeBox
+        iconVisible={false}
+        descriptions={[
+          '해당 시험지는 과정에서 사용 중입니다. 일부 정보만 변경할 수 있고, 삭제는 할 수 없습니다.',
+        ]}
+      />
+      <div className={styles.row_wrap}>
+        <div className={styles.main_container}>
+          <FormSubTitle label={'역할 정보'} />
+        </div>
+        <div className={styles.sub_container}>
+          <FormSubTitle label={'시험지'} />
+        </div>
       </div>
     </div>
   );
