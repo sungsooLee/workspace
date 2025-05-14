3d656c8137d6ee9f7b50b3103cbbb9d9b992f3f9
이성수/협력사 <9490817@ict-companion.com>
2025-05-14 10:37:03 +0900

    [퍼블] bo 시험지 상세 작업중

--- apps/pub/bo/src/pages/_layout/learning/-tabcontents/question-info.tsx
+++ apps/pub/bo/src/pages/_layout/learning/-tabcontents/question-info.tsx
@@ -2,7 +2,8 @@
 import { FC } from 'react';
 import { cn } from '@learnway/shared';
 import { FormSubTitle } from '../../../../../../../bo/src/shared/ui/form';
-import { ContentsRow, Tabs } from '@learnway/ui';
+import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
+import { ContentsRow, Tabs, TableBox, Input } from '@learnway/ui';
 
 /* styles */
 import styles from './test-detail.module.css';
@@ -23,6 +24,41 @@ const QuestionInfoComponent: FC<{}> = ({}) => {
       content: '',
     },
   ];
+
+  // Table
+  const columnHelper = createColumnHelper<any>();
+  const data: any[] = [
+    {
+      Sort: <strong>PC</strong>,
+      ComponentId: <Input value={'dsbdbshjdbshbd'} disabled />,
+    },
+    {
+      Sort: <strong>Mobile</strong>,
+      ComponentId: <Input value={'dsbdbshjdbshbd'} disabled />,
+    },
+  ];
+
+  const columns = [
+    columnHelper.accessor('Sort', {
+      cell: (info) => info.getValue(),
+      header: '구분',
+      enableGrouping: false,
+      size: 100,
+      meta: {
+        headerAlign: 'center', // 헤더 정렬
+        cellAlign: 'left', // 셀 정렬
+      },
+    }),
+    columnHelper.accessor('ComponentId', {
+      cell: (info) => info.getValue(),
+      header: '컴포넌트 ID',
+      enableGrouping: false,
+      meta: {
+        headerAlign: 'center', // 헤더 정렬
+        cellAlign: 'left', // 셀 정렬
+      },
+    }),
+  ] as ColumnDef<any, unknown>[];
   return (
     <div className={styles.wrap}>
       <FormSubTitle label={'기본정보'} />
@@ -71,6 +107,9 @@ const QuestionInfoComponent: FC<{}> = ({}) => {
           </div>
         </div>
       </ContentsRow>
+      <div className={styles.table_wrap}>
+        <TableBox data={data} columns={columns} tableMode={true} title={'컴포넌트 ID'} />
+      </div>
     </div>
   );
 };
