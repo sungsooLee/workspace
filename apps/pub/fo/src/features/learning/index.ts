45ddb538ba4795fedcdeea1dfb8fc0a2ff0757f9
정민혁/협력사 <9493801@ict-companion.com>
2025-05-14 15:02:03 +0900

    [퍼블] 학습창 작업중

--- apps/pub/fo/src/features/learning/index.ts
+++ apps/pub/fo/src/features/learning/index.ts
@@ -1,2 +1,3 @@
 export * from './ui/side-panel';
 export * from './popup/curriculum-popup';
+export * from './popup/next-learning';
--- apps/pub/fo/src/pages/_learning/video.tsx
+++ apps/pub/fo/src/pages/_learning/video.tsx
@@ -1,6 +1,8 @@
 import { isMobile } from 'react-device-detect';
 import { cn } from '@learnway/shared';
 import { createFileRoute, Link } from '@tanstack/react-router';
+import { useModal } from '@learnway/ui';
+
 import styles from './video.module.css';
 
 import bnrImage1 from '@learnway/styles/fo/assets/images/temp/category_product_01.png';
@@ -10,6 +12,20 @@ export const Route = createFileRoute('/_learning/video')({
 });
 
 function RouteComponent() {
+  const { alert: openAlert } = useModal();
+
+  const handleCompleteAlert = () => {
+    openAlert({
+      type: 'complete',
+      title: <>완료되었습니다.</>,
+      content: (
+        <>
+          이것은 description 입니다. <br /> 줄바꿈 적용 <br /> 줄바꿈 적용
+        </>
+      ),
+    });
+  };
+
   return (
     <div className={styles.start}>
       {/* 예시 이미지 */}
