31f66bf1926454a110b8526edbaae0be4d9791d1
협력사/김석태 <9496491@ict-companion.com>
2025-05-16 17:25:50 +0900

    chore: [NLP_BO_TMS_1000] 목록 날짜 노출 방식 변경

--- apps/bo/src/features/tenant/management/ui/tenant-managment-list.tsx
+++ apps/bo/src/features/tenant/management/ui/tenant-managment-list.tsx
@@ -1,48 +1,115 @@
 import { FC, useState, useCallback } from 'react';
 import { createFileRoute, Link } from '@tanstack/react-router';
 import { t } from 'i18next';
-import { IcoRefresh02, IcoSearch } from '@learnway/icons';
-import { CellContext, ColumnDef, createColumnHelper } from '@tanstack/react-table';
-import { PageContainer } from '@widgets/layout/ui/container/page-container';
-import searchStyles from '@learnway/styles/bo/assets/styles/modules/search-box.module.css'; // search-box.module.css
 import boxStyles from '@learnway/styles/bo/assets/styles/modules/wrap-box.module.css'; // 하단 layout style - line
 import { SearchBox } from '@shared/ui/search-box';
 import { useRouter } from '@tanstack/react-router';
 
-import { cn } from '@learnway/shared';
+import { cn, DATE_TIME_FORMAT, getDateToString } from '@learnway/shared';
 
-import { Button, GridBox, useGridBox, Input, DynamicFormField, Dropdown } from '@learnway/ui';
+import { Button, GridBox, useGridBox } from '@learnway/ui';
 import { useSearchBox, SearchBoxConfig } from '@learnway/hooks';
-import { MainContents } from '@widgets/layout/ui/container/slot/main-contents';
-import { ContentsButtons } from '@widgets/layout/ui/container/slot/contents-buttons';
 
 import { tenantQueryOptions } from '@entities/tenant/service/tenant.queries';
 
 const TenantManagmentListComponent: FC<any> = ({ rootPath }) => {
   const router = useRouter();
 
-  const gridConfig = { ...gridBaseConfig };
-
-  gridConfig.columns.splice(1, 0, {
-    name: 'tenantName',
-    label: t('테넌트명'),
-    render: (info: any) => (
-      <Button
-        className="link"
-        onClick={() => {
-          router.navigate({
-            to: `${rootPath}/tenant/management/detail`,
-            state: {
-              tenantId: info.row.original.tenantId,
-              tenantName: info.row.original.tenantName,
-            },
+  const gridConfig = {
+    query: tenantQueryOptions.all,
+    columns: [
+      {
+        name: 'no1',
+        label: 'NO.',
+        type: 'numbering',
+      },
+      {
+        name: 'tenantName',
+        label: t('테넌트명'),
+        render: (info: any) => (
+          <Button
+            className="link"
+            onClick={() => {
+              router.navigate({
+                to: `${rootPath}/tenant/management/detail`,
+                state: {
+                  tenantId: info.row.original.tenantId,
+                  tenantName: info.row.original.tenantName,
+                },
+              });
+            }}
+          >
+            {info.row.original.tenantName}
+          </Button>
+        ),
+      },
+      {
+        name: 'tenantSite',
+        label: t('테넌트 사이트'),
+        render: (info: any) => (
+          <Link to={info.row.original.tenantSite} className="link">
+            {info.row.original.tenantId}
+          </Link>
+        ),
+      },
+      {
+        name: 'companyTenantList',
+        label: t('회사'),
+        render: (info: any) => {
+          const companyNames = info.row.original.companyTenantList.map((item: any) => {
+            return item.companyName;
           });
-        }}
-      >
-        {info.row.original.tenantName}
-      </Button>
-    ),
-  });
+          return companyNames.toString();
+        },
+      },
+      {
+        name: 'tenantRoleList',
+        label: t('테넌트 담당자'),
+        render: (info: any) => {
+          const tenantRoleList = info.row.original.tenantRoleList.map((item: any) => {
+            return item.roleName;
+          });
+          return tenantRoleList.toString();
+        },
+      },
+      {
+        name: 'isUsed',
+        label: '사용여부',
+        render: (info: any) => {
+          return info.row.original.isUsed ? t('LABEL.common.enable') : t('LABEL.common.disable');
+        },
+      },
+      { name: 'createdBy', label: '등록자' },
+      {
+        name: 'createdDate',
+        label: '등록일시',
+        render: (info: any) => {
+          return getDateToString(
+            new Date(info.row.original.createdDate),
+            DATE_TIME_FORMAT.DATETIME_SEC,
+          );
+        },
+      },
+      { name: 'lastModifiedBy', label: '수정자' },
+      {
+        name: 'modifiedDate',
+        label: '수정일시',
+        render: (info: any) => {
+          return getDateToString(
+            new Date(info.row.original.modifiedDate),
+            DATE_TIME_FORMAT.DATETIME_SEC,
+          );
+        },
+      },
+    ],
+    data: [],
+
+    pagination: {
+      pageSize: 10,
+      pageIndex: 0,
+      totalRows: 0,
+    },
+  };
 
   const { provider: searchProvider, getValues } = useSearchBox(searchConfig);
   const { config: gConfig, gridFetch, data } = useGridBox(gridConfig, getValues);
@@ -125,63 +192,4 @@ const searchConfig: SearchBoxConfig = {
       },
     ],
   ],
-};
-
-const gridBaseConfig = {
-  query: tenantQueryOptions.all,
-  columns: [
-    {
-      name: 'no1',
-      label: 'NO.',
-      type: 'numbering',
-    },
-
-    {
-      name: 'tenantSite',
-      label: t('테넌트 사이트'),
-      render: (info: any) => (
-        <Link to={info.row.original.tenantSite} className="link">
-          {info.row.original.tenantId}
-        </Link>
-      ),
-    },
-    {
-      name: 'companyTenantList',
-      label: t('회사'),
-      render: (info: any) => {
-        const companyNames = info.row.original.companyTenantList.map((item: any) => {
-          return item.companyName;
-        });
-        return companyNames.toString();
-      },
-    },
-    {
-      name: 'tenantRoleList',
-      label: t('테넌트 담당자'),
-      render: (info: any) => {
-        const tenantRoleList = info.row.original.tenantRoleList.map((item: any) => {
-          return item.roleName;
-        });
-        return tenantRoleList.toString();
-      },
-    },
-    {
-      name: 'isUsed',
-      label: '사용여부',
-      render: (info: any) => {
-        return info.row.original.isUsed ? t('LABEL.common.enable') : t('LABEL.common.disable');
-      },
-    },
-    { name: 'createdBy', label: '등록자' },
-    { name: 'createdDate', label: '등록일시' },
-    { name: 'lastModifiedBy', label: '수정자' },
-    { name: 'modifiedDate', label: '수정일시' },
-  ],
-  data: [],
-
-  pagination: {
-    pageSize: 10,
-    pageIndex: 0,
-    totalRows: 0,
-  },
 };
