8f992cf9bde0aa9c84239d59b480a30936e658b1
김태경/협력사 <9488416@ict-companion.com>
2025-05-19 17:30:28 +0900

    [퍼블]

--- apps/pub/fo/src/features/layout/ui/edu-licenses/licenses-history.tsx
+++ apps/pub/fo/src/features/layout/ui/edu-licenses/licenses-history.tsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import { Link } from '@tanstack/react-router';
-import { Button, Panel } from '@learnway/ui';
+import { Button, ContentsRow, Panel, DatePicker } from '@learnway/ui';
 import { IcoPlus } from '@learnway/icons';
 import { cn } from '@learnway/shared';
 import styles from './licenses-history.module.css';
 import searchBoxStyles from './search-box.module.css';
 import bulletStyles from '../../../../shared/ui/list/bullet.module.css';
+import formStyles from '@learnway/styles/fo/assets/styles/modules/form.module.css';
+import dynamicFormStyles from '@learnway/styles/fo/assets/styles/modules/dynamic.form.module.css';
 
 const LicensesHistoryComponent = () => {
   return (
@@ -16,8 +18,8 @@ const LicensesHistoryComponent = () => {
             <li>자격증 취득 이력을 등록할 수 있으며, 승인 현황을 조회할 수 있습니다.</li>
           </ul>
         </div>
-        <Button variant="line" size="lg">
-          <IcoPlus width={20} height={20} stroke="#00AFD5" />
+        <Button variant="line" size="lg" className={styles.btn}>
+          <IcoPlus />
           취득이력등록
         </Button>
       </div>
@@ -27,7 +29,15 @@ const LicensesHistoryComponent = () => {
         className={`${searchBoxStyles.start} ${styles.search_date}`}
         type="rounded_fill"
       >
-        <div className={searchBoxStyles.search_box}>content</div>
+        <div className={searchBoxStyles.search_box}>
+          <div className={styles.form_content}>
+            <ContentsRow>
+              <div className={formStyles.form_item}>
+                <DatePicker displayType={'day'} size={'lg'} />
+              </div>
+            </ContentsRow>
+          </div>
+        </div>
       </Panel>
     </div>
   );
