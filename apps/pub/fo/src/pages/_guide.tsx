da9512a5c1c7f873c87afebd001845af29f1dd3f
김태경/협력사 <9488416@ict-companion.com>
2025-05-22 10:52:09 +0900

    [퍼블]업데이트

--- apps/pub/fo/src/pages/_guide.tsx
+++ apps/pub/fo/src/pages/_guide.tsx
@@ -22,6 +22,7 @@ function RouteComponent() {
         { label: '모바일', path: '/guide/mobile' },
         { label: 'Form', path: '/guide/form' },
         { label: '이미지', path: '/guide/image' },
+        { label: 'E-mail(피드백메일)', path: '/guide/email' },
       ],
     },
 
