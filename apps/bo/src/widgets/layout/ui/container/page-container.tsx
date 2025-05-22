b846c5f88e0ae95cfce1df51c91433573a78365a
이성수/협력사 <9490817@ict-companion.com>
2025-05-22 10:59:14 +0900

    [퍼블] bo 페이지 스크롤 이슈 수정

--- apps/bo/src/widgets/layout/ui/container/page-container.tsx
+++ apps/bo/src/widgets/layout/ui/container/page-container.tsx
@@ -64,9 +64,11 @@ const PageContainerComponent: FC<{
     if (scrollContainerRef.current) {
       const scrollTop = scrollContainerRef.current.scrollTop;
       setScrollPosition(scrollTop);
-      scrollTop > scrollPosition
-        ? document.body.classList.add('scrolled')
-        : document.body.classList.remove('scrolled');
+      if (scrollTop === 0) {
+        document.body.classList.remove('scrolled');
+      } else if (scrollTop > scrollPosition + 50) {
+        document.body.classList.add('scrolled');
+      }
     }
   };
 
@@ -82,7 +84,7 @@ const PageContainerComponent: FC<{
         container.removeEventListener('scroll', handleScroll);
       }
     };
-  }, []);
+  }, [hasScroll]);
 
   useEffect(() => {
     const checkScroll = () => {
