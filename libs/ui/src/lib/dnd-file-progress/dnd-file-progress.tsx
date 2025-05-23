aad62f3280efdc173b999c67fe282ab7cb4380fa
조일환/협력사 <9500896@ict-companion.com>
2025-05-23 13:50:03 +0900

    fix: DndFileProgessProps의 files type 설정

--- libs/ui/src/lib/dnd-file-progress/dnd-file-progress.tsx
+++ libs/ui/src/lib/dnd-file-progress/dnd-file-progress.tsx
@@ -229,7 +229,7 @@ const DndFileProgressComponent: FC<DndFileProgressProps> = ({
         {/* 파일 업로드 후 */}
         {files.length > 0 && (
           <div className={styles.upload_status}>
-            {files.map((file: any) => (
+            {files.map((file) => (
               <div className={styles.file_item}>
                 <div className={styles.file_name}>
                   <IcoFileExcel width={'24'} height={'25'} className={styles.icon_type} />
