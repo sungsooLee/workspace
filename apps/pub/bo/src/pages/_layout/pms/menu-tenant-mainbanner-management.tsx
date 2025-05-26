59bb72e404ae4f856e363f6aedbb7dbe3e20f707
이성수/협력사 <9490817@ict-companion.com>
2025-05-26 15:21:29 +0900

    [퍼블] bo 작업 업로드

--- apps/pub/bo/src/pages/_layout/pms/menu-tenant-mainbanner-management.tsx
+++ apps/pub/bo/src/pages/_layout/pms/menu-tenant-mainbanner-management.tsx
@@ -65,7 +65,7 @@ function RouteComponent() {
     },
   ];
   return (
-    <PageContainer scrollHidden={true}>
+    <PageContainer scrollHidden={false}>
       {/* main_contents */}
       <div className={cn(styles.main_contents)}>
         <Tabs
