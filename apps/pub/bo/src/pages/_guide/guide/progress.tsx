3bf7a039f61bdd14fc9eac61d880bf601112b09d
김태경/협력사 <9488416@ict-companion.com>
2025-05-26 12:46:59 +0900

    [퍼블]머지

--- apps/pub/bo/src/pages/_guide/guide/progress.tsx
+++ apps/pub/bo/src/pages/_guide/guide/progress.tsx
@@ -14,7 +14,7 @@ function RouteComponent() {
         <pre className="code_block">
           <code>
             {`// 초기 import
-  import { Progress } from '@learnway/ui';
+  import { Progress, ProgressCheck } from '@learnway/ui';
   
   // 적용방법(예시) % 기준으로 value값 추가, 실패인 경우 isFailed 속성 추가, 90%이상일때, 100%일때 컬러 변경됨
   <ProgressBar progress={40} />
