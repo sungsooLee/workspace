1126269ea32a1ade2485d61bc72f76e367ac4361
이성수/협력사 <9490817@ict-companion.com>
2025-05-27 17:06:19 +0900

    [퍼블] bo 작업 업로드

--- libs/icons/src/index.ts
+++ libs/icons/src/index.ts
@@ -136,6 +136,7 @@ export { ReactComponent as IcoLearning03 } from './lib/ic_learning_03.svg'; // l
 export { ReactComponent as IcoLearning04 } from './lib/ic_learning_04.svg'; // learning 4
 export { ReactComponent as IcoPrevPlay } from './lib/ic_prev_play.svg'; // learning prev
 export { ReactComponent as IcoPrevNext } from './lib/ic_next_play.svg'; // learning next
+export { ReactComponent as IcoArrowDownFilled } from './lib/ic_arrow_down_filled.svg'; // date picker
 //export Default as Information from './lib/information.svg?react';
 
 //import Information from './lib/information.svg';
