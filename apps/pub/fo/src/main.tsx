9a0607e387b21fdeb6653445f836ffb1a5a6ecf9
이성수/협력사 <9490817@ict-companion.com>
2025-05-28 11:01:27 +0900

    [퍼블] date-picker 설정 수정

--- apps/pub/fo/src/main.tsx
+++ apps/pub/fo/src/main.tsx
@@ -4,6 +4,10 @@ import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 
 import { appConfig } from '@learnway/config';
+import { useEffect, useState } from 'react';
+import { getDefaultLang } from '@learnway/config';
+import { initReactI18next } from 'react-i18next';
+import i18next from 'i18next';
 
 const router = createRouter({
   routeTree,
@@ -19,9 +23,44 @@ declare module '@tanstack/react-router' {
 
 appConfig.init({});
 
+export const I18nProvider: React.FC<any> = ({ children }) => {
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  useEffect(() => {
+    if (!i18next.isInitialized) {
+      i18next
+        .use(initReactI18next)
+        .init({
+          debug: false,
+          lng: getDefaultLang(),
+          fallbackLng: 'ko',
+          react: {
+            useSuspense: false,
+          },
+          interpolation: {
+            escapeValue: false,
+          },
+        })
+        .then(() => {
+          setIsInitialized(true);
+        });
+    } else {
+      setIsInitialized(true);
+    }
+  }, []);
+
+  if (!isInitialized) {
+    return <div>Loading i18n...</div>;
+  }
+
+  return <>{children}</>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <I18nProvider>
+      <RouterProvider router={router} />
+    </I18nProvider>
   </StrictMode>,
 );
