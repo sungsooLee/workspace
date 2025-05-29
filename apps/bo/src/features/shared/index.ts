2b48fd8531feede890f85a90799124369d3eea3a
9497019 <9497019@ict-companion.com>
2025-05-29 16:00:04 +0900

    feat: external-user-shuttle-modal 추가

--- apps/bo/src/features/shared/index.ts
+++ apps/bo/src/features/shared/index.ts
@@ -11,6 +11,7 @@ export * from './ui/modal/user-choice-for-group-modal';
 export * from './ui/modal/user-choice-modal';
 export * from './ui/modal/user-shuttle-modal';
 export * from './ui/modal/external-user-choice-modal';
+export * from './ui/modal/external-user-shuttle-modal';
 export * from './ui/modal/user-and-external-user-tab-choice-modal';
 export * from './ui/modal/tenant-choice-modal';
 export * from './ui/modal/tenant-shuttle-modal';
