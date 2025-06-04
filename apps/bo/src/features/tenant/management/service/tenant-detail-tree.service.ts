c80830372d0617d169938a91240682ad4cf94a8c
협력사/김석태 <9496491@ict-companion.com>
2025-06-04 17:56:07 +0900

    feat: [NLP_BO_PMS_1200_2] 메뉴 관리 기획 변경 내용 적용

--- apps/bo/src/features/tenant/management/service/tenant-detail-tree.service.ts
+++ apps/bo/src/features/tenant/management/service/tenant-detail-tree.service.ts
@@ -237,7 +237,7 @@ export const transformMenuApiDataToTreeData = (apiData: any) => {
         ...node,
 
         // 필수 트리 속성
-        key: node.menuId.toString(), // menuId를 key로 사용
+        key: node.tenantMappingMenuId?.toString(), // menuId를 key로 사용
         title: node.menuName || node.menuCode, // title이 없으면 menuCode 사용
         parentKey: node.parentId?.toString(), // parentId를 parentKey로 변환
         children: node.children || [],
