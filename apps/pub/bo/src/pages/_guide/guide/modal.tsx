772f821163eb4e0db70b39bd635a6f227bc26e43
김태경/협력사 <9488416@ict-companion.com>
2025-06-04 13:02:03 +0900

    [퍼블]BO 해상도에 따른 자동 모달

--- apps/pub/bo/src/pages/_guide/guide/modal.tsx
+++ apps/pub/bo/src/pages/_guide/guide/modal.tsx
@@ -1,6 +1,36 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
 import { isMobile } from 'react-device-detect';
-import { Button, ModalBody, ModalContainer, ModalFooter, ModalTitle, useModal } from '@learnway/ui';
+import {
+  Button,
+  ModalBody,
+  ModalContainer,
+  ModalFooter,
+  ModalTitle,
+  useModal,
+  Dropdown,
+  Input,
+  Checkbox,
+  GridBox,
+} from '@learnway/ui';
+import { PageContainer } from '../../../widgets/layout/ui/container/page-container';
+import { useState } from 'react';
+
+import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
+import { cn } from '@learnway/shared';
+import {
+  IcoRefresh02,
+  IcoSearch,
+  IcoClipboard,
+  IcoClock01,
+  IcoFormRequired,
+  IcoDownload,
+  IcoCopy,
+} from '@learnway/icons';
+
+/* style */
+import styles from '@learnway/styles/bo/assets/styles/modules/page-contents.module.css';
+import searchStyles from '@learnway/styles/bo/assets/styles/modules/search-box.module.css'; // search-box.module.css
+import boxStyles from '@learnway/styles/bo/assets/styles/modules/wrap-box.module.css'; // 하단 layout style - line
 
 const ContentModal = () => {
   const { close: closeModal } = useModal();
@@ -18,6 +48,371 @@ const ContentModal = () => {
   );
 };
 
+const ResponModal = () => {
+  // grid
+  const [pageNumber, setpageNumber] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+  const data: any[] = [
+    {
+      type: '동영상',
+      name: (
+        // <Link to={'/'} className="link">
+        //   학습자원명
+        // </Link>
+        <Button className="link" label={'학습자원명'} />
+      ),
+      tenant: '테넌트',
+      channel: '채널',
+      owner: '김현대',
+      detailInfo: (
+        <span className="icon_wrap">
+          <IcoClock01 width={'16'} height={'16'} stroke={'#4c515e'} />
+          {'02:00:00'}
+        </span>
+      ),
+      function: (
+        <>
+          <Link to={'/'} className="link">
+            미리보기
+          </Link>
+          <Link to={'/'} className="link">
+            문항관리
+          </Link>
+        </>
+      ),
+      education: 'Y',
+      course: (
+        <Link to={'/'} className="link">
+          2
+        </Link>
+      ),
+    },
+    {
+      type: '동영상',
+      name: (
+        // <Link to={'/'} className="link">
+        //   학습자원명
+        // </Link>
+        <Button className="link" label={'학습자원명'} />
+      ),
+      tenant: '테넌트',
+      channel: '채널',
+      owner: '김현대',
+      detailInfo: (
+        <span className="icon_wrap">
+          <IcoClipboard width={'16'} height={'16'} />
+          {'20개'}
+        </span>
+      ),
+      function: (
+        <>
+          <Link to={'/'} className="link">
+            미리보기
+          </Link>
+          <Link to={'/'} className="link">
+            문항관리
+          </Link>
+        </>
+      ),
+      education: 'Y',
+      course: (
+        <Link to={'/'} className="link">
+          2
+        </Link>
+      ),
+    },
+  ];
+
+  const columnHelper = createColumnHelper<any>();
+
+  const columns = [
+    columnHelper.accessor('type', {
+      cell: (info) => info.getValue(),
+      header: '구분',
+      size: 60,
+      enableGrouping: false,
+    }),
+    columnHelper.accessor('name', {
+      cell: (info) => info.getValue(),
+      header: '학습자원명',
+      size: 380,
+      enableGrouping: false,
+    }),
+    columnHelper.accessor('tenant', {
+      cell: (info) => info.getValue(),
+      header: '테넌트',
+      enableGrouping: false,
+      size: 120,
+    }),
+    columnHelper.accessor('channel', {
+      cell: (info) => info.getValue(),
+      header: '채널',
+      size: 180,
+      enableGrouping: false,
+    }),
+
+    columnHelper.accessor('owner', {
+      cell: (info) => info.getValue(),
+      header: '담당자',
+      size: 104,
+      enableGrouping: false,
+    }),
+    columnHelper.accessor('detailInfo', {
+      cell: (info) => info.getValue(),
+      header: '세부정보',
+      enableGrouping: false,
+      size: 120,
+    }),
+    columnHelper.accessor('function', {
+      cell: (info) => info.getValue(),
+      header: '기능',
+      enableGrouping: false,
+      size: 150,
+    }),
+    columnHelper.accessor('education', {
+      cell: (info) => info.getValue(),
+      header: '교육활용',
+      size: 104,
+      enableGrouping: false,
+    }),
+    columnHelper.accessor('course', {
+      cell: (info) => info.getValue(),
+      header: '과정수',
+      enableGrouping: false,
+      size: 104,
+    }),
+  ] as ColumnDef<any, unknown>[];
+
+  // dropdown
+  const [selectedValues, setSelectedValues] = useState<null>(null);
+  const [selectedValues2, setSelectedValues2] = useState<null>(null);
+  const [selectedValues3, setSelectedValues3] = useState<null>(null);
+  const [selectedValues4, setSelectedValues4] = useState<null>(null);
+  const [selectedValues5, setSelectedValues5] = useState<null>(null);
+  const [selectedValues6, setSelectedValues6] = useState<null>(null);
+  const options = [
+    { value: 'option1', label: '전체' },
+    { value: 'option2', label: '옵션 2' },
+    { value: 'option3', label: '옵션 3' },
+  ];
+  return (
+    <ModalContainer>
+      <ModalBody>
+        <form className="form_row">
+          <PageContainer>
+            {/* main_contents */}
+            <div className={styles.main_contents}>
+              <div className={cn(searchStyles.start, searchStyles.wrap)}>
+                <div className={searchStyles.contents}>
+                  <div className={searchStyles.item_row}>
+                    <div className={searchStyles.item_wrap}>
+                      <div className={searchStyles.inner}>
+                        <div className={searchStyles.item}>
+                          <label htmlFor="name-1" className={searchStyles.label}>
+                            <span className={searchStyles.text}>테넌트</span>
+                            {/* 필수 케이스 */}
+                            <span className={cn(searchStyles.status, searchStyles.required)}>
+                              <IcoFormRequired width={8} height={8} />
+                            </span>
+                          </label>
+                          <div className={searchStyles.box}>
+                            <Dropdown
+                              options={options}
+                              value={selectedValues}
+                              onChange={(selected) => setSelectedValues(selected)}
+                              variant="default"
+                              placeholder="선택"
+                              size={'sm'}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div className={searchStyles.inner}>
+                        <div className={searchStyles.item}>
+                          <label htmlFor="name-2" className={searchStyles.label}>
+                            <span className={searchStyles.text}>채널</span>
+                            {/* 필수 케이스 */}
+                            <span className={cn(searchStyles.status, searchStyles.required)}>
+                              <IcoFormRequired width={8} height={8} />
+                            </span>
+                          </label>
+                          <div className={searchStyles.box}>
+                            <Dropdown
+                              options={options}
+                              value={selectedValues2}
+                              onChange={(selected) => setSelectedValues2(selected)}
+                              variant="default"
+                              placeholder="선택"
+                              size={'sm'}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div className={searchStyles.inner}>
+                        <div className={searchStyles.item}>
+                          <label htmlFor="name-3" className={searchStyles.label}>
+                            <span className={searchStyles.text}>유형</span>
+                          </label>
+                          <div className={searchStyles.box}>
+                            <Dropdown
+                              options={options}
+                              value={selectedValues3}
+                              onChange={(selected) => setSelectedValues3(selected)}
+                              variant={'text'}
+                              placeholder="선택"
+                              size={'sm'}
+                              isMulti
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div className={searchStyles.inner}>
+                        <div className={searchStyles.item}>
+                          <label htmlFor="name-4" className={searchStyles.label}>
+                            <span className={searchStyles.text}>학습자원명</span>
+                          </label>
+                          <div className={searchStyles.box}>
+                            <Input type={'text'} placeholder={'입력'} id={'name-4'} />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className={searchStyles.item_wrap}>
+                      <div className={searchStyles.inner}>
+                        <div className={searchStyles.item}>
+                          <label htmlFor="name-5" className={searchStyles.label}>
+                            <span className={searchStyles.text}>외주여부</span>
+                            {/* 필수 케이스 */}
+                            <span className={cn(searchStyles.status, searchStyles.required)}>
+                              <IcoFormRequired width={8} height={8} />
+                            </span>
+                          </label>
+                          <div className={searchStyles.box}>
+                            <Dropdown
+                              options={options}
+                              value={selectedValues4}
+                              onChange={(selected) => setSelectedValues4(selected)}
+                              variant="default"
+                              placeholder="선택"
+                              size={'sm'}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div className={searchStyles.inner}>
+                        <div className={searchStyles.item}>
+                          <label htmlFor="name-6" className={searchStyles.label}>
+                            <span className={searchStyles.text}>사용가능</span>
+                            {/* 필수 케이스 */}
+                            <span className={cn(searchStyles.status, searchStyles.required)}>
+                              <IcoFormRequired width={8} height={8} />
+                            </span>
+                          </label>
+                          <div className={searchStyles.box}>
+                            <Dropdown
+                              options={options}
+                              value={selectedValues5}
+                              onChange={(selected) => setSelectedValues5(selected)}
+                              variant="default"
+                              placeholder="선택"
+                              size={'sm'}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div className={searchStyles.inner}>
+                        <div className={searchStyles.item}>
+                          <label htmlFor="name-7" className={searchStyles.label}>
+                            <span className={searchStyles.text}>교육활용</span>
+                          </label>
+                          <div className={searchStyles.box}>
+                            <Dropdown
+                              options={options}
+                              value={selectedValues6}
+                              onChange={(selected) => setSelectedValues6(selected)}
+                              variant="default"
+                              placeholder="선택"
+                              size={'sm'}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div className={searchStyles.inner}>
+                        <div className={searchStyles.item}>
+                          <label htmlFor="name-8" className={searchStyles.label}>
+                            <span className={searchStyles.text}>담당자</span>
+                          </label>
+                          <div className={searchStyles.box}>
+                            <Input type={'text'} placeholder={'입력'} id={'name-8'} />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={searchStyles.btn_box}>
+                    <Button
+                      type="button"
+                      className={searchStyles.btn_refresh}
+                      variant="search"
+                      size="sm"
+                      onlyIcon
+                    >
+                      <IcoRefresh02 className={searchStyles.icon_refresh} />
+                    </Button>
+                    <Button
+                      type="button"
+                      variant="search"
+                      size="sm"
+                      className={searchStyles.btn_search}
+                    >
+                      <IcoSearch className={searchStyles.icon_sm_search} />
+                      조회
+                    </Button>
+                  </div>
+                </div>
+              </div>
+              <div className={cn(boxStyles.start, boxStyles.inner)}>
+                <div className="grid_wrap">
+                  <GridBox
+                    data={data}
+                    columns={columns}
+                    showColumnSettings={false}
+                    showNumberingColumn
+                    showExcelDownload
+                    columnPinning={{ columns: ['numbering', 'type', 'name'] }}
+                    pagination={{
+                      pageSize,
+                      pageNumber,
+                      totalPages: 100,
+                      onPageChange: setpageNumber,
+                      onPageSizeChange: setPageSize,
+                    }}
+                    title="목록"
+                    customButtonNode={
+                      <>
+                        <Checkbox label={'나의 학습자원'} size={'md'} />
+                        <Button
+                          label={'프로그램/가이드 다운로드'}
+                          icon={<IcoDownload width={16} height={16} stroke={'#4C515E'} />}
+                        />
+                        <Button label={'일괄설정'} variant={'text'} />
+                        <Button
+                          label={'복사'}
+                          icon={<IcoCopy width={16} height={16} stroke={'#131c30'} />}
+                        />
+                      </>
+                    }
+                  />
+                </div>
+              </div>
+            </div>
+          </PageContainer>
+        </form>
+      </ModalBody>
+    </ModalContainer>
+  );
+};
+
 export const Route = createFileRoute('/_guide/guide/modal')({
   component: RouteComponent,
 });
@@ -29,7 +424,8 @@ function RouteComponent() {
       <h2 className="guide_tit2">Modal Component Guide</h2>
       <p className="loc react">/libs/ui/src/lib/modal/modal.tsx (공통)</p>
       <p className="info">
-        모달 size(가로 기준) : sm(600px), md(800px), lg(1024px), xl(1400px) width 속성 적용
+        모달 size(가로 기준) : sm(600px), md(800px), lg(1024px), xl(1400px), respon(해상도에 따라
+        자동 변경) width 속성 적용
       </p>
 
       <div className="code_example">
@@ -106,7 +502,7 @@ onClick={() =>
 </Button>`}</code>
           </pre>
         </div>
-        <h3 className="guide_tit3">Modal(반응형) 띄울때</h3>
+        <h3 className="guide_tit3">Modal(반응형-모바일) 띄울때</h3>
         <div className="flex_box">
           <div className="desc">
             <Button
@@ -225,6 +621,51 @@ useEffect(() => {
         </div>
 
         <div className="info">페이지 접근시 모달팝업 자동실행(퍼블확인용)</div>
+
+        <h3 className="guide_tit3">Modal(해상도에 따라 자동 사이즈 조절) 띄울때</h3>
+        <div className="flex_box">
+          <div className="desc">
+            <Button
+              onClick={() =>
+                openModal({
+                  width: 'respon', // respon
+                  hideCloseButton: true, // close 버튼 가림
+                  content: <ResponModal />,
+                })
+              }
+            >
+              모달 팝업 열기
+            </Button>
+          </div>
+        </div>
+        <div className="code_example">
+          <pre className="code_block">
+            <code>{`// 팝업 내용
+const ResponModal = () => {
+  return (
+    <ModalContainer>
+      <ModalBody>
+        <p>컨텐츠 영역</p>
+      </ModalBody>
+    </ModalContainer>
+  );
+};
+
+// 실행 코드
+<Button
+  onClick={() =>
+    openModal({
+      width: 'respon',
+      hideCloseButton: true,
+      content: <ResponModal />,
+    })
+  }
+>
+  모달 팝업 열기
+</Button>
+`}</code>
+          </pre>
+        </div>
       </div>
     </div>
   );
