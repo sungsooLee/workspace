70f13777433e1286e06def3d76de91555d0404da
조일환/협력사 <9500896@ict-companion.com>
2025-06-09 19:50:37 +0900

    feat: [NLP_BO_CMS_1001] 멀티 드롭다운 적용

--- apps/bo/src/features/learning/ui/learning-resource/learning-resource-table.tsx
+++ apps/bo/src/features/learning/ui/learning-resource/learning-resource-table.tsx
@@ -57,7 +57,9 @@ function LearningResourceTableComponent() {
           name: 'contentTypes',
           type: 'dropdown',
           label: t('LABEL.content.learning-resource.contentType'),
-          value: '',
+          value: [],
+          isMulti: true,
+          variant: 'text',
           optionsConfig: {
             options: [{ value: '', label: t('전체') }],
             codeGroup: CODE_GROUP['cms.content.ContentType'],
