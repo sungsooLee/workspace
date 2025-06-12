bd72a6adcfc13aaa3724738cf12835ef8976e4f8
조일환/협력사 <9500896@ict-companion.com>
2025-06-12 12:30:58 +0900

    refactor: [NLP_BO_CMS_1054] extract SelectCoordinatorModal

--- apps/bo/src/features/learning/ui/learning-resource/learning-resource-batch-setting-modal.tsx
+++ apps/bo/src/features/learning/ui/learning-resource/learning-resource-batch-setting-modal.tsx
@@ -1,10 +1,7 @@
-import formStyles from '@learnway/styles/bo/assets/styles/modules/form.module.css'; // form module css
+import formStyles from '@learnway/styles/bo/assets/styles/modules/form.module.css';
 import dynamicFormStyles from '@learnway/styles/bo/assets/styles/modules/dynamic.form.module.css';
-import searchStyles from '@learnway/styles/bo/assets/styles/modules/search-box.module.css';
-import popSearchStyles from '@learnway/styles/bo/assets/styles/modules/popup-search.module.css';
-import popupStyles from '@learnway/styles/bo/assets/styles/modules/popup-contents.module.css';
-import popLayoutstyles from './contents-layout.module.css'; // 팝업 컨텐츠 레이아웃
-import styles from './setting-item.module.css'; // 화면 css
+import popLayoutstyles from './contents-layout.module.css';
+import styles from './setting-item.module.css';
 import { useState } from 'react';
 import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
 import {
@@ -12,11 +9,8 @@ import {
   Checkbox,
   ContentsRow,
   DatePicker,
-  Dropdown,
   GridBox,
-  Input,
   InputModalSelectorFormField,
-  List,
   ModalBody,
   ModalContainer,
   ModalFooter,
@@ -29,111 +23,7 @@ import {
 import { IcoAlertCircle, IcoFormRequired, IcoRefresh02, IcoSearch } from '@learnway/icons';
 import { cn } from '@learnway/shared';
 import { FormSubTitle } from '@shared/ui';
-
-function ModalManagerContent() {
-  const { close: closeModal } = useModal();
-  const [selectedValues, setSelectedValues] = useState<string[]>([]);
-  const options = [
-    { value: 'type1', label: '전체' },
-    { value: 'type2', label: '항목' },
-    { value: 'type3', label: '항목3' },
-  ];
-  return (
-    <ModalContainer>
-      <ModalBody>
-        <div className={popupStyles.wrap}>
-          <div className={popupStyles.title_wrap}>
-            <h2 className={popupStyles.title}>{'담당자를 선택하세요.'}</h2>
-          </div>
-          <div className={cn(searchStyles.start, searchStyles.wrap)}>
-            <div className={searchStyles.contents}>
-              <div className={searchStyles.item_row}>
-                <div className={searchStyles.item_wrap}>
-                  <div className={searchStyles.inner}>
-                    <div className={searchStyles.item}>
-                      <label htmlFor="name-channelName" className={searchStyles.label}>
-                        <span className={searchStyles.text}>채널</span>
-                      </label>
-                      <div className={searchStyles.box}>
-                        <Dropdown
-                          options={options}
-                          value={selectedValues}
-                          onChange={(selected) => setSelectedValues(selected)}
-                          variant="default"
-                          size={'sm'}
-                        />
-                      </div>
-                    </div>
-                    <div className={searchStyles.item}>
-                      <label htmlFor="name-managerName" className={searchStyles.label}>
-                        <span className={searchStyles.text}>담당자명</span>
-                      </label>
-                      <div className={searchStyles.box}>
-                        <Input
-                          id="name-managerName"
-                          type="text"
-                          placeholder="담당자명으로 조회하세요."
-                          value=""
-                          className={searchStyles.input}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className={searchStyles.btn_box}>
-                <Button
-                  type="button"
-                  className={searchStyles.btn_refresh}
-                  variant="search"
-                  size="sm"
-                  onlyIcon
-                >
-                  <IcoRefresh02 className={searchStyles.icon_refresh} />
-                </Button>
-                <Button
-                  type="button"
-                  variant="search"
-                  size="sm"
-                  className={searchStyles.btn_search}
-                >
-                  <IcoSearch className={searchStyles.icon_sm_search} />
-                  조회
-                </Button>
-              </div>
-            </div>
-          </div>
-          {/* 채널 리스트 */}
-          <div className={popSearchStyles.channel_wrap}>
-            <List
-              options={[
-                { value: 'type1', label: '선택한 채널의 소속 채널 소유자명 (사번 또는 이메일)' },
-                { value: 'type2', label: '선택한 채널의 소속 채널 소유자명2 (사번 또는 이메일)' },
-                { value: 'type3', label: '선택한 채널의 소속 채널 소유자명3 (사번 또는 이메일)' },
-                { value: 'type4', label: '선택한 채널의 소속 채널 소유자명4 (사번 또는 이메일)' },
-                { value: 'type5', label: '선택한 채널의 소속 채널 소유자명5 (사번 또는 이메일)' },
-                { value: 'type6', label: '선택한 채널의 소속 채널 소유자명6 (사번 또는 이메일)' },
-                { value: 'type7', label: '선택한 채널의 소속 채널 소유자명7 (사번 또는 이메일)' },
-                { value: 'type8', label: '선택한 채널의 소속 채널 소유자명8 (사번 또는 이메일)' },
-                { value: 'type9', label: '선택한 채널의 소속 채널 소유자명9 (사번 또는 이메일)' },
-                {
-                  value: 'type10',
-                  label: '선택한 채널의 소속 채널 소유자명10 (사번 또는 이메일)',
-                },
-              ]}
-              onOptionsSelect={(options) => console.log(options)}
-              hideBorder
-            />
-          </div>
-        </div>
-      </ModalBody>
-      <ModalFooter>
-        <Button label={'취소'} variant={'gray'} size={'lg'} onClick={() => closeModal()} />
-        <Button label={'확인'} variant={'primary'} size={'lg'} onClick={() => closeModal()} />
-      </ModalFooter>
-    </ModalContainer>
-  );
-}
+import { SelectCoordinatorModal } from './learning-resource-select-coordinator-modal';
 
 function BatchSettingModalComponent() {
   const { close: closeModal } = useModal();
@@ -146,27 +36,23 @@ function BatchSettingModalComponent() {
       size: 220,
       enableGrouping: false,
       meta: {
-        headerAlign: 'left', // 헤더만 가운데 정렬
-        cellAlign: 'left', // 셀은 오른쪽 정렬
+        headerAlign: 'left',
+        cellAlign: 'left',
       },
     }),
   ] as ColumnDef<any, unknown>[];
 
-  // Date picker
   const [date, setDate] = useState(new Date());
   const [date2, setDate2] = useState(new Date());
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleDate = (value: any) => {
     setDate(value);
   };
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleDate2 = (value: any) => {
     setDate2(value);
   };
 
-  // switch : 사용기한
   const [checked, setChecked] = useState<{ [key: number]: boolean }>({
     1: false,
     2: false,
@@ -175,7 +61,6 @@ function BatchSettingModalComponent() {
     5: false,
   });
 
-  // 상태 변경 함수 (Switch id에 따라 상태를 업데이트)
   const handleCheckedChange = (id: number) => (checked: boolean) => {
     setChecked((prev) => ({ ...prev, [id]: checked }));
   };
@@ -186,7 +71,6 @@ function BatchSettingModalComponent() {
         <div className={cn(popLayoutstyles.start, popLayoutstyles.wrap)}>
           <div className={popLayoutstyles.contents}>
             <div className={popLayoutstyles.left_contents}>
-              {/* settimg-item.module.css */}
               <div className={styles.start}>
                 <GridBox
                   data={[
@@ -229,49 +113,41 @@ function BatchSettingModalComponent() {
                 <div className={formStyles.form_item}>
                   <label htmlFor="name-manager" className={formStyles.form_label}>
                     <span className={cn(formStyles.form_text)}>담당자</span>
-                    {/* 필수 케이스 */}
                     <span className={cn(formStyles.status, formStyles.required)}>
                       <IcoFormRequired width={12} height={12} />
                     </span>
                   </label>
-                  {/* 퍼블수정 20240317 : Modal 수정 S  */}
                   <div className={formStyles.input_box}>
                     <InputModalSelectorFormField
                       modalConfig={{
                         width: 'md',
-                        content: <ModalManagerContent />,
+                        content: <SelectCoordinatorModal />,
                       }}
                       placeholder={'담당자를 선택하세요.'}
                     />
                   </div>
-                  {/* 퍼블수정 20240317 : Modal 수정 E  */}
                 </div>
                 <div className={formStyles.form_item}>
                   <label htmlFor="name-managerNum" className={formStyles.form_label}>
                     <span className={cn(formStyles.form_text)}>연락처</span>
-                    {/* 필수 케이스 */}
                     <span className={cn(formStyles.status, formStyles.required)}>
                       <IcoFormRequired width={12} height={12} />
                     </span>
                   </label>
                   <div className={formStyles.input_box}>
-                    {/* 퍼블수정 20240313 : PhoneNumber 컴포넌트로 수정 S */}
                     <PhoneNumber
                       options={[
                         { value: 'type1', label: '+82' },
                         { value: 'type2', label: '+83' },
                       ]}
                     />
-                    {/* 퍼블수정 20240313 : PhoneNumber 컴포넌트로 수정 E */}
                   </div>
                 </div>
               </ContentsRow>
               <ContentsRow type="horizontal">
-                {/* form_item */}
                 <div className={formStyles.form_item}>
                   <label htmlFor="name-term" className={formStyles.form_label}>
                     <span className={formStyles.form_text}>사용기한</span>
-                    {/* 필수 케이스 */}
                     <span className={cn(formStyles.status, formStyles.required)}>
                       <IcoFormRequired width={12} height={12} />
                     </span>
@@ -287,7 +163,6 @@ function BatchSettingModalComponent() {
                     </Tooltip>
                   </label>
                   <div className={formStyles.input_box}>
-                    {/* Switch 텍스트 : '무기한' : '기간 설정' */}
                     <Switch
                       id="switch01"
                       className={formStyles.btn_switch}
@@ -320,11 +195,9 @@ function BatchSettingModalComponent() {
                 </div>
               )}
               <ContentsRow type="horizontal">
-                {/* Textarea type */}
                 <div className={formStyles.form_item}>
                   <label htmlFor="name-conjugation" className={formStyles.form_label}>
                     <span className={formStyles.form_text}>교육자원 활용여부</span>
-                    {/* 필수 케이스 */}
                     <span className={cn(formStyles.status, formStyles.required)}>
                       <IcoFormRequired width={12} height={12} />
                     </span>
@@ -374,7 +247,7 @@ function BatchSettingModalComponent() {
               </ContentsRow>
               <div className={formStyles.form_contents_wrap}>
                 <strong className={formStyles.tit_sub}>
-                  최종 확인{/* 필수 케이스 */}
+                  최종 확인
                   <span className={cn(formStyles.status, formStyles.required)}>
                     <IcoFormRequired width={12} height={12} />
                   </span>
@@ -384,7 +257,6 @@ function BatchSettingModalComponent() {
                     <div className={formStyles.form_item}>
                       <label htmlFor="name-confirm" className={formStyles.form_label}>
                         <span className={formStyles.form_text}>검수 확인</span>
-                        {/* 필수 케이스 */}
                         <span className={cn(formStyles.status, formStyles.required)}>
                           <IcoFormRequired width={12} height={12} />
                         </span>
@@ -404,7 +276,6 @@ function BatchSettingModalComponent() {
                     <div className={formStyles.form_item}>
                       <label htmlFor="name-confirm2" className={formStyles.form_label}>
                         <span className={formStyles.form_text}>저작권 확인</span>
-                        {/* 필수 케이스 */}
                         <span className={cn(formStyles.status, formStyles.required)}>
                           <IcoFormRequired width={12} height={12} />
                         </span>
@@ -426,7 +297,6 @@ function BatchSettingModalComponent() {
                     <div className={formStyles.form_item}>
                       <label htmlFor="name-confirm3" className={formStyles.form_label}>
                         <span className={formStyles.form_text}>보안 확인</span>
-                        {/* 필수 케이스 */}
                         <span className={cn(formStyles.status, formStyles.required)}>
                           <IcoFormRequired width={12} height={12} />
                         </span>
