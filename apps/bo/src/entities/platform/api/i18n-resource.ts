d9c4e38c9eae14e38af18e27499a29be5ab6f8c4
최원석/L&D플랫폼팀 <1seok@hyundai-autoever.com>
2025-06-12 10:54:19 +0900

    feat: [common] 다국어 파일 s3 json -> 목업 데이터 가지고 오는 로직으로 수정, API관리 화면 트리 0뎁스 클릭 방지 추가

--- apps/bo/src/entities/platform/api/i18n-resource.ts
+++ apps/bo/src/entities/platform/api/i18n-resource.ts
@@ -1,18 +1,45 @@
 import { httpService } from '@learnway/shared';
-import { PMSApiPrefix } from '@learnway/config';
+import { isLocal, PMSApiPrefix } from '@learnway/config';
 
 import i18nResourceEnMock from '../../mock/i18n-resource-en.json';
 import i18nResourceKoMock from '../../mock/i18n-resource-ko.json';
 
 export default class I18nResourceService {
-  static fetchResource(languageCode: string) {
-    //return httpService.get<Company[]>(`${PMSApiPrefix()}/codes?languageCode=${languageCode}`);
-    console.log('I18nResourceService.fetchResource', languageCode);
-    return new Promise((resolve) =>
-      setTimeout(
-        () => resolve((languageCode === 'ko' ? i18nResourceKoMock : i18nResourceEnMock) as any),
-        1000,
-      ),
-    );
+  static async fetchResource(languageCode: string) {
+    console.log();
+
+    const sources = [
+      {
+        type: 's3',
+        url: `${import.meta.env.VITE_AXIOS_S3_URL}/public/${languageCode}.json`,
+      },
+      {
+        type: 'local',
+        loader: () => this.getLocalMockData(languageCode),
+      },
+    ];
+
+    for (const source of sources) {
+      try {
+        if (source.type === 's3' && !isLocal()) {
+          const response = await fetch(source.url as string);
+          if (!response.ok) throw new Error(`S3 fetch failed: ${response.status}`);
+          return await response.json();
+        } else if (source.type === 'local' && source.loader) {
+          return await source.loader();
+        }
+      } catch (error) {
+        continue;
+      }
+    }
+    throw new Error('모든 번역 소스 로딩 실패');
+  }
+
+  static getLocalMockData(languageCode: string) {
+    return new Promise((resolve) => {
+      const mockData = languageCode === 'ko' ? i18nResourceKoMock : i18nResourceEnMock;
+      console.log('local');
+      setTimeout(() => resolve(mockData), 100);
+    });
   }
 }
