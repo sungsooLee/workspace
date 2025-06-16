51b97fcae1ffe04be720c7fbcfff158efa7f7d1f
조일환/협력사 <9500896@ict-companion.com>
2025-06-16 19:49:45 +0900

    feat: [NLP_BO_CMS_1001] 버튼 비활성화 반영(child일 경우)

--- apps/bo/src/features/learning/ui/learning-resource/learning-resource-table.tsx
+++ apps/bo/src/features/learning/ui/learning-resource/learning-resource-table.tsx
@@ -18,7 +18,7 @@ import { learningResourceQueryOptions } from '@entities/learning-resource';
 import { ModifierInfoModal } from './learning-resource-modifier-info-modal';
 import { ProgramGuideModal } from './learning-resource-program-guide-modal';
 import { BatchSettingModal } from './learning-resource-batch-setting-modal';
-import { map, some, uniq } from 'lodash';
+import { first, get, map, some, uniq } from 'lodash';
 import { CopyModal } from './learning-resource-copy-modal';
 import { useRouter } from '@tanstack/react-router';
 import { useFetchAuthUser } from '@learnway/auth/entities';
@@ -288,7 +288,7 @@ function LearningResourceTableComponent() {
   };
 
   const { provider: searchProvider, getValues, setOptions, setValue } = useSearchBox(searchConfig);
-  const { config: gConfig, gridFetch } = useGridBox(gridConfig, getValues);
+  const { config: gConfig, gridFetch, data } = useGridBox(gridConfig, getValues);
   const [selectedRows, setSelectedRows] = useState<any[]>([]);
   const [tableInstance, setTableInstance] = useState<Table<any>>(); // Grid 로부터 받을 table 인스턴스를 저장할 상태
 
@@ -326,7 +326,10 @@ function LearningResourceTableComponent() {
     gridFetch(query);
   }
 
-  function handleShare() {}
+  function handleShare() {
+    console.log('🚀 ~ handleShare ~ getValues():', getValues());
+    console.log('🚀 ~ handleShare ~ data:', data);
+  }
 
   function openProgramGuide() {
     openModal({
@@ -388,7 +391,12 @@ function LearningResourceTableComponent() {
             <Button
               variant="text"
               label={t('LABEL.grid.header.share')}
-              disabled={selectedRows.length !== 1}
+              disabled={
+                selectedRows.length !== 1 ||
+                !data?.content?.find(
+                  (_: any) => _.contentUuid === get(first(selectedRows), 'contentUuid'),
+                ) // child
+              }
               onClick={handleShare}
             />
             <Button
@@ -421,7 +429,12 @@ function LearningResourceTableComponent() {
               variant="text"
               label={t('LABEL.grid.header.copy')}
               icon={<IcoCopy width={16} height={16} stroke="#4C515E" />}
-              disabled={selectedRows.length !== 1}
+              disabled={
+                selectedRows.length !== 1 ||
+                !data?.content?.find(
+                  (_: any) => _.contentUuid === get(first(selectedRows), 'contentUuid'),
+                ) // child
+              }
               onClick={handleCopy}
             />
           </>
