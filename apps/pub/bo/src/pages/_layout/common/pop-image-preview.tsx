1ebeeb3c8c0bd1c0a65b851a22a73203c6cdad68
이성수/협력사 <9490817@ict-companion.com>
2025-06-17 16:42:14 +0900

    [퍼블] bo 이미지 미리보기 팝업 수정

--- apps/pub/bo/src/pages/_layout/common/pop-image-preview.tsx
+++ apps/pub/bo/src/pages/_layout/common/pop-image-preview.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useRef } from 'react';
 import { createFileRoute } from '@tanstack/react-router';
 import { ModalBody, ModalContainer, useModal, Button } from '@learnway/ui';
-import { IcoDownload } from '@learnway/icons';
+import { IcoDownload, IcoArrowPrev, IcoArrowNext } from '@learnway/icons';
 import { cn } from '@learnway/shared';
 
 // style
-import styles from './pop-image-preview.module.css';
+import styles from '@learnway/styles/bo/assets/styles/modules/pop-image-preview.module.css';
 
 /* images */
 import mapImg from '../../../assets/images/temp/img_temp_map.jpg';
@@ -16,33 +16,40 @@ export const Route = createFileRoute('/_layout/common/pop-image-preview')({
 
 function RouteComponent() {
   const { open: openModal } = useModal();
-  const PreviewImageContent = () => {
-    return (
-      <ModalContainer>
-        <ModalBody>
-          <div className={cn(styles.start, styles.img_wrap)}>
-            <img src={mapImg} alt="" />
-          </div>
-        </ModalBody>
-      </ModalContainer>
-    );
-  };
-  // 한번만 실행
-  const hasRun = useRef(false);
   useEffect(() => {
-    if (!hasRun.current) {
-      openModal({
-        width: 'full',
-        height: 'full',
-        content: <PreviewImageContent />,
-        headerActionNode: (
-          <Button onlyIcon>
-            <IcoDownload width={40} height={40} stroke="#131C30" />
-          </Button>
-        ),
-      });
-      hasRun.current = true;
-    }
+    openModal({
+      width: 'full',
+      height: 'full',
+      content: <PreviewImageContent />,
+      headerActionNode: (
+        <Button onlyIcon>
+          <IcoDownload width={40} height={40} stroke="#131C30" />
+        </Button>
+      ),
+    });
   }, [openModal]);
   return <div>이미지 미리보기</div>;
 }
+
+const PreviewImageContent = () => {
+  return (
+    <ModalContainer>
+      <ModalBody>
+        <div className={cn(styles.start, styles.img_wrap)}>
+          <img src={mapImg} alt="" />
+          <Button
+            onlyIcon
+            icon={<IcoArrowPrev width={32} height={32} />}
+            className={styles.btn_prev}
+            disabled
+          />
+          <Button
+            onlyIcon
+            icon={<IcoArrowNext width={32} height={32} />}
+            className={styles.btn_next}
+          />
+        </div>
+      </ModalBody>
+    </ModalContainer>
+  );
+};
---
+++ apps/pub/bo/src/pages/_layout/completion/detail-calculation-reason.tsx
@@ -0,0 +1,141 @@
+/* eslint-disable @nx/enforce-module-boundaries */
+import { createFileRoute } from '@tanstack/react-router';
+import { PageContainer } from '../../../widgets/layout/ui/container/page-container';
+import { SectionLayout } from '../-components/section-layout';
+import { TreeBox, TreeNode, Button, TableBox, Input, Switch, Checkbox } from '@learnway/ui';
+import { MainContents } from '../../../../../../bo/src/widgets/layout/ui/container/slot/main-contents';
+import { FormSubTitle } from '../../../../../../bo/src/shared/ui/form';
+import { IcoMinus, IcoPlus } from '@learnway/icons';
+import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
+
+export const Route = createFileRoute('/_layout/completion/detail-calculation-reason')({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const sampleData: TreeNode[] = [
+    {
+      key: '1',
+      title: 'Root Node 1',
+      isUsed: false,
+      children: [
+        {
+          key: '1-1',
+          title: 'Child 1',
+          isUsed: true,
+          children: [
+            { key: '1-1-1', title: 'Grandchild 1', isUsed: true },
+            { key: '1-1-2', title: 'Grandchild 2', isUsed: false },
+          ],
+        },
+        {
+          key: '1-2',
+          title: 'Child 2',
+          isUsed: true,
+          children: [
+            { key: '2-1', title: 'Child 3', isUsed: false },
+            { key: '2-2', title: 'Child 4', isUsed: false },
+          ],
+        },
+      ],
+    },
+  ];
+  const columnHelper = createColumnHelper<any>();
+  // thead : 'value'
+  const data: any[] = [
+    {
+      title1: '식비',
+      title2: (
+        <div className="">
+          <Input type={'text'} placeholder={'입력'} value={''} />
+          <Button onlyIcon icon={<IcoPlus width={16} height={16} stroke={'#4C515E'} />} />
+          <Button onlyIcon icon={<IcoMinus width={16} height={16} stroke={'#4C515E'} />} />
+        </div>
+      ),
+      title3: <Checkbox />,
+      title4: <Input suffixText={'원'} placeholder={'입력'} type={'number'} />,
+      title5: <Switch />,
+    },
+  ];
+
+  // Thead 정의
+  const columns = [
+    columnHelper.accessor('title1', {
+      cell: (info) => info.getValue(),
+      header: '세부산출근거',
+      size: 200,
+      meta: {
+        headerAlign: 'center',
+      },
+    }),
+    columnHelper.accessor('title2', {
+      cell: (info) => info.getValue(),
+      header: '세부 항목',
+      meta: {
+        headerAlign: 'center',
+        cellAlign: 'center',
+        size: 'auto',
+      },
+    }),
+    columnHelper.accessor('title3', {
+      cell: (info) => info.getValue(),
+      header: '단가 지정',
+      size: 80,
+      meta: {
+        headerAlign: 'center',
+        cellAlign: 'center',
+      },
+    }),
+    columnHelper.accessor('title4', {
+      cell: (info) => info.getValue(),
+      header: '단가',
+      meta: {
+        headerAlign: 'center',
+        cellAlign: 'center',
+        size: 'auto',
+      },
+    }),
+    columnHelper.accessor('title5', {
+      cell: (info) => info.getValue(),
+      header: '활성화 여부',
+      size: 100,
+      meta: {
+        headerAlign: 'center',
+        cellAlign: 'center',
+      },
+    }),
+  ] as ColumnDef<any, unknown>[];
+  return (
+    <PageContainer>
+      <MainContents>
+        <SectionLayout contentsRatio={'thirty'}>
+          <TreeBox data={sampleData} treeId={'menu-tree'} title={'교육본부'} />
+          <div>
+            <FormSubTitle
+              label={'세부산출근거 단가'}
+              lineType={'dark'}
+              actionNode={
+                <>
+                  <Button
+                    variant={'text'}
+                    size={'sm'}
+                    label={'추가'}
+                    icon={<IcoPlus width={16} height={16} stroke={'#4C515E'} />}
+                  />
+                  <Button
+                    variant={'text'}
+                    size={'sm'}
+                    label={'삭제'}
+                    icon={<IcoMinus width={16} height={16} stroke={'#4C515E'} />}
+                  />
+                  <Button variant={'save'} size={'sm'} label={'저장'} />
+                </>
+              }
+            />
+            <TableBox data={data} columns={columns} tableMode={true} multiple />
+          </div>
+        </SectionLayout>
+      </MainContents>
+    </PageContainer>
+  );
+}
