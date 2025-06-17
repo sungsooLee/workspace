6609d693936e3664d0336d14e40a6215e5139e51
조일환/협력사 <9500896@ict-companion.com>
2025-06-17 14:40:25 +0900

    feat: [NLP_BO_CMS_1001] i18n defaultValue 명시

--- apps/bo/src/features/learning/ui/learning-resource/learning-resource-table.tsx
+++ apps/bo/src/features/learning/ui/learning-resource/learning-resource-table.tsx
@@ -38,28 +38,28 @@ function LearningResourceTableComponent() {
         {
           name: 'tenantUuid',
           type: 'dropdown',
-          label: t('LABEL.form.label.tenant'),
+          label: t('LABEL.form.label.tenant', '테넌트'),
           value: '',
           format: 'number',
-          presetOptionLabel: t('LABEL.form.label.select'),
+          presetOptionLabel: t('LABEL.form.label.select', '선택'),
           options: [],
         },
         {
           name: 'channelUuid',
           type: 'dropdown',
-          label: t('LABEL.form.label.channel'),
+          label: t('LABEL.form.label.channel', '채널'),
           value: '',
-          presetOptionLabel: t('LABEL.form.label.select'),
+          presetOptionLabel: t('LABEL.form.label.select', '선택'),
           options: [],
         },
         {
           name: 'contentTypes',
           type: 'dropdown',
-          label: t('LABEL.form.label.contentType'),
+          label: t('LABEL.form.label.contentType', '유형'),
           value: [ALL_OPTION],
           isMulti: true,
           variant: 'text',
-          presetOptionLabel: t('LABEL.form.label.all'),
+          presetOptionLabel: t('LABEL.form.label.all', '전체'),
           optionsConfig: {
             codeGroup: CODE_GROUP['cms.content.ContentType'],
           },
@@ -67,7 +67,7 @@ function LearningResourceTableComponent() {
         {
           name: 'contentName',
           type: 'text',
-          label: t('LABEL.form.label.contentName'),
+          label: t('LABEL.form.label.contentName', '학습자원명'),
           value: '',
         },
       ],
@@ -75,9 +75,9 @@ function LearningResourceTableComponent() {
         {
           name: 'isVendored',
           type: 'dropdown',
-          label: t('LABEL.form.label.isVendored'),
+          label: t('LABEL.form.label.isVendored', '외주여부'),
           value: '',
-          presetOptionLabel: t('LABEL.form.label.all'),
+          presetOptionLabel: t('LABEL.form.label.all', '전체'),
           optionsConfig: {
             options: [
               { value: 'true', label: 'Y' },
@@ -88,9 +88,9 @@ function LearningResourceTableComponent() {
         {
           name: 'isContentEnabled',
           type: 'dropdown',
-          label: t('LABEL.form.label.isContentEnabled'),
+          label: t('LABEL.form.label.isContentEnabled', '사용가능'),
           value: '',
-          presetOptionLabel: t('LABEL.form.label.all'),
+          presetOptionLabel: t('LABEL.form.label.all', '전체'),
           optionsConfig: {
             options: [
               { value: 'true', label: 'Y' },
@@ -101,9 +101,9 @@ function LearningResourceTableComponent() {
         {
           name: 'isCourseUsed',
           type: 'dropdown',
-          label: t('LABEL.form.label.isCourseUsed'),
+          label: t('LABEL.form.label.isCourseUsed', '교육활용'),
           value: '',
-          presetOptionLabel: t('LABEL.form.label.all'),
+          presetOptionLabel: t('LABEL.form.label.all', '전체'),
           optionsConfig: {
             options: [
               { value: 'true', label: 'Y' },
@@ -114,7 +114,7 @@ function LearningResourceTableComponent() {
         {
           name: 'coordinatorName',
           type: 'text',
-          label: t('LABEL.form.label.coordinator'),
+          label: t('LABEL.form.label.coordinator', '담당자'),
           value: '',
         },
       ],
@@ -122,9 +122,9 @@ function LearningResourceTableComponent() {
         {
           name: 'langCountryCode',
           type: 'dropdown',
-          label: t('LABEL.form.label.langCountryCode'),
+          label: t('LABEL.form.label.langCountryCode', '언어'),
           value: '',
-          presetOptionLabel: t('LABEL.form.label.select'),
+          presetOptionLabel: t('LABEL.form.label.select', '선택'),
           optionsConfig: {
             codeGroup: CODE_GROUP['pms.multilingual.LangCountryCode'],
           },
@@ -132,7 +132,7 @@ function LearningResourceTableComponent() {
         {
           name: 'createdBy',
           type: 'text',
-          label: t('LABEL.form.label.createdBy'),
+          label: t('LABEL.form.label.createdBy', '등록자'),
           value: '',
         },
       ],
@@ -154,13 +154,13 @@ function LearningResourceTableComponent() {
       {
         size: 79,
         name: 'contentType',
-        label: t('LABEL.grid.column.contentType'),
+        label: t('LABEL.grid.column.contentType', '유형'),
         render: (_: any) => t(`cms.content.ContentType.${_.getValue()}`),
       },
       {
         size: 338,
         name: 'contentName',
-        label: t('LABEL.grid.column.contentName'),
+        label: t('LABEL.grid.column.contentName', '학습자원명'),
         meta: {
           size: 'auto',
         },
@@ -180,7 +180,7 @@ function LearningResourceTableComponent() {
       {
         size: 127,
         name: 'tenantName',
-        label: t('LABEL.grid.column.tenant'),
+        label: t('LABEL.grid.column.tenant', '테넌트'),
         meta: {
           size: 'auto',
         },
@@ -188,7 +188,7 @@ function LearningResourceTableComponent() {
       {
         size: 153,
         name: 'channelName',
-        label: t('LABEL.grid.column.channel'),
+        label: t('LABEL.grid.column.channel', '채널'),
         meta: {
           size: 'auto',
         },
@@ -196,12 +196,12 @@ function LearningResourceTableComponent() {
       {
         size: 104,
         name: 'coordinatorName',
-        label: t('LABEL.grid.column.coordinator'),
+        label: t('LABEL.grid.column.coordinator', '담당자'),
       },
       {
         size: 125,
         name: 'contentAddInfo',
-        label: t('LABEL.grid.column.detailInfo'),
+        label: t('LABEL.grid.column.contentAddInfo', '세부정보'),
         render: (_: any) => {
           if (_.row.original.contentAddInfoType !== 'VIDEO_ADD_INFO')
             // enum code 사용하도록 변경해야 함
@@ -218,7 +218,7 @@ function LearningResourceTableComponent() {
       {
         size: 137,
         name: 'util',
-        label: t('LABEL.grid.column.util'),
+        label: t('LABEL.grid.column.util', '기능'),
         render: (_: any) => (
           <span>
             <Button
@@ -227,7 +227,7 @@ function LearningResourceTableComponent() {
                 e.stopPropagation();
               }}
             >
-              {t('LABEL.grid.column.preview')}
+              {t('LABEL.grid.column.preview', '미리보기')}
             </Button>
             {
               /* 시험지, 문제은행, 설문지 */
@@ -238,7 +238,7 @@ function LearningResourceTableComponent() {
                     e.stopPropagation();
                   }}
                 >
-                  {t('LABEL.grid.column.questionManage')}
+                  {t('LABEL.grid.column.questionManage', '문항관리')}
                 </Button>
               )
             }
@@ -248,19 +248,19 @@ function LearningResourceTableComponent() {
       {
         size: 95,
         name: 'isContentEnabled',
-        label: t('LABEL.grid.column.isContentEnabled'),
+        label: t('LABEL.grid.column.isContentEnabled', '사용가능'),
         render: (_: any) => (_.getValue() ? 'Y' : 'N'),
       },
       {
         size: 83,
         name: 'langCountryCode',
-        label: t('LABEL.grid.column.localization'),
+        label: t('LABEL.grid.column.translation', '번역'),
         render: (_: any) => t(`pms.multilingual.LangCountryCode.${_.getValue()}`),
       },
       {
         size: 79,
         name: 'modifiedDate',
-        label: t('LABEL.grid.column.updatedInfo'),
+        label: t('LABEL.grid.column.updatedInfo', '수정정보'),
         render: (_: any) => (
           <Button
             className="link"
@@ -277,7 +277,7 @@ function LearningResourceTableComponent() {
               });
             }}
           >
-            {t('LABEL.form.label.look')}
+            {t('LABEL.form.label.view', '보기')}
           </Button>
         ),
       },
@@ -342,16 +342,20 @@ function LearningResourceTableComponent() {
     const selectedContentTypes = map(selectedRows, 'contentType');
     if (uniq(selectedContentTypes).length !== 1) {
       return alert({
-        title: t('LABEL.alert.contentTypeNotMatched.title'),
-        content: t('LABEL.alert.contentTypeNotMatched.content'),
+        title: t('LABEL.alert.contentTypeNotMatched.title', '같은 유형의 학습자원들을 선택하세요.'),
+        content: t(
+          'LABEL.alert.contentTypeNotMatched.content',
+          '일괄설정은 같은 유형의 학습자원들에서만 적용됩니다.',
+        ),
       });
     }
 
     const selectedIsCourseUsed = map(selectedRows, 'isCourseUsed');
     if (some(selectedIsCourseUsed)) {
+      // API로 체크하도록 변경해야 함
       return alert({
-        title: t('LABEL.alert.isCourseUsed.title'),
-        content: t('LABEL.alert.isCourseUsed.content'),
+        title: t('LABEL.alert.isCourseUsed.title', '교육과정에서 사용 중입니다.'),
+        content: t('LABEL.alert.isCourseUsed.content', '사용 중인 학습자원은 일괄설정 불가합니다.'),
       });
     }
 
@@ -364,8 +368,8 @@ function LearningResourceTableComponent() {
   function handleCopy() {
     if (selectedRows.length !== 1) {
       return alert({
-        title: t('LABEL.alert.canNotCopy.title'),
-        content: t('LABEL.alert.canNotCopy.content'),
+        title: t('LABEL.alert.canNotCopy.title', '1개의 학습자원을 선택하세요'),
+        content: t('LABEL.alert.canNotCopy.content', '복사기능은 1개의 학습자원들서만 적용됩니다.'),
       });
     }
 
@@ -390,7 +394,7 @@ function LearningResourceTableComponent() {
           <>
             <Button
               variant="text"
-              label={t('LABEL.grid.header.share')}
+              label={t('LABEL.grid.header.share', '공유')}
               disabled={
                 selectedRows.length !== 1 ||
                 !data?.content?.find(
@@ -401,33 +405,40 @@ function LearningResourceTableComponent() {
             />
             <Button
               variant="text"
-              label={t('LABEL.grid.header.guideDownload')}
+              label={t('LABEL.grid.header.guideDownload', '프로그램/가이드 다운로드')}
               icon={<IcoDownload width={16} height={16} stroke="#4C515E" />}
               onClick={openProgramGuide}
             />
             <span className="type_tooltip">
               <Button
                 variant="text"
-                label={t('LABEL.grid.header.batchSetting')}
+                label={t('LABEL.grid.header.batchSetting', '일괄설정')}
                 disabled={selectedRows.length === 0}
                 onClick={openBatchSetting}
               />
               <Tooltip
                 side="bottom"
                 align="start"
-                content={<pre>{t('LABEL.grid.tooltip.batchSetting')}</pre>}
+                content={
+                  <pre>
+                    {t(
+                      'LABEL.grid.tooltip.batchSetting',
+                      '학습자원의 담당자, 사용기한, 공유채널설정, \n교육자원활용, 보안콘텐츠 적용, 검수진행을 \n한번에 할 수 있어요.',
+                    )}
+                  </pre>
+                }
               >
                 <IcoAlertCircle width={16} height={16} fill="#A9AFB8" stroke="#ffffff" />
               </Tooltip>
             </span>
             <Button
               variant="text"
-              label={t('LABEL.grid.header.excelDownload')}
+              label={t('LABEL.grid.header.excelDownload', '엑셀 다운로드')}
               icon={<IcoDownload width={16} height={16} stroke="#4C515E" />}
             />
             <Button
               variant="text"
-              label={t('LABEL.grid.header.copy')}
+              label={t('LABEL.grid.header.copy', '복사')}
               icon={<IcoCopy width={16} height={16} stroke="#4C515E" />}
               disabled={
                 selectedRows.length !== 1 ||
