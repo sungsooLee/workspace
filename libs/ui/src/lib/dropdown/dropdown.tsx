098fc12d3607aaa16dda2cde43db018c12f75575
김태경 <tkkim@wylie.co.kr>
2025-06-26 10:22:23 +0900

    [퍼블]FO 드롭다운 재업로드

--- libs/ui/src/lib/dropdown/dropdown.tsx
+++ libs/ui/src/lib/dropdown/dropdown.tsx
@@ -231,7 +231,6 @@ const PrimitiveComponent = forwardRef<any, ReactSelectComponentProps>(
         )}
       >
         <Select
-          menuIsOpen
           id={uuid}
           ref={ref}
           options={options}
