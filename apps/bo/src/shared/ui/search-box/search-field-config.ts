e0de5a9cff041e66d1584bbbe230972bdbcea37e
이창기/협력사 <9496504@ict-companion.com>
2025-06-26 21:18:49 +0900

    fix: AutoComplate SearchBox 컴포넌트 추가

--- apps/bo/src/shared/ui/search-box/search-field-config.ts
+++ apps/bo/src/shared/ui/search-box/search-field-config.ts
@@ -1,9 +1,10 @@
 // import { DateRangePickerFormField } from '@features/learning/ui/resource/date-range-picker-form-field';
 import { DateRangeFormField } from './date-range-form-field';
-import { DropdownFormField, InputFormField } from '@features/form';
+import { AutoCompleteDropdownFormField, DropdownFormField, InputFormField } from '@features/form';
 
 export const searchFieldConfig = {
   text: InputFormField,
   dropdown: DropdownFormField,
+  'auto-dropdown': AutoCompleteDropdownFormField,
   'date-range': DateRangeFormField,
 };
