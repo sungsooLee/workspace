94f0ff39a06cd0395bf4232e695a4375357b7ebc
신민제/협력사 <9497019@ict-companion.com>
2025-07-01 16:06:04 +0900

    fix: common-popup usergroupchoicemodal tenantIds 추가

--- apps/bo/src/pages/_layout/common-popup.lazy.tsx
+++ apps/bo/src/pages/_layout/common-popup.lazy.tsx
@@ -462,7 +462,7 @@ function RouteComponent() {
                 e.stopPropagation();
                 openModal({
                   width: 'xl',
-                  content: <UserGroupTabsChoiceModal initialTab="JOB_GROUP" />,
+                  content: <UserGroupTabsChoiceModal tenantIds={[1]} initialTab="JOB_GROUP" />,
                 });
               }}
             >
