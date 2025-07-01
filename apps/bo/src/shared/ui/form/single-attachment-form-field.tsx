a037812bfb8016077d97fd6d91ae4508a0fd1234
조일환/협력사 <9500896@ict-companion.com>
2025-07-01 19:48:15 +0900

    feat: 파일 업로더 props 수정

--- apps/bo/src/shared/ui/form/single-attachment-form-field.tsx
+++ apps/bo/src/shared/ui/form/single-attachment-form-field.tsx
@@ -3,7 +3,6 @@ import { forwardRef, useEffect, useState } from 'react';
 import { SingleAttachment } from '@learnway/ui'; // @learnway/ui에서 Attachment 컴포넌트 import
 import {
   BaseFormFieldProps,
-  DEFAULT_MULTIPART_THRESHOLD,
   formatFileSize,
   S3UploaderConfig,
   useFileManager,
@@ -16,7 +15,9 @@ import formStyles from '@learnway/styles/bo/assets/styles/modules/form.module.cs
  * AttachmentFormField 컴포넌트의 props 인터페이스
  * 폼 필드로서 Attachment 컴포넌트를 래핑하여 폼 시스템과 통합합니다.
  */
-type AttachmentFormFieldProps = BaseFormFieldProps<string> & S3UploaderConfig;
+interface AttachmentFormFieldProps extends BaseFormFieldProps<string> {
+  uploadConfig: S3UploaderConfig;
+}
 
 /**
  * 폼 필드용 썸네일 이미지 업로드 컴포넌트
@@ -31,15 +32,7 @@ const SingleAttachmentFormFieldComponent = forwardRef<
 >(
   (
     {
-      s3Path,
-      groupConfig,
-      groupMode = 'batch',
-      auto = true,
-      async = true,
-      multipartThreshold = DEFAULT_MULTIPART_THRESHOLD,
-      acceptFiles = [],
-      maxFileCount = 1,
-      maxFileSize = 5 * 1024 * 1024,
+      uploadConfig,
       name,
       value,
       onChange,
@@ -48,15 +41,29 @@ const SingleAttachmentFormFieldComponent = forwardRef<
     },
     ref, // forwardRef로 전달받은 Ref 객체
   ) => {
+    const {
+      s3Path,
+      affairsType,
+      languageCode = 'ko',
+      groupUuid,
+      groupMode = 'batch',
+      auto = true,
+      async = true,
+      acceptFiles = [],
+      maxFileCount = 10,
+      maxFileSize = 5 * 1024 * 1024,
+    } = uploadConfig;
+
     const { getFileInfo } = useFileManager();
     const { stats, files, addFiles, onPause, onRetry, onResume, onRemove, onFetch, inputAccept } =
       useS3Uploader({
         s3Path,
-        groupConfig,
+        affairsType,
+        languageCode,
+        groupUuid,
         groupMode,
         auto,
         async,
-        multipartThreshold,
         acceptFiles,
         maxFileCount,
         maxFileSize,
