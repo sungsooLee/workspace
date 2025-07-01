98f4a1a540bfb7724356f285336e43fcb15eacc4
협력사/김석태 <9496491@ict-companion.com>
2025-07-01 19:21:17 +0900

    feat: [SCORM] fo vite config에 resolve 추가

--- apps/fo/vite.config.ts
+++ apps/fo/vite.config.ts
@@ -5,6 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import svgr from '@svgr/rollup';
+import path from 'path';
 
 // vitest automatically sets NODE_ENV to 'test' when running tests
 const isTest = process.env.NODE_ENV === 'test';
@@ -50,6 +51,19 @@ export default defineConfig(({ mode }) => {
     //  plugins: [ nxViteTsPaths() ],
     // },
     base: basePath,
+    resolve: {
+      alias: [
+        { find: '@/', replacement: path.resolve(__dirname, 'src') },
+        { find: '@app', replacement: path.resolve(__dirname, 'src/app') },
+        { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
+        { find: '@entities', replacement: path.resolve(__dirname, 'src/entities') },
+        { find: '@features', replacement: path.resolve(__dirname, 'src/features') },
+        { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+        { find: '@shared', replacement: path.resolve(__dirname, 'src/shared') },
+        { find: '@types', replacement: path.resolve(__dirname, 'src/types') },
+        { find: '@widgets', replacement: path.resolve(__dirname, 'src/widgets') },
+      ],
+    },
     build: {
       outDir: '../../dist/apps/fo',
       assetsDir: 'assets',
