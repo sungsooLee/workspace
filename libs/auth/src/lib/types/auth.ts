d4e8e035eaa2d2bad2bee91a3c8187c621c9bbd3
협력사/유홍식 <9488404@ict-companion.com>
2025-07-02 10:27:14 +0900

    feat: [공통] myRoles 타입 정의

--- libs/auth/src/lib/types/auth.ts
+++ libs/auth/src/lib/types/auth.ts
@@ -24,7 +24,6 @@ export interface AuthUser {
   activeRole?: Role;
   mainTenantId?: number;
   menus?: any;
-  myRoles?: any; // 나의 역할 목록 조회 결과
   avataImage?: string;
   latestLoginDatetime?: Date;
   phoneNumberNationCode?: string;
@@ -33,6 +32,7 @@ export interface AuthUser {
   lastVisitedBoTenantId: number | null;
   lastVisitedFoRoleId: number | null;
   lastVisitedFoTenantId: number | null;
+  myRoles?: RoleInfo[]; // 나의 역할 목록 조회 결과
 }
 
 export interface AuthSSOHealthcheck {
@@ -60,3 +60,73 @@ export interface Role {
 
 export type AUTH_TOOL_TYPE = 'PHONE' | 'EMAIL';
 export type MEMBER_TYPE = 'GENERAL' | 'HTA';
+
+export interface AuthUserInfo {
+  userId: number;
+  uuid: string;
+  employeeNumber: string;
+  name: string;
+  birthday: number;
+  companyId: number;
+  companyCode: string;
+}
+
+export interface RoleInfo {
+  /** 역할 ID */
+  roleId: number;
+  /** 사이트 구분 */
+  siteScope: string;
+  /** 역할 타입 */
+  roleType: string;
+  /** 부모 역할 ID */
+  parentRoleId: number;
+  /** 역할 트리 순서 */
+  sortOrder: number;
+  /** 이름 */
+  name: string;
+  /** 설명 */
+  description: string;
+  /** 테넌트 적용 범위 */
+  tenantScope: string;
+  /** 테넌트 ID */
+  tenantId: number;
+  /** 테넌트명 */
+  tenantName: string;
+  /** 회사 적용 범위 */
+  companyScope: string;
+  /** 회사 목록 */
+  companies: [
+    {
+      id: number;
+      name: string;
+    },
+  ];
+  /** 채널 적용 범위 */
+  channelScope: string;
+  /** 채널 목록 */
+  channels: [
+    {
+      uuid: string;
+      name: string;
+    },
+  ];
+  /** 팀 적용 범위 */
+  deptScope: string;
+  /** 팀 목록 */
+  depts: [
+    {
+      id: number;
+      name: string;
+    },
+  ];
+  /** 사용여부 */
+  isUsed: boolean;
+  /** 등록자ID */
+  createdBy: string;
+  /** 등록일시 */
+  createdDate: string;
+  /** 최종수정자ID */
+  lastModifiedBy: string;
+  /** 최종수정일시 */
+  modifiedDate: string;
+}
