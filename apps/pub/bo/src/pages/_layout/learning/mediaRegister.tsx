e70ad47b3dd22c9da3fd145e6edd492e74d7c7b9
이성수/협력사 <9490817@ict-companion.com>
2025-07-03 16:56:48 +0900

    [퍼블] bo 아이콘 수정

--- apps/pub/bo/src/pages/_layout/learning/mediaRegister.tsx
+++ apps/pub/bo/src/pages/_layout/learning/mediaRegister.tsx
@@ -53,6 +53,7 @@ import {
   IcoRefresh02,
   IcoSearch,
   IcoStatusFail,
+  IcoDelete04,
 } from '@learnway/icons';
 import { FormSubTitle } from '../../../../../../bo/src/shared/ui/form';
 
@@ -1152,9 +1153,13 @@ function RouteComponent() {
                       <Button variant="gray" size="sm" className={dynamicFormStyles.btn_edit}>
                         자막 변경
                       </Button>
-                      <Button onlyIcon className={dynamicFormStyles.btn_delete}>
-                        <IcoCloseCircle width={24} height={24} fill="#D6DAE1" stroke="#ffffff" />
-                      </Button>
+                      <Button
+                        onlyIcon
+                        className={dynamicFormStyles.btn_delete}
+                        icon={
+                          <IcoDelete04 width={20} height={20} fill={'none'} stroke={'#4C515E'} />
+                        }
+                      />
                     </ContentsRow>
                     <ContentsRow className={dynamicFormStyles.row_inner}>
                       <Dropdown
@@ -1172,9 +1177,13 @@ function RouteComponent() {
                       <Button variant="gray" size="sm" className={dynamicFormStyles.btn_edit}>
                         자막 변경
                       </Button>
-                      <Button onlyIcon className={dynamicFormStyles.btn_delete}>
-                        <IcoCloseCircle width={24} height={24} fill="#D6DAE1" stroke="#ffffff" />
-                      </Button>
+                      <Button
+                        onlyIcon
+                        className={dynamicFormStyles.btn_delete}
+                        icon={
+                          <IcoDelete04 width={20} height={20} fill={'none'} stroke={'#4C515E'} />
+                        }
+                      />
                     </ContentsRow>
                     <ContentsRow className={dynamicFormStyles.row_inner}>
                       <Dropdown
@@ -1192,9 +1201,13 @@ function RouteComponent() {
                       <Button variant="gray" size="sm" className={dynamicFormStyles.btn_edit}>
                         자막 변경
                       </Button>
-                      <Button onlyIcon className={dynamicFormStyles.btn_delete}>
-                        <IcoCloseCircle width={24} height={24} fill="#D6DAE1" stroke="#ffffff" />
-                      </Button>
+                      <Button
+                        onlyIcon
+                        className={dynamicFormStyles.btn_delete}
+                        icon={
+                          <IcoDelete04 width={20} height={20} fill={'none'} stroke={'#4C515E'} />
+                        }
+                      />
                     </ContentsRow>
                     <ContentsRow className={dynamicFormStyles.row_inner}>
                       <Dropdown
