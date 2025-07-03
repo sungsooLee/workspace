cb26eccbf40633c4070d8cd4849785f42aa34098
정민혁/협력사 <9493801@ict-companion.com>
2025-07-03 16:19:57 +0900

    [퍼블] 학습창 css 정리

--- apps/pub/fo/src/pages/_learning/ebook.tsx
+++ apps/pub/fo/src/pages/_learning/ebook.tsx
@@ -1,7 +1,7 @@
 import { isMobile } from 'react-device-detect';
 import { cn } from '@learnway/shared';
 import { createFileRoute, Link } from '@tanstack/react-router';
-import styles from './ebook.module.css';
+import styles from './learning.module.css';
 
 import ebookImg from '@learnway/styles/fo/assets/images/temp/img_ebook.png';
 
