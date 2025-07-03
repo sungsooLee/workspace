d1bba9f046492fdb2162d9375a152046c7e90e5f
협력사/김석태 <9496491@ict-companion.com>
2025-07-03 15:58:00 +0900

    feat: [NLP_FO_CLA_1025] 경로 구조 변경

---
+++ apps/fo/src/features/learning-window/ui/video-player.tsx
@@ -0,0 +1,32 @@
+import { Player, usePlayer } from '@learnway/ui';
+
+const VideoPlayerComponent = () => {
+  const player = usePlayer();
+  return (
+    <Player
+      ref={player.playerRef}
+      playing={player.playing}
+      onProgress={player.onProgress}
+      onDuration={player.onDuration}
+      // url={''}
+      config={{
+        file: {
+          // attributes: {
+          //   crossOrigin: 'anonymous', // 자막 로드할 때 필요할 수 있음
+          // },
+          tracks: [
+            {
+              kind: 'subtitles',
+              src: 'https://gist.githubusercontent.com/samdutton/ca37f3adaf4e23679957b8083e061177/raw/sample.vtt',
+              srcLang: 'en',
+              default: true,
+              label: 'en',
+            },
+          ],
+        },
+      }}
+    />
+  );
+};
+
+export const VideoPlayer = VideoPlayerComponent;
--- apps/fo/src/pages/_learning/learning-window.tsx
+++ apps/fo/src/pages/_learning/learning-window.tsx
@@ -1,25 +1,15 @@
 import { useEffect, useState } from 'react';
-import { t } from 'i18next';
-
 import { createFileRoute, useRouter, useRouterState } from '@tanstack/react-router';
 
-import { IcoCaution03 } from '@learnway/icons';
-import { Button, Checkbox, useModal } from '@learnway/ui';
-import { cn } from '@learnway/shared';
-import { useDeleteUser, useLogoutUser } from '@learnway/auth/entities';
-
-import { MAIN_CONTAINERS } from '@widgets/layout';
-import { pageRouteConfig } from '@features/auth';
-import { BrowserFooter, MobileResponsiveContainerFooter } from '@shared/m.ui';
-
-import styles from '@learnway/styles/fo/pages/_layout/my-page/privacy/withdraw-menbership.module.css';
+import { t } from 'i18next';
 
 import { ScormPlayer, ScormPlayerConfigProperties } from '@features/learning-window';
 import { useGetContentDetail } from '@entities/content/service/content.hook';
 import { useLearningWindowStore } from '@widgets/layout/service/learning-window.store';
 import { useGetScormRteScoUrl } from '@entities/scorm/service/scorm-rte.hook';
+import { VideoPlayer } from '@features/learning-window/ui/video-player';
 
-export const Route = createFileRoute('/_learning-window/learning-window/')({
+export const Route = createFileRoute('/_learning/learning-window')({
   component: RouteComponent,
 });
 
@@ -28,6 +18,7 @@ function RouteComponent() {
   const routerState = useRouterState();
 
   const [scormConfig, setScormConfig] = useState<ScormPlayerConfigProperties>();
+  const [videoInfo, setVideoInfo] = useState<any>();
 
   const { curriculum, playInfo, setBaseInfo } = useLearningWindowStore((state) => state);
   const { data: scormInfo } = useGetScormRteScoUrl(scormConfig);
@@ -59,5 +50,10 @@ function RouteComponent() {
     })();
   }, []);
 
-  return scormConfig && <ScormPlayer scormConfig={scormConfig} />;
+  return (
+    <>
+      {scormConfig && <ScormPlayer scormConfig={scormConfig} />}
+      {videoInfo && <VideoPlayer />}
+    </>
+  );
 }
