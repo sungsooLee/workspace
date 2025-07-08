18d1c757ea1da9600d3b0728a0fd2a4e70e5b22a
유홍식/렵력사 <9488404@ict-companion.com>
2025-07-08 09:04:17 +0900

    fix: swagger json 갱신

---
+++ temp/lms/models/com_ever_edu_lms_tag_dto_res_TagResDto.ts
@@ -0,0 +1,8 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * 태그 이름 목록
+ */
+export type com_ever_edu_lms_tag_dto_res_TagResDto = Record<string, any>;
