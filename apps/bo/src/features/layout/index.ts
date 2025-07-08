626b2b294b24e6824d037267e03e9990ff269e27
이창기/협력사 <9496504@ict-companion.com>
2025-07-08 19:10:26 +0900

    fix: 컴포넌트 폴더 변경

--- apps/bo/src/features/layout/index.ts
+++ apps/bo/src/features/layout/index.ts
@@ -4,3 +4,5 @@ export * from './ui/notification/notification-button';
 export * from './ui/my-menu/my-menu';
 export * from './ui/error-component';
 export * from './ui/not-found';
+export * from './ui/gnb-role-select';
+export * from './ui/language/language';
