3f4c88c411af5002e1b84ada0bdb442b56937c43
유홍식/렵력사 <9488404@ict-companion.com>
2025-07-08 17:47:35 +0900

    feat: [LMS-0004] 과정등록

--- apps/bo/src/pages/_unauth/learning_test/course/-components/basic-info/basic-info.tsx
+++ apps/bo/src/pages/_unauth/learning_test/course/-components/basic-info/basic-info.tsx
@@ -42,7 +42,7 @@ const BasicInfoComponent = forwardRef<TabFormRef, CourseTabBaseProps>(
       validate: async () => {
         // 모든 필드에 대해 유효성 검사 수행
         const isValid = await onFormValid();
-        const data = formDataToRequestData(getValues());
+        const data = formDataToRequestData(getValues() as Course);
         const errors = formState.errors;
 
         return {
