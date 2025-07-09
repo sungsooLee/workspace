56cd7c14f16da7edf5fe99c66a59f5a774732fbb
최원석/L&D플랫폼팀 <1seok@hyundai-autoever.com>
2025-07-09 20:04:18 +0900

    refactor: entities 레이어 Index.ts 파일 생성 및 업데이트

--- apps/fo/src/entities/company/index.ts
+++ apps/fo/src/entities/company/index.ts
@@ -1,1 +1,4 @@
+export * from './api/company';
+export * from './model/company';
 export * from './service/company.hook';
+export * from './service/company.queries';
---
+++ apps/fo/src/entities/content/index.ts
@@ -0,0 +1,3 @@
+export * from './api/content';
+export * from './service/content.hook';
+export * from './service/content.queries';
