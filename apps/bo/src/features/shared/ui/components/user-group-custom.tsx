f8568daa8b8240abeb6f8658e062bf5bf1fae1ea
신민제/협력사 <9497019@ict-companion.com>
2025-07-09 15:02:40 +0900

    fix: [NLP_BO_TMS_1100_01] 유저그룹검색 팝업 수정

--- apps/bo/src/features/shared/ui/components/user-group-custom.tsx
+++ apps/bo/src/features/shared/ui/components/user-group-custom.tsx
@@ -1,39 +1,61 @@
 import { ShuttleTreeToChipsV2, TreeNode, useShuttleTreeToChips } from '@learnway/ui';
 import styles from '@learnway/styles/bo/assets/styles/modules/popup-contents.module.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo } from 'react';
 import { cn } from '@learnway/shared';
 import { useFetchOrganizationTree } from '@entities/user-group';
-import { OrganizationTreeResponse } from '@types';
+import { CombineUserGroup } from '@types';
 
 type UserGroupCustomComponentProps = {
   tenantIds: number[];
-  handleSetOption: (data: any) => void;
+  option: CombineUserGroup[];
+  handleSetOption: (data: CombineUserGroup[]) => void;
 };
 
 const UserGroupCustomComponent = ({
   tenantIds,
+  option,
   handleSetOption,
 }: UserGroupCustomComponentProps) => {
-  const { data } = useFetchOrganizationTree(tenantIds);
+  const { data = [] } = useFetchOrganizationTree(tenantIds);
 
-  const [treeData, setTreeData] = useState<TreeNode[]>([]);
+  const initValue = useMemo<TreeNode[]>(
+    () =>
+      option.map(({ key, keys, id, ids, fullName, combiners }) => ({
+        id,
+        ids,
+        key,
+        keys,
+        fullName,
+        isCombined: combiners?.length > 1,
+      })),
+    [option],
+  );
+
   const {
     selectedItems,
     handleSelectItem,
     handleSelectItemWithChildren,
     cancelSelectItem,
     cancelAll,
-  } = useShuttleTreeToChips();
+  } = useShuttleTreeToChips(initValue);
 
   useEffect(() => {
-    if (data && data.length > 0) {
-      const transformedData = transformApiDataToTreeData(data);
-      setTreeData(transformedData);
+    if (selectedItems.length > 0) {
+      const newOption: CombineUserGroup[] = selectedItems.map(
+        ({ key, keys, id, ids, fullName }) => ({
+          combiners:
+            ids?.length > 0
+              ? ids.map((combineValue: number) => ({ combineType: 'USER_GROUP', combineValue }))
+              : [{ combineType: 'USER_GROUP', combineValue: id }],
+          fullName,
+          id,
+          ids,
+          key,
+          keys,
+        }),
+      );
+      handleSetOption(newOption);
     }
-  }, [data]);
-
-  useEffect(() => {
-    if (selectedItems.length > 0) handleSetOption(selectedItems);
   }, [selectedItems]);
 
   return (
@@ -42,8 +64,7 @@ const UserGroupCustomComponent = ({
         <ShuttleTreeToChipsV2
           sourceTitle="유저그룹 - 조직"
           targetTitle="선택 유저그룹 목록"
-          treeData={treeData}
-          selectedKey="fullName"
+          apiData={data}
           selectedItems={selectedItems}
           handleSelectItem={handleSelectItemWithChildren}
           cancelSelectItem={cancelSelectItem}
@@ -55,29 +76,3 @@ const UserGroupCustomComponent = ({
 };
 
 export const UserGroupCustom = UserGroupCustomComponent;
-
-const transformApiDataToTreeData = (apiData: OrganizationTreeResponse[]): TreeNode[] => {
-  const transform = (nodes: OrganizationTreeResponse[], parentId?: string) => {
-    if (!nodes) return [];
-
-    return nodes.map((node) => {
-      const transformedNode = {
-        ...node,
-        key: `${parentId ? `${parentId}-` : ''}${node.id.toString()}`,
-        title: node.name,
-        parentId,
-      } as unknown as TreeNode;
-
-      if (node.children && node.children.length > 0) {
-        transformedNode.children = transform(
-          node.children,
-          `${parentId ? `${parentId}-` : ''}${transformedNode.id}`,
-        );
-      }
-
-      return transformedNode;
-    });
-  };
-
-  return transform(apiData);
-};
