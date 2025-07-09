f79b3c8239fab430dedc3336e1ecd3906765dc25
이성수/협력사 <9490817@ict-companion.com>
2025-07-09 16:28:03 +0900

    [퍼블] fo 가이드 업로드

---
+++ apps/pub/fo/src/pages/_guide/guide/divider.tsx
@@ -0,0 +1,42 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { Divider } from '@learnway/ui';
+
+export const Route = createFileRoute('/_guide/guide/divider')({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div>
+      <h2 className="guide_tit2">Divider Component Guide</h2>
+      <p className="loc react">/libs/ui/src/lib/elements/divider/divider.tsx</p>
+      <p className="info">구분선에 주로 사용</p>
+      <div className="code_example">
+        <pre className="code_block">
+          <code>
+            {`// 초기 import
+  import { Divider } from '@learnway/ui';
+    
+  // 적용방법(예시)
+  <Divider />`}
+          </code>
+        </pre>
+      </div>
+      <div className="group">
+        <h3 className="guide_tit3">기본 사용</h3>
+        <div className="flex_box">
+          <div className="desc w-full">
+            <Divider />
+          </div>
+        </div>
+
+        <h3 className="guide_tit3">연한 타입</h3>
+        <div className="flex_box">
+          <div className="desc w-full">
+            <Divider bgType={'light'} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- libs/ui/src/lib/elements/divider/divider.tsx
+++ libs/ui/src/lib/elements/divider/divider.tsx
@@ -6,11 +6,13 @@ interface DividerProps {
   orientation?: 'horizontal' | 'vertical'; // 수평 또는 수직
   spacing?: 'full' | 'inner'; // 여백 조절 (선택 사항)
   className?: string; // 추가 CSS 클래스 (선택 사항)
+  bgType?: 'normal' | 'light'; // 배경색 type 선언
 }
 
 export const Divider: React.FC<DividerProps> = ({
   orientation = 'horizontal', // 기본값은 수평선
   spacing,
+  bgType = 'normal',
   className,
 }) => {
   return (
@@ -19,6 +21,7 @@ export const Divider: React.FC<DividerProps> = ({
         styles.divider,
         styles[orientation], // horizontal 또는 vertical 클래스 적용
         spacing && styles[spacing], // 좌우 여백 type 적용
+        styles[bgType],
         className,
         'divider',
       )}
