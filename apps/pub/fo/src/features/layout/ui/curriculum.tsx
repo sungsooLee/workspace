afb572a099599d27c25262579ebe1e55917741fe
정민혁/협력사 <9493801@ict-companion.com>
2025-07-10 17:52:21 +0900

    [퍼블] fo 작업중

--- apps/pub/fo/src/features/layout/ui/curriculum.tsx
+++ apps/pub/fo/src/features/layout/ui/curriculum.tsx
@@ -1,104 +1,39 @@
-import { memo, useState } from 'react';
+import { memo } from 'react';
 import { cn } from '@learnway/shared';
 import { IcoClock01 } from '@learnway/icons';
-import { Accordion } from '@learnway/ui';
 
 import styles from './curriculum.module.css';
 
+interface CurriculumData {
+  txt: string;
+  type?: string;
+  time: string;
+}
+
 interface CurriculumProps {
+  curriculumData: CurriculumData[];
   className?: string;
 }
 
-const CurriculumComponent = ({ className }: CurriculumProps) => {
-  const [curriculumValue, setCurriculumValue] = useState<string>('a');
-  const accordionValueItems = [
-    {
-      value: 'a',
-      title: (
-        <div className={styles.title}>
-          <p>
-            1일 업무를 10분만에 해결하는 파이썬 업무자동화<span>1시간</span>
-          </p>
-        </div>
-      ),
-      children: (
-        <ol className={styles.list}>
-          <li>
-            <div className={styles.box}>
-              <p>1.현업사례로 보는 업무자동화에 파이썬이 필요한 이유</p>
-              <div>
-                <span>이북</span>
-                <span>
-                  <IcoClock01 width={20} height={20} stroke="#131416" />
-                  <span>1시간 3분</span>
-                </span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className={styles.box}>
-              <p>2.현업사례로 보는 업무자동화에 파이썬이 필요한 이유</p>
-              <div>
-                <span>동영상</span>
-                <span>
-                  <IcoClock01 width={20} height={20} stroke="#131416" />
-                  <span>1시간 3분</span>
-                </span>
-              </div>
-            </div>
-          </li>
-        </ol>
-      ),
-    },
-    {
-      value: 'b',
-      title: (
-        <div className={styles.title}>
-          <p>
-            1일 업무를 10분만에 해결하는 파이썬 업무자동화<span>1시간</span>
-          </p>
-        </div>
-      ),
-      children: (
-        <ol className={styles.list}>
-          <li>
-            <div className={styles.box}>
-              <p>1.현업사례로 보는 업무자동화에 파이썬이 필요한 이유</p>
-              <div>
-                <span>이북</span>
-                <span>
-                  <IcoClock01 width={20} height={20} stroke="#131416" />
-                  <span>1시간 3분</span>
-                </span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className={styles.box}>
-              <p>2.현업사례로 보는 업무자동화에 파이썬이 필요한 이유</p>
-              <div>
-                <span>동영상</span>
-                <span>
-                  <IcoClock01 width={20} height={20} stroke="#131416" />
-                  <span>1시간 3분</span>
-                </span>
-              </div>
-            </div>
-          </li>
-        </ol>
-      ),
-    },
-  ];
-
+const CurriculumComponent = ({ className, curriculumData }: CurriculumProps) => {
   return (
     <div className={cn(styles.start, styles.curriculum, className)}>
-      <Accordion
-        items={accordionValueItems}
-        value={curriculumValue}
-        className={styles.acc_curriculum}
-        onValueChange={(value) => setCurriculumValue(value as string)}
-        type="multiple"
-      />
+      <ol className={styles.list}>
+        {curriculumData.map((curriculumItems, index) => (
+          <li key={index}>
+            <div className={styles.box}>
+              <p>{curriculumItems.txt}</p>
+              <div>
+                {curriculumItems.type && <span>{curriculumItems.type}</span>}
+                <span className={styles.type}>
+                  <IcoClock01 width={20} height={20} stroke="#131416" />
+                  <span>{curriculumItems.time}</span>
+                </span>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 };
