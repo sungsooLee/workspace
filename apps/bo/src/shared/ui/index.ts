8071dbf6700e1fce0a152993ccfc5c4cc5237a02
조일환/협력사 <9500896@ict-companion.com>
2025-07-10 20:32:15 +0900

    feat: [NLP_BO_CMS_1016] 썸네일 적용

--- apps/bo/src/shared/ui/index.ts
+++ apps/bo/src/shared/ui/index.ts
@@ -78,3 +78,5 @@ export * from './search-box/search-box';
 export * from './search-box/search-field-config';
 export * from './search-box/type';
 export * from './temp-server-status/server-status';
+
+export * from '../lib/convert-to-thumbnail-object';
