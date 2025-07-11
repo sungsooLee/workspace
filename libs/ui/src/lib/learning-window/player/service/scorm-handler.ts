6331a88c30ed32c9a19274ad7b07b35ef08865c4
협력사/김석태 <9496491@ict-companion.com>
2025-07-11 15:14:31 +0900

    feat: [NLP_FO_CLA_1025] 스콤 handler 로그 출력 방식 변경

--- libs/ui/src/lib/learning-window/player/service/scorm-handler.ts
+++ libs/ui/src/lib/learning-window/player/service/scorm-handler.ts
@@ -1,5 +1,6 @@
 import { ScormRteClient } from './scorm-rte-client';
 import { ScormDataManager } from './scorm-data-manager';
+import { checkSetValue } from './scorm-cmi-validate';
 
 /**
  * ASIS: AjaxDMHandlerAPI <- Sample RTE class
@@ -61,7 +62,7 @@ export class ScormHandler {
       result = 'true';
     }
     this.srte.log(
-      'Initialize Returned Error Code ' + this.srte.getErrorManager().getCurrentErrorCode(),
+      `Initialize Returned Error Code ${this.srte.getErrorManager().getCurrentErrorCode()}`,
     );
 
     return result;
@@ -73,7 +74,7 @@ export class ScormHandler {
     if (this.srte.getTerminatedState()) {
       this.srte.getErrorManager().setCurrentErrorCode('143');
       this.srte.log(
-        'Commit Returned Error Code ' + this.srte.getErrorManager().getCurrentErrorCode(),
+        `Commit Returned Error Code ${this.srte.getErrorManager().getCurrentErrorCode()}`,
       );
       return result;
     }
@@ -83,39 +84,39 @@ export class ScormHandler {
     if (this.srte.getTerminatedState()) {
       this.srte.getErrorManager().setCurrentErrorCode('143');
       this.srte.log(
-        'Commit Returned Error Code ' + this.srte.getErrorManager().getCurrentErrorCode(),
+        `Commit Returned Error Code  ${this.srte.getErrorManager().getCurrentErrorCode()}`,
       );
       return result;
     }
     if (!this.srte.isInitialized()) {
       this.srte.getErrorManager().setCurrentErrorCode('142');
       this.srte.log(
-        'Commit Returned Error Code ' + this.srte.getErrorManager().getCurrentErrorCode(),
+        `Commit Returned Error Code ${this.srte.getErrorManager().getCurrentErrorCode()}`,
       );
       return result;
     }
     if (param !== '') {
       this.srte.getErrorManager().setCurrentErrorCode('201');
       this.srte.log(
-        'Commit Returned Error Code ' + this.srte.getErrorManager().getCurrentErrorCode(),
+        `Commit Returned Error Code ${this.srte.getErrorManager().getCurrentErrorCode()}`,
       );
       return result;
     }
 
-    // request type = type_set (4)
-    const reqdata = {
-      mActivityData: this.dm.calllist(),
-      mIsFinished: this.srte.getTerminateCalled(),
-      mRequestType: 4,
-      mCourseID: this.srte.getCourseID(),
-      mStudentID: this.srte.getUserID(),
-      mUserName: this.srte.getUserName(),
-      mStateID: this.srte.getStateID(),
-      mActivityID: this.srte.getActivityID(),
-      mNumAttempt: this.srte.getNumAttempts(),
-      mQuitPushed: this.srte.getWasQuitButtonPushed(),
-      mSuspendPushed: this.srte.getWasLmsSuspendAllPushed(),
-    };
+    // // request type = type_set (4)
+    // const reqdata = {
+    //   mActivityData: this.dm.calllist(),
+    //   mIsFinished: this.srte.getTerminateCalled(),
+    //   mRequestType: 4,
+    //   mCourseID: this.srte.getCourseID(),
+    //   mStudentID: this.srte.getUserID(),
+    //   mUserName: this.srte.getUserName(),
+    //   mStateID: this.srte.getStateID(),
+    //   mActivityID: this.srte.getActivityID(),
+    //   mNumAttempt: this.srte.getNumAttempts(),
+    //   mQuitPushed: this.srte.getWasQuitButtonPushed(),
+    //   mSuspendPushed: this.srte.getWasLmsSuspendAllPushed(),
+    // };
     this.dm.clearcalllist();
     // ClientRTS:1293
     this.srte.restAsyncCommit(this.dm.elements);
@@ -132,7 +133,7 @@ export class ScormHandler {
     //top.frames['LMSFrame'].refreshMenu();
 
     this.srte.log(
-      'Commit Returned Error Code ' + this.srte.getErrorManager().getCurrentErrorCode(),
+      `Commit Returned Error Code ${this.srte.getErrorManager().getCurrentErrorCode()}`,
     );
 
     return result;
@@ -146,21 +147,21 @@ export class ScormHandler {
     if (this.srte.getTerminatedState()) {
       this.srte.getErrorManager().setCurrentErrorCode('113');
       this.srte.log(
-        'Terminate Returned Error Code ' + this.srte.getErrorManager().getCurrentErrorCode(),
+        `Terminate Returned Error Code  + ${this.srte.getErrorManager().getCurrentErrorCode()}`,
       );
       return result;
     }
     if (!this.srte.isInitialized()) {
       this.srte.getErrorManager().setCurrentErrorCode('112');
       this.srte.log(
-        'Terminate Returned Error Code ' + this.srte.getErrorManager().getCurrentErrorCode(),
+        `Terminate Returned Error Code ${this.srte.getErrorManager().getCurrentErrorCode()}`,
       );
       return result;
     }
     if (param !== '') {
       this.srte.getErrorManager().setCurrentErrorCode('201');
       this.srte.log(
-        'Terminate Returned Error Code ' + this.srte.getErrorManager().getCurrentErrorCode(),
+        `Terminate Returned Error Code ${this.srte.getErrorManager().getCurrentErrorCode()}`,
       );
       return result;
     }
@@ -245,7 +246,7 @@ export class ScormHandler {
     }
 
     this.srte.log(
-      'Terminate Returned Error Code ' + this.srte.getErrorManager().getCurrentErrorCode(),
+      `Terminate Returned Error Code ${this.srte.getErrorManager().getCurrentErrorCode()}`,
     );
     return result;
   }
@@ -257,22 +258,27 @@ export class ScormHandler {
     if (this.srte.getTerminatedState()) {
       this.srte.getErrorManager().setCurrentErrorCode('133');
       this.srte.log(
-        'SetValue Returned Error Code ' + this.srte.getErrorManager().getCurrentErrorCode(),
+        `SetValue Returned Error Code ${this.srte.getErrorManager().getCurrentErrorCode()}`,
       );
       return 'false';
     }
     if (!this.srte.isInitialized()) {
       this.srte.getErrorManager().setCurrentErrorCode('132');
       this.srte.log(
-        'SetValue Returned Error Code ' + this.srte.getErrorManager().getCurrentErrorCode(),
+        `SetValue Returned Error Code ${this.srte.getErrorManager().getCurrentErrorCode()}`,
       );
+      return 'false';
+    }
+
+    if (!checkSetValue(dmelement)) {
+      this.srte.getErrorManager().setCurrentErrorCode('404');
       return 'false';
     }
 
     this.dm.setValue(dmelement, value);
     val = 'true';
 
-    this.srte.log('SetValue Returned ' + val);
+    this.srte.log(`SetValue Returned ${val}`);
     return val;
   }
 
@@ -282,14 +288,14 @@ export class ScormHandler {
     if (this.srte.getTerminatedState()) {
       this.srte.getErrorManager().setCurrentErrorCode('123');
       this.srte.log(
-        'GetValue Returned Error Code ' + this.srte.getErrorManager().getCurrentErrorCode(),
+        `GetValue Returned Error Code ${this.srte.getErrorManager().getCurrentErrorCode()}`,
       );
       return val;
     }
     if (!this.srte.isInitialized()) {
       this.srte.getErrorManager().setCurrentErrorCode('122');
       this.srte.log(
-        'GetValue Returned Error Code ' + this.srte.getErrorManager().getCurrentErrorCode(),
+        `GetValue Returned Error Code ${this.srte.getErrorManager().getCurrentErrorCode()}`,
       );
       return val;
     }
@@ -324,7 +330,7 @@ export class ScormHandler {
     }
 
     this.srte.log(
-      'GetValue Returned Error Code ' + this.srte.getErrorManager().getCurrentErrorCode(),
+      `GetValue Returned Error Code ${this.srte.getErrorManager().getCurrentErrorCode()}`,
     );
     return val;
   }
@@ -332,21 +338,21 @@ export class ScormHandler {
   GetLastError() {
     this.srte.log('Called GetLastError() ');
     const val = this.srte.getErrorManager().getCurrentErrorCode();
-    this.srte.log('GetLastError Returned ' + val);
+    this.srte.log(`GetLastError Returned ${val}`);
     return val;
   }
 
   GetErrorString(errcode: string) {
     this.srte.log(`Called GetErrorString(${errcode}) `);
     const val = this.srte.getErrorManager().getErrorDescription(errcode);
-    this.srte.log('GetErrorString Returned ' + val);
+    this.srte.log(`GetErrorString Returned ${val}`);
     return val;
   }
 
   GetDiagnostic(errcode: string) {
     this.srte.log(`Called GetDiagnostic(${errcode}) `);
     const val = this.srte.getErrorManager().getErrorDiagnostic(errcode);
-    this.srte.log('GetDiagnostic Returned ' + val);
+    this.srte.log(`GetDiagnostic Returned  ${val}`);
     return val;
   }
 }
