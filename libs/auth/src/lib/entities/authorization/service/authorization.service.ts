c45dfe08a4fca6a5c58c7f9d6001d679fb04a286
이창기/협력사 <9496504@ict-companion.com>
2025-07-16 13:00:32 +0900

    fix: authuser role BO/FO 필터링

--- libs/auth/src/lib/entities/authorization/service/authorization.service.ts
+++ libs/auth/src/lib/entities/authorization/service/authorization.service.ts
@@ -21,36 +21,45 @@ export function removeToken() {
   tokenService.clear();
 }
 
-// TODO Active BO, FO 체크 추가
+/**
+ * @description 로그인 데이터를 변경 처리
+ * @param data (AxiosResponse)
+ * @returns AuthUser
+ */
 export function convertToAuthUser(data: AxiosResponse): AuthUser {
   console.log('### convertToAuthUser', data);
-  const user = data.data;
-  const { tenants, roles } = user;
+  const user = data.data as AuthUser;
+  const { tenants, roles: rolesAll } = user;
 
   const APP_INFO = getConfig().APP_INFO;
 
   console.log('### APP_INFO', APP_INFO);
 
+  // 마지막 저장된 테넌트, 롤 정보
   const tenantId = APP_INFO === 'BO' ? user.lastVisitedBoTenantId : user.lastVisitedFoTenantId;
   const roleId = APP_INFO === 'BO' ? user.lastVisitedBoRoleId : user.lastVisitedFoRoleId;
+
+  // 로그인 데이터 중 siteScope에 맞는 역할 필터링
+  const roles = rolesAll?.filter((role) => role.siteScope === APP_INFO);
 
   console.log('### LAST 테넌트 : ', tenantId);
   console.log('### LAST 롤: ', roleId);
 
   const tenant = tenants?.find((tenant: { tenantId: any }) => tenant.tenantId === tenantId);
-  const role = roles?.find((role: { roleId: any }) => role.roleId === roleId);
+  const role = roles.find((role: { roleId: any }) => role.roleId === roleId);
 
   return {
     ...user,
-    activeTenant: tenant ? tenant : user.tenants?.length > 0 ? tenants?.[0] : null,
-    activeRole: role ? role : roles?.roles?.length > 0 ? roles?.roles?.[0] : null,
+    roles,
+    activeTenant: tenant ? tenant : user.tenants?.length > 0 ? tenants?.[0] : undefined,
+    activeRole: role ? role : roles?.length > 0 ? roles?.[0] : undefined,
     phoneNumberNationCode: user?.phoneNumberNationCode ?? 'KR',
-    accessToken: data.headers['access-token'],
-    refreshToken: data.headers['refresh-token'],
     exp: data.headers['refresh-token-exp'],
-    //passwordExpireDate: '2025-04-14T14:03:35.000+00:00',
-    //activeTenantId: user.tenantIds?.length > 0 ? tenantIds[0] : null,
-    //activeRoleId: user.roles?.length > 0 ? user.roles[0].roleId : null,
+    // accessToken: data.headers['access-token'],
+    // refreshToken: data.headers['refresh-token'],
+    // passwordExpireDate: '2025-04-14T14:03:35.000+00:00',
+    // activeTenantId: user.tenantIds?.length > 0 ? tenantIds[0] : null,
+    // activeRoleId: user.roles?.length > 0 ? user.roles[0].roleId : null,
   };
 }
 
