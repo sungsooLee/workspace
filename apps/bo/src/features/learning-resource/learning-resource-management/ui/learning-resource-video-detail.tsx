2a7a346a185ab929defacd38c6a0d843aaf4a344
조일환/협력사 <9500896@ict-companion.com>
2025-07-17 13:31:59 +0900

    feat: [NLP_BO_CMS_1002] ContentHistoryInfo 항목에서 데이터 출력

--- apps/bo/src/features/learning-resource/learning-resource-management/ui/learning-resource-video-detail.tsx
+++ apps/bo/src/features/learning-resource/learning-resource-management/ui/learning-resource-video-detail.tsx
@@ -129,7 +129,7 @@ const LearningResourceVideoDetailComponent = ({ provider }: Props) => {
         </ContentsRow>
       </FormDisplay>
       <MediaContentRequiredCheckFormField provider={provider} />
-      <ContentsHistoryInfoFormField />
+      <ContentsHistoryInfoFormField provider={provider} />
     </>
   );
 };
