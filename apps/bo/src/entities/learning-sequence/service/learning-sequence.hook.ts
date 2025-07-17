e271e22a23fd4bc1809cc89ada3c4ba8afe2075e
김동영/협력사 <9505174@ict-companion.com>
2025-07-17 16:26:22 +0900

    feat: [NLP_BO_LMS_0035,0036,0037] 수강신청목록 추가/강사관리 폴더 이동

---
+++ apps/bo/src/entities/learning-sequence/service/learning-sequence.hook.ts
@@ -0,0 +1,3 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
+import { queryOptions, mutateOptions } from './learning-sequence.queries';
--- apps/bo/src/pages/_layout/learning/course/detail/-tabs/sequence/index.tsx
+++ apps/bo/src/pages/_layout/learning/course/detail/-tabs/sequence/index.tsx
@@ -1,8 +1,15 @@
-import { forwardRef, useImperativeHandle } from 'react';
+import { forwardRef, useState, useImperativeHandle } from 'react';
 import { CourseDetailTabBaseProps, CourseDetailTabFormRef } from '../../../-common/type';
+import {
+  SequenceDetail,
+  SequenceList,
+} from '@features/learning-operate/learning-sequence/sequence-management';
 
 const SequenceComponent = forwardRef<CourseDetailTabFormRef, CourseDetailTabBaseProps>(
   ({ courseId }, ref) => {
+    const [mode, setMode] = useState<string>('MAIN');
+    const [sequenceId, setSequenceId] = useState<number>(0);
+
     // 부모 컴포넌트에서 호출할 수 있는 메서드
     useImperativeHandle(ref, () => ({
       getValues: () => console.log('getValues'),
@@ -12,7 +19,15 @@ const SequenceComponent = forwardRef<CourseDetailTabFormRef, CourseDetailTabBase
       },
     }));
 
-    return <>Sequence</>;
+    return (
+      <>
+        {mode === 'MAIN' ? (
+          <SequenceList setMode={setMode} setSequenceId={setSequenceId} courseId={1} />
+        ) : (
+          <SequenceDetail setMode={setMode} courseId={1} sequenceId={sequenceId} />
+        )}
+      </>
+    );
   },
 );
 
