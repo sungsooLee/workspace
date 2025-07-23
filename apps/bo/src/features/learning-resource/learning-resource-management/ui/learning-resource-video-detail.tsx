3bf0f408b1f88021c7975a626a177fe1e186700b
조일환/협력사 <9500896@ict-companion.com>
2025-07-23 13:30:43 +0900

    refactor: [NLP_BO_CMS_1002] extract VideoSubtitlesForm

--- apps/bo/src/features/learning-resource/learning-resource-management/ui/learning-resource-video-detail.tsx
+++ apps/bo/src/features/learning-resource/learning-resource-management/ui/learning-resource-video-detail.tsx
@@ -1,53 +1,19 @@
 //  IA105 / NLP_BO_CMS_1016, NLP_BO_CMS_1002 / 학습자원조회_나의 학습자원_등록_동영상(자체)
 
-import { FormDisplay, SubTitlesFormField } from '@features/form';
 import { MediaContentRequiredCheckFormField } from '@features/form/ui';
 import { DynamicFormProvider } from '@learnway/hooks';
-import { ContentsRow } from '@learnway/ui';
-import { ContentsHistoryInfoFormField, FormRow2, SwitchFormField } from '@shared/ui';
+import { ContentsHistoryInfoFormField } from '@shared/ui';
 import { LearningResourceBaseForm } from './learning-resource-base-form';
-import { t } from 'i18next';
+import { VideoSubtitlesForm } from './video-subtitles-form';
 
 interface Props {
   provider: DynamicFormProvider;
 }
 const LearningResourceVideoDetailComponent = ({ provider }: Props) => {
-  const { watch } = provider;
-  const isSubtitles = watch('isSubtitles');
-  const videoSubtitles = watch('videoSubtitles');
   return (
     <>
       <LearningResourceBaseForm provider={provider} showAiInfo showLessonTime readOnlyLessonTime />
-      <ContentsRow type="horizontal" className="inactive">
-        {/* 자막 여부 */}
-        <FormRow2
-          label={t('자막 추가')}
-          provider={provider}
-          name="isSubtitles"
-          switchConfig={{
-            labelTarget: 'subtitles',
-          }}
-          element={
-            <SwitchFormField
-              label={isSubtitles ? `자막 ${videoSubtitles?.length || 0}개` : '자막 없음'}
-            />
-          }
-          value={false}
-        />
-      </ContentsRow>
-      <FormDisplay provider={provider} dependencies={[{ name: 'isSubtitles', value: true }]}>
-        <ContentsRow>
-          {/*자막 목록*/}
-          <FormRow2
-            provider={provider}
-            name="videoSubtitles"
-            type="custom"
-            format="array"
-            value={[]}
-            element={<SubTitlesFormField />}
-          />
-        </ContentsRow>
-      </FormDisplay>
+      <VideoSubtitlesForm provider={provider} />
       <MediaContentRequiredCheckFormField provider={provider} />
       <ContentsHistoryInfoFormField provider={provider} />
     </>
