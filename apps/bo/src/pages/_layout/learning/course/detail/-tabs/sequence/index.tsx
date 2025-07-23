af905a538fc805744581cfea8dfc84d6a7deb0fd
협력사/유홍식 <9488404@ict-companion.com>
2025-07-23 10:17:01 +0900

    feat: [LMS-0005] 과정등록-상태관리추가

--- apps/bo/src/pages/_layout/learning/course/detail/-tabs/sequence/index.tsx
+++ apps/bo/src/pages/_layout/learning/course/detail/-tabs/sequence/index.tsx
@@ -1,14 +1,44 @@
-import { forwardRef, useState, useImperativeHandle, useEffect } from 'react';
-import { CourseDetailTabBaseProps, CourseDetailTabFormRef } from '../../../-common/type';
 import {
   SequenceDetail,
   SequenceList,
 } from '@features/learning-operate/learning-sequence/sequence-management';
+import { useNavigate } from '@tanstack/react-router';
+import { useUpdateEffect } from 'ahooks';
+import { forwardRef, useEffect, useState } from 'react';
+import { CourseDetailTabBaseProps, CourseDetailTabFormRef } from '../../../-common/type';
+import {
+  ContentViewType,
+  TriggerKey,
+  useCourseActions,
+  useCourseStore,
+} from '../../../-store/use-course-store';
+import { Route as CourseRoute } from '../../../index';
 
 const SequenceComponent = forwardRef<CourseDetailTabFormRef, CourseDetailTabBaseProps>(
   ({ courseId, courseSequenceId, setCourseSequenceId }, ref) => {
     const [mode, setMode] = useState<string>('MAIN');
     const [sequenceId, setSequenceId] = useState<number>(0);
+    const lastTriggered = useCourseStore((state) => state.lastTriggered);
+    const { setContentViewType } = useCourseActions();
+    const navigate = useNavigate();
+
+    useUpdateEffect(() => {
+      switch (lastTriggered?.key) {
+        case TriggerKey.LIST:
+          navigate({ to: CourseRoute.to });
+          break;
+        case TriggerKey.SAVE:
+          break;
+        case TriggerKey.DELETE:
+          // handleDeleteAction(lastTriggered.payload);
+          break;
+      }
+    }, [lastTriggered]);
+
+    useEffect(() => {
+      console.log('lastTriggered', lastTriggered);
+      setContentViewType(mode === 'MAIN' ? ContentViewType.LIST : ContentViewType.DETAIL); // 탭
+    }, [mode]);
 
     useEffect(() => {
       console.log('차수ID:', sequenceId);
