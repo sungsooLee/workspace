61553b2e263ae440dbd9361c9c65dfe08217ce5c
이성수/협력사 <9490817@ict-companion.com>
2025-07-23 13:23:58 +0900

    [퍼블] fo 카테고리 작업중

--- apps/pub/fo/src/pages/-components/toggle-button/toggle-button.tsx
+++ apps/pub/fo/src/pages/-components/toggle-button/toggle-button.tsx
@@ -2,21 +2,34 @@
 import React, { useState } from 'react';
 import styles from './toggle-button.module.css';
 import { Button } from '@learnway/ui';
+import { cn } from '@learnway/shared';
+import { IcoHeart } from '@learnway/icons';
+
+type VariantType = 'heart' | undefined; // 아이콘 케이스 추가
 
 interface ToggleButtonProps {
   label?: string;
-  icon?: React.ReactNode;
-  variant?: 'heart';
+  variant?: VariantType;
   defaultChecked?: boolean;
   onToggle?: (checked: boolean) => void;
+  className?: string;
 }
+
+const getIconByVariant = (variant: VariantType): React.ReactNode => {
+  switch (variant) {
+    case 'heart':
+      return <IcoHeart width={24} height={24} fill={'none'} stroke="#fff" />;
+    default:
+      return null;
+  }
+};
 
 const ToggleButton: React.FC<ToggleButtonProps> = ({
   label,
-  icon,
   variant,
   defaultChecked = false,
   onToggle,
+  className,
 }) => {
   const [checked, setChecked] = useState(defaultChecked);
 
@@ -26,11 +39,18 @@ const ToggleButton: React.FC<ToggleButtonProps> = ({
     onToggle?.(next);
   };
 
+  const icon = getIconByVariant(variant);
+
   return (
     <Button
       label={label}
       aria-checked={checked}
-      className={styles.toggle_btn}
+      className={cn(
+        styles.toggle_btn,
+        variant && styles[variant],
+        checked ? styles.checked : null,
+        className,
+      )}
       onClick={handleClick}
       icon={icon}
     />
