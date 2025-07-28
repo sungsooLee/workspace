dc83ec13c142b1114fa37149bc169c492f4f97d4
정민혁/협력사 <9493801@ict-companion.com>
2025-07-28 15:51:54 +0900

    [퍼블] gnb mobile 작업

--- apps/pub/fo/src/pages/_layout/my/information-change.tsx
+++ apps/pub/fo/src/pages/_layout/my/information-change.tsx
@@ -2,11 +2,7 @@ import { createFileRoute, Link } from '@tanstack/react-router';
 import { cn } from '@learnway/shared';
 import { isMobile } from 'react-device-detect';
 import { ContentsRow, Input, Button, useModal } from '@learnway/ui';
-import {
-  PasswordChangePopup,
-  PhoneChangePopup,
-  SettingLanguagePopup,
-} from '../../../features/layout';
+import { PasswordChangePopup, PhoneChangePopup } from '../../../features/layout';
 import noticeBoxStyles from '@learnway/styles/fo/shared/ui/notice-box/notice-box.module.css';
 import { IcoCaution } from '@learnway/icons';
 
@@ -56,7 +52,7 @@ function RouteComponent() {
                     onClick={() =>
                       openModal({
                         width: isMobile ? 'm_full' : 'sm',
-                        content: <SettingLanguagePopup />,
+                        content: <PasswordChangePopup />,
                       })
                     }
                   >
