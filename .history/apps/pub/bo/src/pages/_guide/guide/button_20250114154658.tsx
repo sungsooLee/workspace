d2c1515457de306512f597bc6c3c5cca40a2d423
최원석/L&D플랫폼팀 <1seok@hyundai-autoever.com>
2025-07-29 11:14:16 +0900

    refactor: 사용하지 않는 history 폴더 삭제

--- .history/apps/pub/bo/src/pages/_guide/guide/button_20250114154658.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_guide/guide/button')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/_guide/guide/button"!</div>
-}
--- .history/apps/pub/bo/src/pages/_guide/guide/components/layout_20250115155723.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/_guide/guide/components/Layout')({
-  component: Layout,
-});
-
-function Layout() {
-  return <div>Hello "/_guide/guide/components/layout"!</div>;
-}
