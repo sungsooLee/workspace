9090e40dcd8f77180933c2837600fc5a38ab0afb
장주용/협력사 <9504287@ict-companion.com>
2025-07-30 13:02:59 +0900

    fix: [NLP_FO_CAT_MA_1000] 모바일 풋터의 학습테마 버튼 수정

--- apps/fo/src/widgets/layout/m.ui/main/footer/footer.tsx
+++ apps/fo/src/widgets/layout/m.ui/main/footer/footer.tsx
@@ -5,18 +5,27 @@ import { Link } from '@tanstack/react-router';
 import { cn } from '@learnway/shared';
 
 import { Language } from '../../../../../features/platform';
-import { Button, useSearchStore } from '@learnway/ui';
+import { Button, useModal, useSearchStore } from '@learnway/ui';
 import { IcoHome03, IcoDotpoints, IcoSearch, IcoMybook, IcoEye, IcoArray } from '@learnway/icons';
 
 import styles from '@learnway/styles/fo/widgets/layout/m.ui/main/footer/footer.module.css';
+import { CategoryPopup } from '@features/layout';
 
 function FooterComponent() {
   const { t } = useTranslation();
+  const { openModal } = useModal();
   const openSearch = useSearchStore((state) => state.openSearch);
 
   const handleSearchClick = () => {
     openSearch();
   };
+
+  const handleCategoryPopup = () => {
+    openModal({
+      width: 'm_full',
+      content: <CategoryPopup />,
+    })
+  }
 
   return (
     <div className={`${styles.start} ${styles.footer_fixed}`}>
@@ -29,7 +38,7 @@ function FooterComponent() {
             </Link>
           </li>
           <li>
-            <Button>
+            <Button onClick={handleCategoryPopup}>
               <IcoArray width={24} height={24} stroke="#131C30"  fill="none"/>
               <span>학습테마</span>
             </Button>
