adb854248eaed9a14a30a9c0cf9a01e99d15e992
협력사/김석태 <9496491@ict-companion.com>
2025-07-31 12:32:47 +0900

    chore: [NLP_FO_CLA_1025] 학습창 다국어 t  함수 추가

--- libs/ui/src/lib/video-player/components/video-setting-popup.tsx
+++ libs/ui/src/lib/video-player/components/video-setting-popup.tsx
@@ -1,5 +1,5 @@
 import { memo } from 'react';
-
+import { t } from 'i18next';
 import { IcoArrowForward } from '@learnway/icons';
 
 import styles from './video-setting-popup.module.css';
@@ -9,13 +9,13 @@ import { Button } from '../../button/button';
 const VideoSettingPopupComponent = () => {
   return (
     <ModalContainer>
-      <ModalTitle>{'설정'}</ModalTitle>
+      <ModalTitle>{t('설정')}</ModalTitle>
       <ModalBody>
         <div className={`${styles.start} ${styles.video_setting}`}>
           <ul>
             <li>
               <Button>
-                <strong>재생속도</strong>
+                <strong>{t('재생속도')}</strong>
                 <span>
                   1x
                   <IcoArrowForward width={16} height={16} stroke="#131416" />
@@ -24,7 +24,7 @@ const VideoSettingPopupComponent = () => {
             </li>
             <li>
               <Button>
-                <strong>소스</strong>
+                <strong>{t('소스')}</strong>
                 <span>
                   Auto
                   <IcoArrowForward width={16} height={16} stroke="#131416" />
@@ -36,10 +36,10 @@ const VideoSettingPopupComponent = () => {
       </ModalBody>
       <ModalFooter>
         <Button variant={'gray'} size={'lg'}>
-          <span>취소</span>
+          <span>{t('취소')}</span>
         </Button>
         <Button variant={'primary'} size={'lg'}>
-          <span>확인</span>
+          <span>{t('확인')}</span>
         </Button>
       </ModalFooter>
     </ModalContainer>
