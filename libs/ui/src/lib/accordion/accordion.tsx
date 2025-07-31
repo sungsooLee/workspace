b5eb33601474429dfb079d6f5a2af4d6e463075b
이창기/협력사 <9496504@ict-companion.com>
2025-07-31 20:29:02 +0900

    fix: accodion 더미 props 사용부분 삭제

--- libs/ui/src/lib/accordion/accordion.tsx
+++ libs/ui/src/lib/accordion/accordion.tsx
@@ -83,20 +83,18 @@ const AccordionComponent = forwardRef<
               className={cn(styles.accordion_item)}
               key={item.value}
             >
-              {!item.isDummy && item.children && (
-                <Primitive.Trigger
-                  className={cn(styles.accordion_btn, item.children && 'has_children')}
-                >
-                  <span className={cn('accordion_title', item?.active === true ? 'active' : '')}>
-                    {item.children && editionValue?.includes(item.value) ? (
-                      <IcoArrowDown width={16} height={16} stroke="#4C515E" />
-                    ) : (
-                      <IcoArrowForward width={16} height={16} stroke="#4C515E" />
-                    )}
-                    {item.title}
-                  </span>
-                </Primitive.Trigger>
-              )}
+              <Primitive.Trigger
+                className={cn(styles.accordion_btn, item.children && 'has_children')}
+              >
+                <span className={cn('accordion_title', item?.active === true ? 'active' : '')}>
+                  {item.children && editionValue?.includes(item.value) ? (
+                    <IcoArrowDown width={16} height={16} stroke="#4C515E" />
+                  ) : (
+                    <IcoArrowForward width={16} height={16} stroke="#4C515E" />
+                  )}
+                  {item.title}
+                </span>
+              </Primitive.Trigger>
               <Primitive.Content className={cn(styles.accordion_content)}>
                 {item.children}
               </Primitive.Content>
