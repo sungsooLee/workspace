23448a8b280f8d34ef0a1e730b19711ad1ef7900
조일환/협력사 <9500896@ict-companion.com>
2025-08-01 16:35:41 +0900

    fix: [ChannelChoicePopup] ]channel 조회시 에러 수정

--- apps/bo/src/shared/ui/modal/channel-choice-modal.tsx
+++ apps/bo/src/shared/ui/modal/channel-choice-modal.tsx
@@ -25,6 +25,10 @@ const ChannelChoicePopupComponent = () => {
   const { data: channel } = useFetchChannelByRoleId(data?.activeRole?.roleId as number);
 
   const getChannels = useMemo(() => {
+    if (!channel)
+      return () => {
+        return { data: [] };
+      };
     return ({ tenantId, channelName }: { tenantId: number; channelName: string }) => {
       const channels = chain(channel)
         .filter((c) => Boolean(c.tenantList.find((t) => t.tenantId === tenantId))) // tenant 필터
