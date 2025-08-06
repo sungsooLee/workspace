08cccd5d1dc7e2dea01635c1b8488cf7c06486db
이창기/협력사 <9496504@ict-companion.com>
2025-08-06 16:17:45 +0900

    fix: fo _layout 정리

--- apps/fo/src/pages/_layout/navigate.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_layout/navigate')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/_layout/navigate"!</div>
-}
