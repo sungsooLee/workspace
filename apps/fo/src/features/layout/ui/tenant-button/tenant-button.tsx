8d6e111f86b5323dda7462298d186d91e4dc0b9d
이창기/협력사 <9496504@ict-companion.com>
2025-08-06 20:44:34 +0900

    fix: [FO_COM_1000] 모바일 아바타, 테넌트선택

--- apps/fo/src/features/layout/ui/tenant-button/tenant-button.tsx
+++ apps/fo/src/features/layout/ui/tenant-button/tenant-button.tsx
@@ -75,7 +75,7 @@ const TenantModal = () => {
 
   return (
     <ModalContainer>
-      <ModalTitle>{t('언어 설정')}</ModalTitle>
+      <ModalTitle>{t('테넌트 선택')}</ModalTitle>
       <ModalBody>
         <div className={`${styles.start} ${styles.tenant_modal}`}>
           <TenantContent />
--- apps/fo/src/features/layout/ui/user-avatar/user-avatar-contents.tsx
+++ apps/fo/src/features/layout/ui/user-avatar/user-avatar-contents.tsx
@@ -78,14 +78,11 @@ const UserAvatarContentsComponent = ({ contentType, setContentType }: any) => {
       <div className={userMyStyles.avatar_area}>
         <div className={userMyStyles.profile_info}>
           <div className={userMyStyles.avatar_img}>
-            {/* 이미지일경우 */}
             <Avatar
               imageUrl={getFullImagePath(authUser?.avataImage)}
               size="2xl"
               fallback={<AvataFallback name={authUser?.name} />}
             />
-            {/* 텍스트일경우 */}
-            {/* <Avatar fallback="AB" size="2xl" /> */}
             <span className={userMyStyles.ico}>
               <Button variant="ghost" size="ts" onlyIcon={true} icon={<IcoLearning03 />} />
             </span>
