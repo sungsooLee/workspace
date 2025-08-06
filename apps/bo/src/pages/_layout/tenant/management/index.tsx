2379baeb233f542aa613a3b468825955bce533bf
최원석/L&D플랫폼팀 <1seok@hyundai-autoever.com>
2025-08-06 15:30:12 +0900

    fix: 빌드 에러 import 수정

--- apps/bo/src/pages/_layout/tenant/management/index.tsx
+++ apps/bo/src/pages/_layout/tenant/management/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import { TenantManagmentList } from '@features/platform-management/tenant/ui/tenant-managment-list';
+import { TenantManagmentList } from '@features/platform-management/tenant';
 import { MainContents, PageContainer } from '@shared/ui/layout';
 export const Route = createFileRoute('/_layout/tenant/management/')({
   component: RouteComponent,
