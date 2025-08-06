3ff26791a10138287edfff0d7982134036ff14c4
이현주 <9504273@ict-companion.com>
2025-08-06 14:22:46 +0900

    fix: [CMS] 문항 추가 관련 버그 수정

--- apps/bo/src/features/learning-resource/learning-resource-management/service/question-bank/use-question-bank-info-input.ts
+++ apps/bo/src/features/learning-resource/learning-resource-management/service/question-bank/use-question-bank-info-input.ts
@@ -13,21 +13,17 @@ import { useToast } from '@learnway/ui/toast';
 import { ContentType } from '@shared/types/enums';
 import { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useLearningResourceQuestionDetailForm } from '../learning-resource-question-detail-from.hook';
 import { useQuestionSort } from '../learning-resource-question-sort.hook';
 
-export const useQuestionBankInfoInput = () => {
+export const useQuestionBankInfoInput = (contentUuid: string) => {
   const { t } = useTranslation();
 
   const { open: openToast } = useToast();
 
-  const { baseInfo } = useLearningResourceQuestionDetailForm();
-
   const [questionItemList, setQuestionItemList] = useState<QuestionItem[]>([]);
   const [selectedQuestionRows, setSelectedQuestionRows] = useState<QuestionItem[]>([]);
 
-  const { data: questionList = [], refetch } = useGetQuestionItemList(baseInfo?.contentUuid);
-  const contentUuid = baseInfo?.contentUuid ?? '';
+  const { data: questionList = [], refetch } = useGetQuestionItemList(contentUuid);
 
   const { sensors, handleOnDragEnd } = useQuestionSort({
     contentUuid,
@@ -121,11 +117,12 @@ export const useQuestionBankInfoInput = () => {
   }, [contentUuid, selectedQuestionRows]);
 
   useEffect(() => {
-    setQuestionItemList(questionList);
+    if (questionList.length) {
+      setQuestionItemList(questionList);
+    }
   }, [questionList]);
 
   return {
-    baseInfo,
     questionItemList,
     setQuestionItemList,
     selectedQuestionRows,
