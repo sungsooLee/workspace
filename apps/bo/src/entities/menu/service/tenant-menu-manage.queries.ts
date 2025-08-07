6c337721b9c907739f18f1f170efa5d37a305cd6
최원석/L&D플랫폼팀 <1seok@hyundai-autoever.com>
2025-08-07 16:49:38 +0900

    refactor: service class default 삭제

--- apps/bo/src/entities/menu/service/tenant-menu-manage.queries.ts
+++ apps/bo/src/entities/menu/service/tenant-menu-manage.queries.ts
@@ -1,9 +1,10 @@
-import TenantMenuManageService from '../api/menu-tenant-manage';
 import { getQuerySkipToken } from '@learnway/shared';
+import { TenantMenuManageService } from '../api/menu-tenant-manage';
 
 export const queryKeys = {
   tree: ['menu-tenant-manage'] as const,
-  detail: (tenantMappingMenuId: number) => [...queryKeys.tree, tenantMappingMenuId] as const };
+  detail: (tenantMappingMenuId: number) => [...queryKeys.tree, tenantMappingMenuId] as const,
+};
 
 export const tenantMenuManageQueryOptions = {
   //메뉴 트리 정보
@@ -12,20 +13,28 @@ export const tenantMenuManageQueryOptions = {
     queryFn: () =>
       tenantId
         ? TenantMenuManageService.findMenuTenantMappingTree(tenantId, menuScope)
-        : getQuerySkipToken() }),
+        : getQuerySkipToken(),
+  }),
   detail: (tenantMappingMenuId: number) => ({
     queryKey: [...queryKeys.detail(tenantMappingMenuId)],
     queryFn: () =>
       tenantMappingMenuId
         ? TenantMenuManageService.findMenuTenantDetail(tenantMappingMenuId)
-        : getQuerySkipToken() }) };
+        : getQuerySkipToken(),
+  }),
+};
 
 export const mutateOptions = {
   updateMenuTenent: () => ({
-    mutationFn: (payload: any) => TenantMenuManageService.updateMenuTenant(payload) }),
+    mutationFn: (payload: any) => TenantMenuManageService.updateMenuTenant(payload),
+  }),
   deleteMenuTenent: () => ({
-    mutationFn: (payload: any) => TenantMenuManageService.deleteMenuTenant(payload) }),
+    mutationFn: (payload: any) => TenantMenuManageService.deleteMenuTenant(payload),
+  }),
   createMenuTenent: () => ({
-    mutationFn: (payload: any) => TenantMenuManageService.createMenuTenant(payload) }),
+    mutationFn: (payload: any) => TenantMenuManageService.createMenuTenant(payload),
+  }),
   changeMenuTenentDnd: () => ({
-    mutationFn: (payload: any) => TenantMenuManageService.changeMenuTenantDnd(payload) }) };
+    mutationFn: (payload: any) => TenantMenuManageService.changeMenuTenantDnd(payload),
+  }),
+};
