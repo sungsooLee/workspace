7e72fdcfcdbd1364c8a9b1d9a59cc5b26e594e50
정민혁/협력사 <9493801@ict-companion.com>
2025-08-11 14:29:42 +0900

    [퍼블] 유저검색 작업

--- apps/pub/fo/src/config/guide-data.ts
+++ apps/pub/fo/src/config/guide-data.ts
@@ -1937,7 +1937,7 @@ export const guideData = [
     layoutType: '', // 자동 분리됨 작성x
     pageId: '',
     pageType: 'Pop-up',
-    completionDate: '',
+    completionDate: '2025-08-11',
     lastUpdateDate: '',
     remarks: '- user-search-popup.tsx',
   },
