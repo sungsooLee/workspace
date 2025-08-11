d2a83120e57f4b7151e997a637d037e4b88d51b9
협력사/김석태 <9496491@ict-companion.com>
2025-08-11 10:07:02 +0900

    feat: [NLP_FO_CLA_MA_1025] 모바일 학습창 렌스명 노출 적용

--- libs/ui/src/lib/learning-window/layout/learning-layout.tsx
+++ libs/ui/src/lib/learning-window/layout/learning-layout.tsx
@@ -8,12 +8,14 @@ import { LearningContainer } from './learning-container/learning-container';
 import styles from '@learnway/styles/fo/pages/_learning/learning-layout.module.css';
 
 import { MobileView, BrowserView } from 'react-device-detect';
+import { useLearningWindow } from '../learnway-learning-window.store';
 
 interface LearningLayoutComponentProps {
   children: ReactNode;
 }
 
 function LearningLayoutComponent({ children }: LearningLayoutComponentProps) {
+  const { playInfo } = useLearningWindow();
   return (
     <>
       <LearningHeader />
@@ -21,7 +23,15 @@ function LearningLayoutComponent({ children }: LearningLayoutComponentProps) {
       <div className={`${styles.start} ${styles.container}`}>
         <div className={styles.inner}>
           <main>
-            <LearningContainer>{children}</LearningContainer>
+            <LearningContainer>
+              {/* 퍼블수정 20250811 모바일 레슨명 추가 */}
+              <MobileView>
+                <div className={styles.lesson_box}>
+                  <strong>{playInfo?.lessonName}</strong>
+                </div>
+              </MobileView>
+              {children}
+            </LearningContainer>
           </main>
         </div>
       </div>
